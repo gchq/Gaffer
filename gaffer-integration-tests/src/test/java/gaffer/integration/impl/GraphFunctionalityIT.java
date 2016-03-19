47eae52f4d82e8c5c7a26a5d288309c3fc0bfaf0
p013570 <p013570@users.noreply.github.com>
2016-03-19 01:48:02 +0000

    gh-112 - Added cache operations

--- gaffer-integration-tests/src/test/java/gaffer/integration/impl/GraphFunctionalityIT.java
+++ gaffer-integration-tests/src/test/java/gaffer/integration/impl/GraphFunctionalityIT.java
@@ -23,6 +23,8 @@ import com.google.common.collect.Lists;
 import gaffer.commonutil.TestGroups;
 import gaffer.commonutil.TestPropertyNames;
 import gaffer.data.element.Edge;
+import gaffer.data.element.Entity;
+import gaffer.data.element.IdentifierType;
 import gaffer.data.element.function.ElementFilter;
 import gaffer.data.elementdefinition.view.View;
 import gaffer.data.elementdefinition.view.ViewEdgeDefinition;
@@ -30,14 +32,24 @@ import gaffer.function.simple.filter.IsLessThan;
 import gaffer.integration.GafferIntegrationTests;
 import gaffer.integration.TraitRequirement;
 import gaffer.integration.domain.EdgeDomainObject;
+import gaffer.integration.domain.EntityDomainObject;
 import gaffer.integration.generators.BasicEdgeGenerator;
+import gaffer.integration.generators.BasicEntityGenerator;
 import gaffer.operation.GetOperation;
 import gaffer.operation.OperationChain;
+import gaffer.operation.OperationChain.Builder;
 import gaffer.operation.OperationException;
 import gaffer.operation.data.ElementSeed;
 import gaffer.operation.data.EntitySeed;
+import gaffer.operation.data.generator.EntitySeedExtractor;
 import gaffer.operation.impl.add.AddElements;
+import gaffer.operation.impl.cache.FetchCache;
+import gaffer.operation.impl.cache.FetchCachedResult;
+import gaffer.operation.impl.cache.UpdateCache;
 import gaffer.operation.impl.generate.GenerateElements;
+import gaffer.operation.impl.generate.GenerateObjects;
+import gaffer.operation.impl.get.GetAdjacentEntitySeeds;
+import gaffer.operation.impl.get.GetEntitiesBySeed;
 import gaffer.operation.impl.get.GetRelatedEdges;
 import gaffer.store.StoreTrait;
 import org.hamcrest.core.IsCollectionContaining;
@@ -47,6 +59,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 public class GraphFunctionalityIT extends GafferIntegrationTests {
 
@@ -63,10 +76,18 @@ public class GraphFunctionalityIT extends GafferIntegrationTests {
                 new EdgeDomainObject("C", "B", false, 7)
         ), new BasicEdgeGenerator());
 
-        AddElements addElements = new AddElements();
+        GenerateElements<EntityDomainObject> generateEntities = new GenerateElements<>(Arrays.asList(
+                new EntityDomainObject("A", "propA", 1),
+                new EntityDomainObject("B", "propB", 2),
+                new EntityDomainObject("C", "propC", 3),
+                new EntityDomainObject("D", "propD", 4)
+        ), new BasicEntityGenerator());
+
         OperationChain<Void> opChain = new OperationChain.Builder()
                 .first(generateEdges)
-                .then(addElements)
+                .then(new AddElements())
+                .then(generateEntities)
+                .then(new AddElements())
                 .build();
 
         graph.execute(opChain);
@@ -209,6 +230,171 @@ public class GraphFunctionalityIT extends GafferIntegrationTests {
         assertThat(results, IsCollectionContaining.hasItems(
                 new Edge(TestGroups.EDGE, "B", "D", true),
                 new Edge(TestGroups.EDGE, "B", "C", true)
+        ));
+    }
+
+    @Test
+    public void shouldCacheResults() throws OperationException {
+        // OPERATE
+        final Map<String, Iterable<?>> cachedResults = graph.execute(new Builder()
+                .first(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache())
+                .then(new FetchCache())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        assertEquals(1, cachedResults.size());
+        assertThat(((Iterable<Edge>) cachedResults.get(UpdateCache.ALL)), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "A", "B", true)
+        ));
+    }
+
+
+    @Test
+    public void shouldCacheResultsAndPassInputToOutput() throws OperationException {
+        // OPERATE
+        final Map<String, Iterable<?>> cachedResults = graph.execute(new Builder()
+                .first(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache("1"))
+                .then(new GenerateObjects.Builder<Edge, EntitySeed>()
+                        .generator(new EntitySeedExtractor(IdentifierType.SOURCE))
+                        .build())
+                .then(new UpdateCache("2"))
+                .then(new FetchCache())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        assertEquals(2, cachedResults.size());
+        assertThat(((Iterable<Edge>) cachedResults.get("1")), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "A", "B", true),
+                new Edge(TestGroups.EDGE, "D", "A", true)
+        ));
+        assertThat(((Iterable<EntitySeed>) cachedResults.get("2")), IsCollectionContaining.hasItems(
+                new EntitySeed("A"),
+                new EntitySeed("D")
+        ));
+    }
+
+    @Test
+    public void shouldCacheResultsSeparately() throws OperationException {
+        // OPERATE
+        final Map<String, Iterable<?>> cachedResults = graph.execute(new Builder()
+                .first(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache("1"))
+                .then(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("B"))
+                        .build())
+                .then(new UpdateCache("2"))
+                .then(new FetchCache())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        assertEquals(2, cachedResults.size());
+        assertThat(((Iterable<Edge>) cachedResults.get("1")), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "A", "B", true),
+                new Edge(TestGroups.EDGE, "D", "A", true)
+        ));
+        assertThat(((Iterable<Edge>) cachedResults.get("2")), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "B", "D", true),
+                new Edge(TestGroups.EDGE, "B", "C", true)
+        ));
+    }
+
+    @Test
+    public void shouldCacheResultsTogether() throws OperationException {
+        // OPERATE
+        final Map<String, Iterable<?>> cachedResults = graph.execute(new Builder()
+                .first(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache())
+                .then(new GetRelatedEdges.Builder()
+                        .addSeed(new EntitySeed("B"))
+                        .build())
+                .then(new UpdateCache())
+                .then(new FetchCache())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        assertEquals(1, cachedResults.size());
+        assertThat(((Iterable<Edge>) cachedResults.get(UpdateCache.ALL)), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "A", "B", true),
+                new Edge(TestGroups.EDGE, "D", "A", true),
+                new Edge(TestGroups.EDGE, "B", "D", true),
+                new Edge(TestGroups.EDGE, "B", "C", true)
+        ));
+    }
+
+    @Test
+    public void shouldCacheResultsAndFetchThemInChain() throws OperationException {
+        // OPERATE
+        final Map<String, Iterable<?>> cachedResults = graph.execute(new Builder()
+                .first(new GetAdjacentEntitySeeds.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache("adj-seeds"))
+                .then(new GetEntitiesBySeed())
+                .then(new UpdateCache("entities"))
+                .then(new FetchCachedResult("adj-seeds"))
+                .then(new GetRelatedEdges())
+                .then(new UpdateCache("edges"))
+                .then(new FetchCache())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        assertEquals(3, cachedResults.size());
+        assertThat(((Iterable<EntitySeed>) cachedResults.get("adj-seeds")), IsCollectionContaining.hasItems(
+                new EntitySeed("B"),
+                new EntitySeed("D")
+        ));
+        assertThat(((Iterable<Entity>) cachedResults.get("entities")), IsCollectionContaining.hasItems(
+                new Entity(TestGroups.ENTITY, "B"),
+                new Entity(TestGroups.ENTITY, "D")
+        ));
+        assertThat(((Iterable<Edge>) cachedResults.get("edges")), IsCollectionContaining.hasItems(
+                new Edge(TestGroups.EDGE, "B", "C", true),
+                new Edge(TestGroups.EDGE, "B", "D", true),
+                new Edge(TestGroups.EDGE, "A", "B", true),
+                new Edge(TestGroups.EDGE, "C", "B", true),
+                new Edge(TestGroups.EDGE, "B", "C", true),
+                new Edge(TestGroups.EDGE, "D", "A", true),
+                new Edge(TestGroups.EDGE, "B", "D", true)
+        ));
+    }
+
+    @Test
+    public void shouldCacheResultsTogetherWhilstTraversing() throws OperationException {
+        // OPERATE
+        final Iterable<?> cachedResults = graph.execute(new Builder()
+                .first(new GetAdjacentEntitySeeds.Builder()
+                        .addSeed(new EntitySeed("A"))
+                        .build())
+                .then(new UpdateCache())
+                .then(new GetAdjacentEntitySeeds.Builder()
+                        .build())
+                .then(new UpdateCache())
+                .then(new FetchCachedResult())
+                .build());
+
+        //CHECK
+        assertNotNull(cachedResults);
+        final List<EntitySeed> results = (List) Lists.newArrayList(cachedResults);
+        assertThat(results, IsCollectionContaining.hasItems(
+                new EntitySeed("A"),
+                new EntitySeed("B"),
+                new EntitySeed("C"),
+                new EntitySeed("D")
         ));
     }
 }
