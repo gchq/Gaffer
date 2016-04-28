9bf01fcd411a341b51d6667db4929195e1746016
gchqdev03 <gchqdev03@users.noreply.github.com>
2016-04-28 15:57:23 +0100

    gh-131 - Gaffer REST
    Initial commit of javascript and css files

---
+++ example-rest/src/main/webapp/js/gaffer.js
@@ -0,0 +1,54 @@
+function addExampleButtons(){
+$("#resource_graph\\/doOperation .operation-params").find("td:eq(2)").append("<input type='button' value='Example JSON' onclick='if(loadExample){loadExample(this)}'>");
+
+}
+
+function loadExample(exampleButton){
+    var urlSuffix = $(exampleButton.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement).find(".path").text().trim();
+    // Bug in Gaffer since 0.3.3
+    if ("/graph/doOperation" === urlSuffix){
+        urlSuffix = "/graph/execute";
+    }
+    var exampleUrl = window.location.origin + window.location.pathname + "v1/example" + urlSuffix;
+    var onSuccess = function(response){
+        var json=JSON.stringify(response, null,"   ");
+        $(exampleButton.parentElement.parentElement).find("textarea").val(json);
+    };
+    $.ajax({url: exampleUrl, success: onSuccess});
+}
+
+  window.swaggerUi = new SwaggerUi({
+  url:"/example-rest/v1/api-docs/",
+  dom_id:"swagger-ui-container",
+  supportedSubmitMethods: ['get','post','put','delete'],
+  onComplete: function(swaggerApi, swaggerUi){
+    log("Loaded swagger");
+    $('pre code').each(function(i,e){hljs.highlightBlock(e)});
+    addExampleButtons();
+  },
+  onFailure: function(data){
+  log("Unable to load SwaggerUI");
+  },
+  docExpansion: "none",
+  sorter: "alpha",
+  apisSorter: "alpha",
+  operationsSorter: "alpha"
+});
+$('#input_apiKey').change(function()
+{
+    var key = $('#input_apiKey')[0].value;
+    log("key: " + key);
+    if (key && key.trim() != "")
+	{
+        log("added key " + key);
+        window.authorizations.add("key", new ApiKeyAuthorization("api_key", key, "query"));
+        }
+        })
+
+      window.swaggerUi.load();
+
+      function log() {
+        if ('console' in window) {
+          console.log.apply(console, arguments);
+        }
+      }
