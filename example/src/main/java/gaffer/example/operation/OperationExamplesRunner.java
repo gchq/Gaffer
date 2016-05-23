d81ce296df206f7e85604f7dcf5361e2e89d2753
p013570 <p013570@users.noreply.github.com>
2016-05-23 08:52:39 +0100

    -gh-217 - Added header to operation examples runner

--- example/src/main/java/gaffer/example/operation/OperationExamplesRunner.java
+++ example/src/main/java/gaffer/example/operation/OperationExamplesRunner.java
@@ -39,11 +39,30 @@ public class OperationExamplesRunner {
     }
 
     public void run() throws Exception {
+        printHeader();
         printTableOfContents();
         for (Class<? extends OperationExample> aClass : getSubClasses(OperationExample.class)) {
             aClass.newInstance().run();
             System.out.println(EXAMPLE_DIVIDER);
         }
+    }
+
+    private void printHeader() {
+        System.out.println("Copyright 2016 Crown Copyright\n"
+                + "\n"
+                + "Licensed under the Apache License, Version 2.0 (the \"License\");\n"
+                + "you may not use this file except in compliance with the License.\n"
+                + "You may obtain a copy of the License at\n"
+                + "\n"
+                + "  http://www.apache.org/licenses/LICENSE-2.0\n"
+                + "\n"
+                + "Unless required by applicable law or agreed to in writing, software\n"
+                + "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+                + "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+                + "See the License for the specific language governing permissions and\n"
+                + "limitations under the License.\n"
+                + "\n"
+                + "_This page has been generated from code. To make any changes please updated the operation examples in the [example](https://github.com/GovernmentCommunicationsHeadquarters/Gaffer/tree/master/example/src/main/java/gaffer/example) module, run it and replace the content of this page with the output._\n\n");
     }
 
     private void printTableOfContents() throws InstantiationException, IllegalAccessException {
