e86df9dfdbad6c30770c575c3c043b216b04c543
t616178 <t616178@users.noreply.github.com>
2016-12-05 12:34:17 +0000

    gh-224 - Removed unnecessary property.

--- library/function-library/src/test/java/uk/gov/gchq/gaffer/function/simple/filter/AgeOffFromDaysTest.java
+++ library/function-library/src/test/java/uk/gov/gchq/gaffer/function/simple/filter/AgeOffFromDaysTest.java
@@ -30,70 +30,16 @@ import static org.junit.Assert.assertTrue;
 public class AgeOffFromDaysTest extends FilterFunctionTest {
     public static final int MINUTE_IN_MILLISECONDS = 60000;
     public static final int DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000;
-    public static final int CUSTOM_AGE_OFF_DAYS = 14;
-    public static final long CUSTOM_AGE_OFF_MILLISECONDS = CUSTOM_AGE_OFF_DAYS * DAY_IN_MILLISECONDS;
-
-    @Test
-    public void shouldUseDefaultAgeOffTime() {
-        // Given
-        final AgeOffFromDays filter = new AgeOffFromDays();
-
-        // When
-        final long ageOffTime = filter.getAgeOffTime();
-
-        // Then
-        assertEquals(AgeOffFromDays.AGE_OFF_TIME_DEFAULT, ageOffTime);
-    }
-
-    @Test
-    public void shouldSetAgeOffInWeeks() {
-        // Given
-        final int ageOffInWeeks = 1;
-        final AgeOffFromDays filter = new AgeOffFromDays();
-        filter.setAgeOffWeeks(ageOffInWeeks);
-
-        // When
-        final long ageOffTime = filter.getAgeOffTime();
-
-        // Then
-        assertEquals(ageOffInWeeks * 7, ageOffTime);
-    }
-
-    @Test
-    public void shouldSetAgeOffInHoursUnder() {
-        // Given
-        final int ageOffInHours = 10;
-        final AgeOffFromDays filter = new AgeOffFromDays();
-        filter.setAgeOffHours(ageOffInHours);
-
-        // When
-        final long ageOffTime = filter.getAgeOffTime();
-
-        // Then
-        assertEquals(1, ageOffTime);
-    }
-
-    @Test
-    public void shouldSetAgeOffInHoursOver() {
-        // Given
-        final int ageOffInHours = 25;
-        final AgeOffFromDays filter = new AgeOffFromDays();
-        filter.setAgeOffHours(ageOffInHours);
-
-        // When
-        final long ageOffTime = filter.getAgeOffTime();
-
-        // Then
-        assertEquals(2, ageOffTime);
-    }
+    public static final int AGE_OFF_DAYS = 14;
+    public static final long AGE_OFF_MILLISECONDS = AGE_OFF_DAYS * DAY_IN_MILLISECONDS;
 
     @Test
     public void shouldAcceptWhenWithinAgeOffLimit() {
         // Given
-        final AgeOffFromDays filter = new AgeOffFromDays(CUSTOM_AGE_OFF_DAYS);
+        final AgeOffFromDays filter = new AgeOffFromDays();
 
         // When
-        final Object[] input = new Object[]{System.currentTimeMillis() - CUSTOM_AGE_OFF_MILLISECONDS + MINUTE_IN_MILLISECONDS, CUSTOM_AGE_OFF_DAYS};
+        final Object[] input = new Object[]{System.currentTimeMillis() - AGE_OFF_MILLISECONDS + MINUTE_IN_MILLISECONDS, AGE_OFF_DAYS};
         final boolean accepted = filter.isValid(input);
 
         // Then
@@ -103,10 +49,10 @@ public class AgeOffFromDaysTest extends FilterFunctionTest {
     @Test
     public void shouldAcceptWhenOutsideAgeOffLimit() {
         // Given
-        final AgeOffFromDays filter = new AgeOffFromDays(CUSTOM_AGE_OFF_DAYS);
+        final AgeOffFromDays filter = new AgeOffFromDays();
 
         // When
-        final Object[] input = new Object[]{System.currentTimeMillis() - CUSTOM_AGE_OFF_MILLISECONDS - DAY_IN_MILLISECONDS, CUSTOM_AGE_OFF_DAYS};
+        final Object[] input = new Object[]{System.currentTimeMillis() - AGE_OFF_MILLISECONDS - DAY_IN_MILLISECONDS, AGE_OFF_DAYS};
         final boolean accepted = filter.isValid(input);
 
         // Then
@@ -116,28 +62,26 @@ public class AgeOffFromDaysTest extends FilterFunctionTest {
     @Test
     public void shouldClone() {
         // Given
-        final AgeOffFromDays filter = new AgeOffFromDays(CUSTOM_AGE_OFF_DAYS);
+        final AgeOffFromDays filter = new AgeOffFromDays();
 
         // When
         final AgeOffFromDays clonedFilter = filter.statelessClone();
 
         // Then
         assertNotSame(filter, clonedFilter);
-        assertNotSame(CUSTOM_AGE_OFF_DAYS, clonedFilter.getAgeOffTime());
     }
 
     @Test
     public void shouldJsonSerialiseAndDeserialise() throws SerialisationException {
         // Given
-        final AgeOffFromDays filter = new AgeOffFromDays(CUSTOM_AGE_OFF_DAYS);
+        final AgeOffFromDays filter = new AgeOffFromDays();
 
         // When
         final String json = new String(new JSONSerialiser().serialise(filter, true));
 
         // Then
         JsonUtil.assertEquals(String.format("{%n" +
-                "  \"class\" : \"uk.gov.gchq.gaffer.function.simple.filter.AgeOffFromDays\",%n" +
-                "  \"ageOffTime\" : 14%n" +
+                "  \"class\" : \"uk.gov.gchq.gaffer.function.simple.filter.AgeOffFromDays\"%n" +
                 "}"), json);
 
         // When 2
@@ -146,7 +90,6 @@ public class AgeOffFromDaysTest extends FilterFunctionTest {
 
         // Then 2
         assertNotNull(deserialisedFilter);
-        assertEquals(CUSTOM_AGE_OFF_DAYS, deserialisedFilter.getAgeOffTime());
     }
 
     @Override
