40c4fcdfe01353279ce79564328714f0492a70e3
t616178 <t616178@users.noreply.github.com>
2016-12-14 16:39:16 +0000

    gh-545 - Add guards to ensure that the JARs are only deployed from the master branch

--- cd/before_deploy.sh
+++ cd/before_deploy.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-#  openssl aes-256-cbc -K $encrypted_de949738249f_key -iv $encrypted_de949738249f_iv -in codesigning.asc.enc -out codesigning.asc -d
-#  gpg --fast-import cd/codesigning.asc
-#fi
-
-# Avoid check for testing purposes
-openssl aes-256-cbc -K $encrypted_de949738249f_key -iv $encrypted_de949738249f_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-gpg --fast-import cd/codesigning.asc
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_de949738249f_key -iv $encrypted_de949738249f_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    gpg --fast-import cd/codesigning.asc
+fi
--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-#    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
-#fi
-
-# Avoid check for testing purposes
-mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    mvn deploy -P sign,build-extras --settings cd/mvnsettings.xml
+fi
