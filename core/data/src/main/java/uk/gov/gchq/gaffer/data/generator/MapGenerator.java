50a50c3c908700cded16548c08e4289c34a75f9f
p013570 <p013570@users.noreply.github.com>
2017-02-20 11:28:49 +0000

    gh-696 - added tests

--- core/data/src/main/java/uk/gov/gchq/gaffer/data/generator/MapGenerator.java
+++ core/data/src/main/java/uk/gov/gchq/gaffer/data/generator/MapGenerator.java
@@ -23,8 +23,8 @@ import java.util.Map;
 
 public class MapGenerator extends OneToOneElementGenerator<Map<String, Object>> {
     public static final String GROUP = "GROUP";
-    private LinkedHashMap<String, String> fields;
-    private LinkedHashMap<String, String> constants;
+    private LinkedHashMap<String, String> fields = new LinkedHashMap<>();
+    private LinkedHashMap<String, String> constants = new LinkedHashMap<>();
 
     @Override
     public Element getElement(final Map<String, Object> map) {
@@ -35,16 +35,10 @@ public class MapGenerator extends OneToOneElementGenerator<Map<String, Object>> 
     public Map<String, Object> getObject(final Element element) {
         final Map<String, Object> map = new LinkedHashMap<>(fields.size() + constants.size());
         for (final Map.Entry<String, String> entry : fields.entrySet()) {
-            final String key = entry.getKey();
-            final IdentifierType idType = IdentifierType.fromName(key);
-            if (null == idType) {
-                if (GROUP.equals(key)) {
-                    map.put(entry.getValue(), element.getGroup());
-                } else {
-                    map.put(entry.getValue(), element.getProperty(key));
-                }
+            final Object value = getFieldValue(element, entry.getKey());
+            if (null != value) {
+                map.put(entry.getValue(), value);
             }
-            map.put(entry.getValue(), element.getIdentifier(idType));
         }
 
         map.putAll(constants);
@@ -52,11 +46,29 @@ public class MapGenerator extends OneToOneElementGenerator<Map<String, Object>> 
         return map;
     }
 
+    private Object getFieldValue(final Element element, final String key) {
+        final IdentifierType idType = IdentifierType.fromName(key);
+        final Object value;
+        if (null == idType) {
+            if (GROUP.equals(key)) {
+                value = element.getGroup();
+            } else {
+                value = element.getProperty(key);
+            }
+        } else {
+            value = element.getIdentifier(idType);
+        }
+        return value;
+    }
+
     public LinkedHashMap<String, String> getFields() {
         return fields;
     }
 
     public void setFields(final LinkedHashMap<String, String> fields) {
+        if (null == fields) {
+            this.fields = new LinkedHashMap<>();
+        }
         this.fields = fields;
     }
 
@@ -65,6 +77,58 @@ public class MapGenerator extends OneToOneElementGenerator<Map<String, Object>> 
     }
 
     public void setConstants(final LinkedHashMap<String, String> constants) {
+        if (null == constants) {
+            this.constants = new LinkedHashMap<>();
+        }
         this.constants = constants;
+    }
+
+    public static class Builder {
+        private LinkedHashMap<String, String> fields = new LinkedHashMap<>();
+        private LinkedHashMap<String, String> constants = new LinkedHashMap<>();
+
+        public Builder group(final String mapKey) {
+            fields.put(GROUP, mapKey);
+            return this;
+        }
+
+        public Builder property(final String propertyName, final String mapKey) {
+            fields.put(propertyName, mapKey);
+            return this;
+        }
+
+        public Builder vertex(final String mapKey) {
+            return identifier(IdentifierType.VERTEX, mapKey);
+        }
+
+        public Builder source(final String mapKey) {
+            return identifier(IdentifierType.SOURCE, mapKey);
+        }
+
+        public Builder destination(final String mapKey) {
+            return identifier(IdentifierType.DESTINATION, mapKey);
+        }
+
+        public Builder direction(final String mapKey) {
+            return identifier(IdentifierType.DIRECTED, mapKey);
+        }
+
+        public Builder identifier(final IdentifierType identifierType, final String mapKey) {
+            fields.put(identifierType.name(), mapKey);
+            return this;
+        }
+
+        public Builder constant(final String key, final String value) {
+            constants.put(key, value);
+            return this;
+        }
+
+        public MapGenerator build() {
+            final MapGenerator generator = new MapGenerator();
+            generator.setFields(fields);
+            generator.setConstants(constants);
+
+            return generator;
+        }
     }
 }
