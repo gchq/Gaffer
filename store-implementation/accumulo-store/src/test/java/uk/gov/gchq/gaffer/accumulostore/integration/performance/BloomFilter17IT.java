4d35350d12f0cf6accd3ae34b8c2e4f23e042852
t616178 <t616178@users.noreply.github.com>
2017-03-14 14:29:15 +0000

    gh-449 - Added Accumulo 1.7.x and 1.8.x unit tests, as well as profiles for specifying the Accumulo version at compile time (defaults to 1.7.2)

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/integration/performance/BloomFilter17IT.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/integration/performance/BloomFilter17IT.java
@@ -77,8 +77,8 @@ import static org.junit.Assert.assertTrue;
  * than looking up data that is present.
  * This class is based on Accumulo's BloomFilterLayerLookupTest (org.apache.accumulo.core.file.BloomFilterLayerLookupTest).
  */
-public class BloomFilterIT {
-    private static final Logger LOGGER = LoggerFactory.getLogger(BloomFilterIT.class);
+public class BloomFilter17IT {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BloomFilter17IT.class);
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder(CommonTestConstants.TMP_DIRECTORY);
     private RangeFactory byteEntityRangeFactory;
