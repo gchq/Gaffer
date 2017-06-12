85653931581f86051d33bd3598488674d010bb62
m55624 <m55624@users.noreply.github.com>
2017-06-12 11:36:11 +0100

    GH-874 - removed unused optimiseSchema method

--- core/store/src/main/java/uk/gov/gchq/gaffer/store/Store.java
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/Store.java
@@ -374,10 +374,6 @@ public abstract class Store {
         schema = schemaOptimiser.optimise(schema, hasTrait(StoreTrait.ORDERED));
     }
 
-    public void optimiseSchema(final SchemaOptimiser optimiser) {
-        schema = optimiser.optimise(schema, hasTrait(StoreTrait.ORDERED));
-    }
-
     public void validateSchemas() {
         final ValidationResult validationResult = new ValidationResult();
         if (null == schema) {
