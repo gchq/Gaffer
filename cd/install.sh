24e0a0a19291fae239031785c2e5e98d83dd30ae
p013570 <p013570@users.noreply.github.com>
2017-06-13 08:18:19 +0100

    gh-882 - fixed install travis script

--- cd/install.sh
+++ cd/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$MODULES" == '' ]; then
+if [ "$MODULES" == '' ] || [[ $MODULES == *'!'* ]]; then
     mvn -q install -P quick,travis,build-extras -B -V
 else
     mvn -q install -P quick,travis,build-extras -B -V -pl $MODULES -am
