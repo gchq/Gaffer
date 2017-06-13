e0bb2c63e66b7790a22865109ca877934fb1a75d
p013570 <p013570@users.noreply.github.com>
2017-06-13 08:30:39 +0100

    gh-882 - added echo statements to travis build

--- cd/verify.sh
+++ cd/verify.sh
@@ -2,14 +2,18 @@
 
 if [ "$MODULES" == '' ]; then
     if [ "$PROFILE" == '' ]; then
+        echo "Running verify script: mvn -q verify -P travis -B"
         mvn -q verify -P travis -B
     else
+        echo "Running verify script: mvn -q verify -P travis -B -P $PROFILE"
         mvn -q verify -P travis -B -P $PROFILE
     fi
 else
     if [ "$PROFILE" == '' ]; then
+        echo "Running verify script: mvn -q verify -P travis -B -pl $MODULES"
         mvn -q verify -P travis -B -pl $MODULES
     else
+        echo "Running verify script: mvn -q verify -P travis -B -P $PROFILE -pl $MODULES"
         mvn -q verify -P travis -B -P $PROFILE -pl $MODULES
     fi
 fi
