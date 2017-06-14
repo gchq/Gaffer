d83c93290b006e40052922433981d01bb3b51376
m55624 <m55624@users.noreply.github.com>
2017-06-14 12:55:21 +0100

    GH-515 - Readded and fixed aggregation test

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/integration/AccumuloAggregationIT.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/integration/AccumuloAggregationIT.java
@@ -21,10 +21,10 @@ import org.junit.Test;
 import uk.gov.gchq.gaffer.accumulostore.utils.AccumuloPropertyNames;
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
 import uk.gov.gchq.gaffer.commonutil.TestGroups;
+import uk.gov.gchq.gaffer.commonutil.TestPropertyNames;
 import uk.gov.gchq.gaffer.commonutil.TestTypes;
 import uk.gov.gchq.gaffer.data.element.Element;
 import uk.gov.gchq.gaffer.data.element.Entity;
-import uk.gov.gchq.gaffer.data.elementdefinition.exception.SchemaException;
 import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
 import uk.gov.gchq.gaffer.data.elementdefinition.view.ViewElementDefinition;
 import uk.gov.gchq.gaffer.graph.Graph;
@@ -32,6 +32,7 @@ import uk.gov.gchq.gaffer.graph.Graph.Builder;
 import uk.gov.gchq.gaffer.operation.OperationException;
 import uk.gov.gchq.gaffer.operation.data.EntitySeed;
 import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
 import uk.gov.gchq.gaffer.serialisation.implementation.StringSerialiser;
 import uk.gov.gchq.gaffer.store.StoreProperties;
@@ -464,35 +465,132 @@ public class AccumuloAggregationIT {
     }
 
     @Test
-    public void shouldHandleAggregatationWhenNoAggregatorsAreProvided() {
-        try {
-            new Builder()
-                    .storeProperties(STORE_PROPERTIES)
-                    .addSchema(new Schema.Builder()
-                            .type(TestTypes.ID_STRING, new TypeDefinition.Builder()
-                                    .clazz(String.class)
-                                    .build())
-                            .type("colQual", new TypeDefinition.Builder()
-                                    .clazz(String.class)
-                                    .serialiser(new StringSerialiser())
-                                    .build())
-                            .entity(TestGroups.ENTITY, new SchemaEntityDefinition.Builder()
-                                    .vertex(TestTypes.ID_STRING)
-                                    .property(AccumuloPropertyNames.COLUMN_QUALIFIER, "colQual")
-                                    .property(AccumuloPropertyNames.COLUMN_QUALIFIER_2, "colQual")
-                                    .property(AccumuloPropertyNames.COLUMN_QUALIFIER_3, "colQual")
-                                    .property(AccumuloPropertyNames.COLUMN_QUALIFIER_4, "colQual")
-                                    .groupBy(AccumuloPropertyNames.COLUMN_QUALIFIER,
-                                            AccumuloPropertyNames.COLUMN_QUALIFIER_2,
-                                            AccumuloPropertyNames.COLUMN_QUALIFIER_3,
-                                            AccumuloPropertyNames.COLUMN_QUALIFIER_4)
-                                    .aggregate(false)
-                                    .build())
-                            .build())
-                    .build();
-        } catch (SchemaException e) {
-            assertNotNull(e.getMessage());
-        }
+    public void shouldHandleAggregatationWhenNoAggregatorsAreProvided() throws OperationException {
+
+        final Graph graph = createGraphNoAggregators();
+        final Entity entity1 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+        final Entity entity2 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, null)
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+        final Entity entity3 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1a")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+        final Entity entity4 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1b")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+        final Entity entity5 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1a")
+                .build();
+        final Entity entity6 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1b")
+                .build();
+        final Entity entity7 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_2, "test2a")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .build();
+
+        graph.execute(new AddElements.Builder().input(
+                entity1,
+                entity2,
+                entity3,
+                entity4,
+                entity5,
+                entity6,
+                entity7
+        ).build(), USER);
+
+        // Duplicate the entities to check they are not aggregated
+        graph.execute(new AddElements.Builder()
+                        .input(entity1,
+                                entity2,
+                                entity3,
+                                entity4,
+                                entity5,
+                                entity6,
+                                entity7)
+                        .build(),
+                USER);
+
+        // Given
+        final GetAllElements getAllEntities = new GetAllElements.Builder()
+                .view(new View.Builder()
+                        .entity(TestGroups.ENTITY)
+                        .build())
+                .build();
+
+        // When
+        final List<Element> results = Lists.newArrayList(graph.execute(getAllEntities, USER));
+
+        // Then
+        assertNotNull(results);
+        assertEquals(14, results.size());
+
+        final Entity expectedEntity1 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "")
+                .property(TestPropertyNames.PROP_2, "")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+
+        final Entity expectedEntity2 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1a")
+                .property(TestPropertyNames.PROP_2, "")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+
+        final Entity expectedEntity3 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "test1b")
+                .property(TestPropertyNames.PROP_2, "")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "test 4")
+                .build();
+
+        final Entity expectedEntity4 = new Entity.Builder()
+                .vertex(VERTEX)
+                .group(TestGroups.ENTITY)
+                .property(TestPropertyNames.PROP_1, "")
+                .property(TestPropertyNames.PROP_2, "test2a")
+                .property(TestPropertyNames.PROP_3, "test 3")
+                .property(TestPropertyNames.PROP_4, "")
+                .build();
+
+        assertThat(results, IsCollectionContaining.hasItems(
+                expectedEntity1,
+                expectedEntity2,
+                expectedEntity3,
+                expectedEntity4
+        ));
+
     }
 
     protected Graph createGraph() {
@@ -551,6 +649,29 @@ public class AccumuloAggregationIT {
                                         AccumuloPropertyNames.COLUMN_QUALIFIER_2,
                                         AccumuloPropertyNames.COLUMN_QUALIFIER_3,
                                         AccumuloPropertyNames.COLUMN_QUALIFIER_4)
+                                .build())
+                        .build())
+                .build();
+    }
+
+    protected Graph createGraphNoAggregators() {
+        return new Builder()
+                .storeProperties(STORE_PROPERTIES)
+                .addSchema(new Schema.Builder()
+                        .type(TestTypes.ID_STRING, new TypeDefinition.Builder()
+                                .clazz(String.class)
+                                .build())
+                        .type("colQual", new TypeDefinition.Builder()
+                                .clazz(String.class)
+                                .serialiser(new StringSerialiser())
+                                .build())
+                        .entity(TestGroups.ENTITY, new SchemaEntityDefinition.Builder()
+                                .vertex(TestTypes.ID_STRING)
+                                .property(TestPropertyNames.PROP_1, "colQual")
+                                .property(TestPropertyNames.PROP_2, "colQual")
+                                .property(TestPropertyNames.PROP_3, "colQual")
+                                .property(TestPropertyNames.PROP_4, "colQual")
+                                .aggregate(false)
                                 .build())
                         .build())
                 .build();
