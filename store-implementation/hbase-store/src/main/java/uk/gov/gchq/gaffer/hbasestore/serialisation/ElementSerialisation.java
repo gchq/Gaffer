ffecd19c53a1f72d9c62487b4be2a649d87d12b8
GCHQDeveloper404 <GCHQDeveloper404@users.noreply.github.com>
2017-06-15 15:36:55 +0100

    gh-855 Reduce byte[] copying
    removed System.arraycopy() from (Hbase-store) ElementSerialisation.

--- store-implementation/hbase-store/src/main/java/uk/gov/gchq/gaffer/hbasestore/serialisation/ElementSerialisation.java
+++ store-implementation/hbase-store/src/main/java/uk/gov/gchq/gaffer/hbasestore/serialisation/ElementSerialisation.java
@@ -100,7 +100,6 @@ public class ElementSerialisation {
         }
         int lastDelimiter = 0;
         final int arrayLength = value.length;
-        long currentPropLength;
         final SchemaElementDefinition elementDefinition = schema.getElement(group);
         if (null == elementDefinition) {
             throw new SerialisationException("No SchemaElementDefinition found for group " + group + ", is this group in your schema or do your table iterators need updating?");
@@ -113,17 +112,18 @@ public class ElementSerialisation {
                 final ToBytesSerialiser serialiser = (typeDefinition != null) ? (ToBytesSerialiser) typeDefinition.getSerialiser() : null;
                 if (null != serialiser) {
                     final int numBytesForLength = CompactRawSerialisationUtils.decodeVIntSize(value[lastDelimiter]);
-                    final byte[] length = new byte[numBytesForLength];
-                    System.arraycopy(value, lastDelimiter, length, 0, numBytesForLength);
+                    int currentPropLength;
                     try {
-                        currentPropLength = CompactRawSerialisationUtils.readLong(length);
+                        // value is never larger than int.
+                        currentPropLength = (int) CompactRawSerialisationUtils.readLong(value, lastDelimiter);
                     } catch (final SerialisationException e) {
                         throw new SerialisationException("Exception reading length of property");
                     }
                     lastDelimiter += numBytesForLength;
                     if (currentPropLength > 0) {
                         try {
-                            properties.put(propertyName, serialiser.deserialise(Arrays.copyOfRange(value, lastDelimiter, lastDelimiter += currentPropLength)));
+                            properties.put(propertyName, serialiser.deserialise(value, lastDelimiter, currentPropLength));
+                            lastDelimiter += currentPropLength;
                         } catch (final SerialisationException e) {
                             throw new SerialisationException("Failed to deserialise property " + propertyName, e);
                         }
@@ -249,28 +249,28 @@ public class ElementSerialisation {
             return properties;
         }
 
-        int lastDelimiter = CompactRawSerialisationUtils.decodeVIntSize(bytes[0]) + Bytes.toBytes(group).length;
+        int carriage = CompactRawSerialisationUtils.decodeVIntSize(bytes[0]) + Bytes.toBytes(group).length;
         final int arrayLength = bytes.length;
-        long currentPropLength;
 
         final Iterator<String> propertyNames = elementDefinition.getGroupBy().iterator();
-        while (propertyNames.hasNext() && lastDelimiter < arrayLength) {
+        while (propertyNames.hasNext() && carriage < arrayLength) {
             final String propertyName = propertyNames.next();
             final TypeDefinition typeDefinition = elementDefinition.getPropertyTypeDef(propertyName);
             final ToBytesSerialiser serialiser = (typeDefinition != null) ? (ToBytesSerialiser) typeDefinition.getSerialiser() : null;
             if (null != serialiser) {
-                final int numBytesForLength = CompactRawSerialisationUtils.decodeVIntSize(bytes[lastDelimiter]);
-                final byte[] length = new byte[numBytesForLength];
-                System.arraycopy(bytes, lastDelimiter, length, 0, numBytesForLength);
+                final int numBytesForLength = CompactRawSerialisationUtils.decodeVIntSize(bytes[carriage]);
+                int currentPropLength;
                 try {
-                    currentPropLength = CompactRawSerialisationUtils.readLong(length);
+                    //this value is never greater than int.
+                    currentPropLength = (int) CompactRawSerialisationUtils.readLong(bytes, carriage);
                 } catch (final SerialisationException e) {
                     throw new SerialisationException("Exception reading length of property");
                 }
-                lastDelimiter += numBytesForLength;
+                carriage += numBytesForLength;
                 if (currentPropLength > 0) {
                     try {
-                        properties.put(propertyName, serialiser.deserialise(Arrays.copyOfRange(bytes, lastDelimiter, lastDelimiter += currentPropLength)));
+                        properties.put(propertyName, serialiser.deserialise(bytes, carriage, currentPropLength));
+                        carriage += currentPropLength;
                     } catch (final SerialisationException e) {
                         throw new SerialisationException("Failed to deserialise property " + propertyName, e);
                     }
@@ -393,17 +393,13 @@ public class ElementSerialisation {
     }
 
     public String getGroup(final byte[] columnQualifier) throws SerialisationException {
-        final int numBytesForLength = CompactRawSerialisationUtils.decodeVIntSize(columnQualifier[0]);
-        final byte[] length = new byte[numBytesForLength];
-        System.arraycopy(columnQualifier, 0, length, 0, numBytesForLength);
-        int currentPropLength;
         try {
-            currentPropLength = (int) CompactRawSerialisationUtils.readLong(length);
+            final int numBytesForLength = CompactRawSerialisationUtils.decodeVIntSize(columnQualifier[0]);
+            int currentPropLength = (int) CompactRawSerialisationUtils.readLong(columnQualifier, 0);
+            return new String(columnQualifier, numBytesForLength, currentPropLength);
         } catch (final SerialisationException e) {
             throw new SerialisationException("Exception reading length of property");
         }
-
-        return Bytes.toString(Arrays.copyOfRange(columnQualifier, numBytesForLength, numBytesForLength + currentPropLength));
     }
 
     @SuppressFBWarnings(value = "BC_UNCONFIRMED_CAST", justification = "If an element is not an Entity it must be an Edge")
