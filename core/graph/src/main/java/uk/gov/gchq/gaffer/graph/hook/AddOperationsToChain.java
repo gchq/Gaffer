cb7b0cea5e82690f1210004362d1d4aa397d36c7
m55624 <m55624@users.noreply.github.com>
2017-07-12 17:43:49 +0100

    gh-1063 - added AddOperationsToChain

---
+++ core/graph/src/main/java/uk/gov/gchq/gaffer/graph/hook/AddOperationsToChain.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.graph.hook;
+
+import uk.gov.gchq.gaffer.commonutil.StreamUtil;
+import uk.gov.gchq.gaffer.data.elementdefinition.exception.SchemaException;
+import uk.gov.gchq.gaffer.exception.SerialisationException;
+import uk.gov.gchq.gaffer.jsonserialisation.JSONSerialiser;
+import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.OperationChain;
+import uk.gov.gchq.gaffer.store.operationdeclaration.OperationDeclarations;
+import uk.gov.gchq.gaffer.user.User;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class AddOperationsToChain implements GraphHook {
+    private static final JSONSerialiser JSON_SERIALISER = new JSONSerialiser();
+
+    private List<Operation> start;
+    private List<Operation> end;
+    private Map<String, List<Operation>> before;
+    private Map<String, List<Operation>> after;
+
+    public AddOperationsToChain() {
+    }
+
+    public AddOperationsToChain(final String addOperationsPath) throws IOException {
+
+        Path path = Paths.get(addOperationsPath);
+        AddOperationsToChain addOperations;
+        if (path.toFile().exists()) {
+            addOperations = fromJson(Files.readAllBytes(path));
+        } else {
+            addOperations = fromJson(StreamUtil.openStream(OperationDeclarations.class, addOperationsPath));
+        }
+        if (addOperations.getStart() != null) {
+            this.setStart(addOperations.getStart());
+        }
+        if (addOperations.getEnd() != null) {
+            this.setEnd(addOperations.getEnd());
+        }
+        if (addOperations.getBefore() != null) {
+            this.setBefore(addOperations.getBefore());
+        }
+        if (addOperations.getAfter() != null) {
+            this.setAfter(addOperations.getAfter());
+        }
+    }
+
+    public List<Operation> getStart() {
+        return start;
+    }
+
+    public void setStart(final List<Operation> start) {
+        this.start = start;
+    }
+
+    public List<Operation> getEnd() {
+        return end;
+    }
+
+    public void setEnd(final List<Operation> end) {
+        this.end = end;
+    }
+
+    public Map<String, List<Operation>> getBefore() {
+        return before;
+    }
+
+    public void setBefore(final Map<String, List<Operation>> before) {
+        this.before = before;
+    }
+
+    public Map<String, List<Operation>> getAfter() {
+        return after;
+    }
+
+    public void setAfter(final Map<String, List<Operation>> after) {
+        this.after = after;
+    }
+
+    @Override
+    public void preExecute(final OperationChain<?> opChain, final User user) {
+        OperationChain<?> newOpChain = new OperationChain<>();
+
+        if (start != null) {
+            newOpChain.getOperations().addAll(this.getStart());
+        }
+        if (opChain != null) {
+            for (Operation originalOp : opChain.getOperations()) {
+
+                for (String beforeOpKey : before.keySet()) {
+                    if (originalOp.getClass().getName().contains(beforeOpKey)) {
+                        newOpChain.getOperations().addAll(before.get(beforeOpKey));
+                    }
+                    newOpChain.getOperations().add(originalOp);
+                }
+
+                for (String afterOpKey : after.keySet()) {
+                    if (originalOp.getClass().getName().contains(afterOpKey)) {
+                        newOpChain.getOperations().addAll(before.get(afterOpKey));
+                    }
+                }
+            }
+        }
+        if (end != null) {
+            newOpChain.getOperations().addAll(end);
+        }
+        opChain.getOperations().clear();
+        opChain.getOperations().addAll(newOpChain.getOperations());
+    }
+
+    @Override
+    public <T> T postExecute(final T result,
+                             final OperationChain<?> opChain, final User user) {
+        return result;
+    }
+
+    public static AddOperationsToChain fromJson(final byte[] json) {
+        try {
+            return JSON_SERIALISER.deserialise(json, AddOperationsToChain.class);
+        } catch (final SerialisationException e) {
+            throw new SchemaException("Failed to load element definitions from bytes", e);
+        }
+    }
+
+    public static AddOperationsToChain fromJson(final InputStream inputStream) {
+        try {
+            return JSON_SERIALISER.deserialise(inputStream, AddOperationsToChain.class);
+        } catch (final SerialisationException e) {
+            throw new SchemaException("Failed to load element definitions from bytes", e);
+        }
+    }
+
+}
---
+++ core/graph/src/test/java/uk/gov/gchq/gaffer/graph/hook/AddOperationsToChainTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.graph.hook;
+
+import org.junit.Test;
+import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.OperationChain;
+import uk.gov.gchq.gaffer.operation.impl.Count;
+import uk.gov.gchq.gaffer.operation.impl.CountGroups;
+import uk.gov.gchq.gaffer.operation.impl.DiscardOutput;
+import uk.gov.gchq.gaffer.operation.impl.Limit;
+import uk.gov.gchq.gaffer.operation.impl.SplitStore;
+import uk.gov.gchq.gaffer.operation.impl.Validate;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
+import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class AddOperationsToChainTest {
+    public static final String ADD_OPERATIONS_TO_CHAIN_PATH = "src/test/resources/addOperationsToChain.json";
+
+    @Test
+    public void shouldAddOperations() throws IOException {
+        AddOperationsToChain addOperationsToChain = new AddOperationsToChain(ADD_OPERATIONS_TO_CHAIN_PATH);
+    }
+
+    @Test
+    public void shouldAddBeforeOperation() throws IOException {
+        AddOperationsToChain addOperationsToChain = new AddOperationsToChain(ADD_OPERATIONS_TO_CHAIN_PATH);
+
+        Operation discardOutput = mock(DiscardOutput.class);
+        Operation splitStore = mock(SplitStore.class);
+        Operation validate = mock(Validate.class);
+        Operation getAdjacentIds = mock(GetAdjacentIds.class);
+        Operation count = mock(Count.class);
+        Operation countGroups = mock(CountGroups.class);
+        Operation getElements = mock(GetElements.class);
+        Operation getAllElements = mock(GetAllElements.class);
+        Operation limit = mock(Limit.class);
+
+
+        final List expectedOperations = new ArrayList<Operation>();
+        expectedOperations.add(discardOutput);
+        expectedOperations.add(splitStore);
+        expectedOperations.add(validate);
+        expectedOperations.add(getAdjacentIds);
+        expectedOperations.add(count);
+        expectedOperations.add(discardOutput);
+        expectedOperations.add(countGroups);
+        expectedOperations.add(getElements);
+        expectedOperations.add(getAllElements);
+        expectedOperations.add(limit);
+        expectedOperations.add(validate);
+        expectedOperations.add(count);
+
+        final OperationChain opChain = new OperationChain.Builder()
+                .first(getAdjacentIds)
+                .then(getElements)
+                .then(getAllElements)
+                .build();
+
+        addOperationsToChain.preExecute(opChain, null);
+
+        for (int i = 0; i > expectedOperations.size(); i++) {
+            assertEquals(expectedOperations.get(i).getClass().getName(), opChain.getOperations().get(i).getClass().getName());
+        }
+    }
+}
