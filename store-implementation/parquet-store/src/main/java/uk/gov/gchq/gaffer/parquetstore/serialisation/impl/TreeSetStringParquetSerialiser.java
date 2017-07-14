784d2435fbb206ec48310dfd8463d207758db906
ac74475 <ac74475@users.noreply.github.com>
2017-07-14 09:42:36 +0100

    gh-923 Added new serialisers for TreeSet<String> and FreqMap objects

---
+++ store-implementation/parquet-store/src/main/java/uk/gov/gchq/gaffer/parquetstore/serialisation/impl/TreeSetStringParquetSerialiser.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2017. Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.gchq.gaffer.parquetstore.serialisation.impl;
+
+import uk.gov.gchq.gaffer.exception.SerialisationException;
+import uk.gov.gchq.gaffer.parquetstore.serialisation.ParquetSerialiser;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class TreeSetStringParquetSerialiser implements ParquetSerialiser<TreeSet<String>> {
+
+    private static final long serialVersionUID = -8284005451029455563L;
+
+    @Override
+    public String getParquetSchema(final String colName) {
+        return "optional group " + colName + " (LIST) {\n" +
+                "  repeated binary element (UTF8);\n" +
+                "}";
+    }
+
+    @Override
+    public Object[] serialise(final TreeSet<String> object) throws SerialisationException {
+        if (object != null) {
+            final String[] objects = new String[object.size()];
+            object.toArray(objects);
+            return objects;
+        }
+        return new Comparable[0];
+    }
+
+    @Override
+    public TreeSet<String> deserialise(final Object[] objects) throws SerialisationException {
+        if (objects.length == 1 && objects[0] instanceof String[]) {
+            final TreeSet<String> treeSet = new TreeSet<>();
+            treeSet.addAll(Arrays.asList(((String[]) objects)));
+            return treeSet;
+        }
+        return null;
+    }
+
+    @Override
+    public TreeSet<String> deserialiseEmpty() throws SerialisationException {
+        return null;
+    }
+
+    @Override
+    public boolean preservesObjectOrdering() {
+        return true;
+    }
+
+    @Override
+    public Object[] serialiseNull() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean canHandle(final Class clazz) {
+        return TreeSet.class.isAssignableFrom(clazz);
+    }
+}
