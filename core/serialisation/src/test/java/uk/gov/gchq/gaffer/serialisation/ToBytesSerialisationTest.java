203600d45ffd9f70f4c344900d95102a8631904d
t616178 <t616178@users.noreply.github.com>
2017-07-20 13:20:57 +0100

    .

--- core/serialisation/src/test/java/uk/gov/gchq/gaffer/serialisation/ToBytesSerialisationTest.java
+++ core/serialisation/src/test/java/uk/gov/gchq/gaffer/serialisation/ToBytesSerialisationTest.java
@@ -44,6 +44,6 @@ public abstract class ToBytesSerialisationTest<T> extends SerialisationTest<T, b
     @Override
     protected void serialiseFirst(final Pair<T, byte[]> pair) throws SerialisationException {
         byte[] serialise = serialiser.serialise(pair.getFirst());
-        assertArrayEquals(Arrays.toString(serialise),pair.getSecond(), serialise);
+        assertArrayEquals(serialise, pair.getSecond());
     }
 }
