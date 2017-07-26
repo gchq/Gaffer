55e8ad850fe1d223a036c3c71d1c2fc35fb76ec2
t616178 <t616178@users.noreply.github.com>
2017-07-26 14:43:21 +0100

    gh-xxx - Speed up Travis

--- rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/DisableOperationsTest.java
+++ rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/DisableOperationsTest.java
@@ -23,8 +23,12 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import uk.gov.gchq.gaffer.commonutil.CommonTestConstants;
 import uk.gov.gchq.gaffer.graph.Graph;
+import uk.gov.gchq.gaffer.mapstore.operation.CountAllElementsDefaultView;
 import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.impl.Count;
+import uk.gov.gchq.gaffer.operation.impl.CountGroups;
 import uk.gov.gchq.gaffer.operation.impl.SplitStore;
+import uk.gov.gchq.gaffer.operation.impl.add.AddElementsFromFile;
 import uk.gov.gchq.gaffer.rest.factory.DefaultGraphFactory;
 import java.io.File;
 import java.io.IOException;
@@ -33,16 +37,16 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
-public class DisableOperationsTest {
+public abstract class DisableOperationsTest {
     @Rule
     public final TemporaryFolder tempFolder = new TemporaryFolder(CommonTestConstants.TMP_DIRECTORY);
-    private final Class<? extends Operation>[] disabledOperations;
-    private File storePropsPath;
-    private File schemaPath;
-    private static final String GRAPH_ID = "graphId";
+    protected final Class<? extends Operation>[] disabledOperations;
+    protected File storePropsPath;
+    protected File schemaPath;
+    protected static final String GRAPH_ID = "graphId";
 
     public DisableOperationsTest() throws IOException {
-        this(SplitStore.class);
+        this(AddElementsFromFile.class);
     }
 
     @SafeVarargs
@@ -60,38 +64,9 @@ public class DisableOperationsTest {
     }
 
     @Test
-    public void shouldDisableOperationsUsingOperationDeclarations() {
-        // Given
-        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
-        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
-        System.setProperty(SystemProperty.GRAPH_ID, "graphId");
-        final DefaultGraphFactory factory = new DefaultGraphFactory();
-
-        // When
-        final Graph graph = factory.createGraph();
-
-        // Then
-        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
-            assertFalse(disabledOperation.getSimpleName() + " should not be supported", graph.isSupported(disabledOperation));
-        }
-    }
+    public abstract void shouldDisableOperationsUsingOperationDeclarations();
 
     @Test
-    public void shouldNotDisableOperationsWhenNotUsingRestApi() {
-        // Given
-        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
-        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
+    public abstract void shouldNotDisableOperationsWhenNotUsingRestApi();
 
-        // When
-        final Graph graph = new Graph.Builder()
-                .graphId(GRAPH_ID)
-                .storeProperties(storePropsPath.toURI())
-                .addSchema(schemaPath.toURI())
-                .build();
-
-        // Then
-        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
-            assertTrue(disabledOperation.getSimpleName() + " should be supported", graph.isSupported(disabledOperation));
-        }
-    }
 }
