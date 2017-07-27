8b0dd675dc69b5e25fa20016cf2de6522329816d
p013570 <p013570@users.noreply.github.com>
2017-07-27 08:27:05 +0100

    Added check for all modules in travis scripts
    Also the gaffer modules are deleted from the mvn repository before the cache is saved

--- rest-api/accumulo-rest/src/test/java/uk/gov/gchq/gaffer/rest/factory/AccumuloDisableOperationsTest.java
+++ rest-api/accumulo-rest/src/test/java/uk/gov/gchq/gaffer/rest/factory/AccumuloDisableOperationsTest.java
@@ -16,19 +16,12 @@
 
 package uk.gov.gchq.gaffer.rest.factory;
 
-import org.junit.Test;
 import uk.gov.gchq.gaffer.accumulostore.operation.hdfs.operation.ImportAccumuloKeyValueFiles;
-import uk.gov.gchq.gaffer.graph.Graph;
 import uk.gov.gchq.gaffer.hdfs.operation.AddElementsFromHdfs;
 import uk.gov.gchq.gaffer.hdfs.operation.SampleDataForSplitPoints;
-import uk.gov.gchq.gaffer.operation.Operation;
 import uk.gov.gchq.gaffer.operation.impl.SplitStore;
 import uk.gov.gchq.gaffer.rest.DisableOperationsTest;
-import uk.gov.gchq.gaffer.rest.SystemProperty;
 import java.io.IOException;
-
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 
 public class AccumuloDisableOperationsTest extends DisableOperationsTest {
@@ -39,43 +32,5 @@ public class AccumuloDisableOperationsTest extends DisableOperationsTest {
                 SampleDataForSplitPoints.class,
                 ImportAccumuloKeyValueFiles.class
         );
-    }
-
-    @Override
-    @Test
-    public void shouldDisableOperationsUsingOperationDeclarations() {
-        // Given
-        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
-        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
-        System.setProperty(SystemProperty.GRAPH_ID, "graphId");
-        final DefaultGraphFactory factory = new DefaultGraphFactory();
-
-        // When
-        final Graph graph = factory.createGraph();
-
-        // Then
-        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
-            assertFalse(disabledOperation.getSimpleName() + " should not be supported", graph.isSupported(disabledOperation));
-        }
-    }
-
-    @Override
-    @Test
-    public void shouldNotDisableOperationsWhenNotUsingRestApi() {
-        // Given
-        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
-        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
-
-        // When
-        final Graph graph = new Graph.Builder()
-                .graphId(GRAPH_ID)
-                .storeProperties(storePropsPath.toURI())
-                .addSchema(schemaPath.toURI())
-                .build();
-
-        // Then
-        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
-            assertTrue(disabledOperation.getSimpleName() + " should be supported", graph.isSupported(disabledOperation));
-        }
     }
 }
--- rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/DisableOperationsTest.java
+++ rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/DisableOperationsTest.java
@@ -23,11 +23,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import uk.gov.gchq.gaffer.commonutil.CommonTestConstants;
 import uk.gov.gchq.gaffer.graph.Graph;
-import uk.gov.gchq.gaffer.mapstore.operation.CountAllElementsDefaultView;
 import uk.gov.gchq.gaffer.operation.Operation;
-import uk.gov.gchq.gaffer.operation.impl.Count;
-import uk.gov.gchq.gaffer.operation.impl.CountGroups;
-import uk.gov.gchq.gaffer.operation.impl.SplitStore;
 import uk.gov.gchq.gaffer.operation.impl.add.AddElementsFromFile;
 import uk.gov.gchq.gaffer.rest.factory.DefaultGraphFactory;
 import java.io.File;
@@ -64,9 +60,38 @@ public abstract class DisableOperationsTest {
     }
 
     @Test
-    public abstract void shouldDisableOperationsUsingOperationDeclarations();
+    public void shouldDisableOperationsUsingOperationDeclarations() {
+        // Given
+        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
+        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
+        System.setProperty(SystemProperty.GRAPH_ID, "graphId");
+        final DefaultGraphFactory factory = new DefaultGraphFactory();
+
+        // When
+        final Graph graph = factory.createGraph();
+
+        // Then
+        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
+            assertFalse(disabledOperation.getSimpleName() + " should not be supported", graph.isSupported(disabledOperation));
+        }
+    }
 
     @Test
-    public abstract void shouldNotDisableOperationsWhenNotUsingRestApi();
+    public void shouldNotDisableOperationsWhenNotUsingRestApi() {
+        // Given
+        System.setProperty(SystemProperty.STORE_PROPERTIES_PATH, storePropsPath.getAbsolutePath());
+        System.setProperty(SystemProperty.SCHEMA_PATHS, schemaPath.getAbsolutePath());
 
+        // When
+        final Graph graph = new Graph.Builder()
+                .graphId(GRAPH_ID)
+                .storeProperties(storePropsPath.toURI())
+                .addSchema(schemaPath.toURI())
+                .build();
+
+        // Then
+        for (final Class<? extends Operation> disabledOperation : disabledOperations) {
+            assertTrue(disabledOperation.getSimpleName() + " should be supported", graph.isSupported(disabledOperation));
+        }
+    }
 }
