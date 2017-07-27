a580d0a08f183f0b358d5054984262ca1ff1ed5f
p013570 <p013570@users.noreply.github.com>
2017-07-27 09:05:09 +0100

    Moved the travis before install code into a separate file

---
+++ cd/before_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+allModules=`mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}' org.codehaus.mojo:exec-maven-plugin:1.3.1:exec`
+echo "All modules: $allModules"
+for module in $allModules
+do
+    if ! grep -q :$module .travis.yml
+    then
+        echo ".travis.yml is missing module $module";
+        exit 1;
+    fi
+done
