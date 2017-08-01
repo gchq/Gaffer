8fde6a42fac8e3e3f11e8e1c87f1d04d228656f3
m55624 <m55624@users.noreply.github.com>
2017-08-01 14:06:54 +0100

    gh-1085 - checkstyle fixes

--- doc/src/main/java/uk/gov/gchq/gaffer/doc/operation/ExportToOtherAuthorisedGraphExample.java
+++ doc/src/main/java/uk/gov/gchq/gaffer/doc/operation/ExportToOtherAuthorisedGraphExample.java
@@ -59,7 +59,8 @@ public class ExportToOtherAuthorisedGraphExample extends OperationExample {
         showExample(opChain, "This example will export all Edges with group 'edge' to another Gaffer graph with new ID 'newGraphId'. " +
                 "The new graph will have the same schema and same store properties as the current graph. " +
                 "In this case it will just create another table in accumulo called 'newGraphId'. " +
-                "It will also compare the running users Operation authorisations to the authorisations supplied to the exporter.");
+                "It will also compare the running users Operation authorisations to the authorisations supplied to the exporter. " +
+                "These will be specified in ExportToOtherAuthorisedGraphOperationDeclarations.json.");
     }
 
     public void simpleExportUsingParentIds() {
@@ -82,6 +83,7 @@ public class ExportToOtherAuthorisedGraphExample extends OperationExample {
 
         showExample(opChain, "This example will export all Edges with group 'edge' to another Gaffer graph with new ID 'newGraphId'. " +
                 "The new graph will have a parent Schema and Store Properties within the graph library specifed by the ID's. " +
-                "It will also compare the running users Operation authorisations to the authorisations supplied to the exporter.");
+                "It will also compare the running users Operation authorisations to the authorisations supplied to the exporter. " +
+                "These will be specified in ExportToOtherAuthorisedGraphOperationDeclarations.json.");
     }
 }
