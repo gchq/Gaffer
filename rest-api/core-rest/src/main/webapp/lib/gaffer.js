35c57b3999b3ece6c8ec8718779a8456a0676801
t616178 <t616178@users.noreply.github.com>
2017-08-09 15:25:21 +0100

    gh-1171 - Updated Swagger to v2.2.10

--- rest-api/core-rest/src/main/webapp/lib/gaffer.js
+++ rest-api/core-rest/src/main/webapp/lib/gaffer.js
@@ -35,29 +35,29 @@ function log() {
 
 function init(onSwaggerComplete){
       window.swaggerUi = new SwaggerUi({
-      url: getVersion() + "/swagger.json",
-      dom_id:"swagger-ui-container",
-      supportedSubmitMethods: ['get','post','put','delete'],
-      onComplete: function(swaggerApi, swaggerUi){
-        log("Loaded swagger");
-        $('pre code').each(function(i,e){hljs.highlightBlock(e)});
-        addExampleButtons();
-        if(onSwaggerComplete) {
-            onSwaggerComplete();
-        }
-      },
-      onFailure: function(data){
-      log("Unable to load SwaggerUI");
-      },
-      docExpansion: "none",
-      sorter: "alpha",
-      apisSorter: "alpha",
-      operationsSorter: "alpha"
-    });
+        url: getVersion() + "/swagger.json",
+        dom_id: "swagger-ui-container",
+        supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
+        onComplete: function(swaggerApi, swaggerUi){
+          log("Loaded swagger");
+              $('pre code').each(function(i,e){hljs.highlightBlock(e)});
+              addExampleButtons();
+              if(onSwaggerComplete) {
+                  onSwaggerComplete();
+              }
+        },
+        onFailure: function(data) {
+          log("Unable to load SwaggerUI");
+        },
+        docExpansion: "none",
+        jsonEditor: false,
+        defaultModelRendering: 'schema',
+        showRequestHeaders: false,
+        showOperationIds: false,
+        sorter: "alpha",
+        apisSorter: "alpha",
+        operationsSorter: "alpha"
+      });
 
-    window.swaggerUi.load();
-}
-
-function getVersion() {
-    return "v1";
+      window.swaggerUi.load();
 }
