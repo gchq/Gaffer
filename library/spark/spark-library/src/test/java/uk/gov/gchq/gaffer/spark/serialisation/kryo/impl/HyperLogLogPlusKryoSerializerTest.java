b37e6ffbe87e6fa8a3073842cca92436a914e9c1
m607123 <m607123@users.noreply.github.com>
2017-08-31 11:51:48 +0100

    gh-1097 - Fixed HLLPKryoSerializerTest

--- library/spark/spark-library/src/test/java/uk/gov/gchq/gaffer/spark/serialisation/kryo/impl/HyperLogLogPlusKryoSerializerTest.java
+++ library/spark/spark-library/src/test/java/uk/gov/gchq/gaffer/spark/serialisation/kryo/impl/HyperLogLogPlusKryoSerializerTest.java
@@ -31,6 +31,11 @@ import static org.junit.Assert.assertEquals;
 
 public class HyperLogLogPlusKryoSerializerTest extends KryoSerializerTest<HyperLogLogPlus> {
 
+    private final Kryo kryo = new Kryo();
+
+    @Before
+    public void setup() { new Registrator().registerClasses(kryo); }
+
     @Override
     protected Class<HyperLogLogPlus> getTestClass() {
         return HyperLogLogPlus.class;
@@ -41,5 +46,25 @@ public class HyperLogLogPlusKryoSerializerTest extends KryoSerializerTest<HyperL
         final HyperLogLogPlus hyperLogLogPlus = new HyperLogLogPlus(5, 5);
         IntStream.range(0, 1000).forEach(i -> hyperLogLogPlus.offer("" + i));
         return hyperLogLogPlus;
+    }
+
+    @Test
+    @Override
+    public void shouldSerialiseAndDeserialise() {
+        // Given
+        final HyperLogLogPlus obj = getTestObject();
+
+        // When
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final Output output = new Output(byteArrayOutputStream);
+        kryo.writeObject(output, obj);
+        output.close();
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        final Input input = new Input(byteArrayInputStream);
+        final HyperLogLogPlus read = kryo.readObject(input, getTestClass());
+        input.close();
+
+        // Then
+        assertEquals(obj.cardinality(), read.cardinality());
     }
 }
