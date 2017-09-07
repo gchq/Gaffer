6e908ffdeeede46a45080391f16d99cc088d96fb
p013570 <p013570@users.noreply.github.com>
2017-09-07 16:15:01 +0100

    gh-538 - fixed checkstyle

--- rest-api/core-rest/src/main/java/uk/gov/gchq/gaffer/rest/service/v2/example/IExamplesServiceV2.java
+++ rest-api/core-rest/src/main/java/uk/gov/gchq/gaffer/rest/service/v2/example/IExamplesServiceV2.java
@@ -20,7 +20,6 @@ import uk.gov.gchq.gaffer.operation.Operation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
