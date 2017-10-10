05f928e0335bb8c8f2898ce8eef9d0526b204f84
p013570 <p013570@users.noreply.github.com>
2017-10-10 09:03:24 +0100

    gh-1396 - changed the way the element converter is serialized

--- library/spark/spark-accumulo-library/src/main/java/uk/gov/gchq/gaffer/sparkaccumulo/operation/utils/java/ElementConverterFunction.java
+++ library/spark/spark-accumulo-library/src/main/java/uk/gov/gchq/gaffer/sparkaccumulo/operation/utils/java/ElementConverterFunction.java
@@ -18,31 +18,47 @@ package uk.gov.gchq.gaffer.sparkaccumulo.operation.utils.java;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Value;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
-import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import uk.gov.gchq.gaffer.accumulostore.key.AccumuloElementConverter;
 import uk.gov.gchq.gaffer.commonutil.pair.Pair;
 import uk.gov.gchq.gaffer.data.element.Element;
+import uk.gov.gchq.gaffer.store.schema.Schema;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class ElementConverterFunction implements PairFlatMapFunction<Element, Key, Value> {
     private static final long serialVersionUID = -3259752069724639102L;
+    private transient Schema schema;
+    private final byte[] schemaBytes;
+    private transient AccumuloElementConverter keyConverter;
+    private final Class<? extends AccumuloElementConverter> keyConverterClass;
 
-    private final Broadcast<AccumuloElementConverter> converterBroadcast;
-
-    public ElementConverterFunction(final Broadcast<AccumuloElementConverter> converterBroadcast) {
-        this.converterBroadcast = converterBroadcast;
+    public ElementConverterFunction(final Schema schema, final AccumuloElementConverter keyConverter) {
+        this.schema = schema;
+        schemaBytes = schema.toCompactJson();
+        this.keyConverter = keyConverter;
+        keyConverterClass = keyConverter.getClass();
     }
 
     @Override
-    public Iterator<Tuple2<Key, Value>> call(final Element e) throws Exception {
+    public Iterator<Tuple2<Key, Value>> call(final Element element) throws Exception {
+        if (null == schema) {
+            schema = Schema.fromJson(schemaBytes);
+        }
+        if (null == keyConverter) {
+            try {
+                keyConverter = keyConverterClass.getConstructor(Schema.class).newInstance(schema);
+            } catch (final InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+                throw new RuntimeException("Unable to instantiate key converter: " + keyConverterClass.getName(), e);
+            }
+        }
         final List<Tuple2<Key, Value>> tuples = new ArrayList<>(2);
-        final Pair<Key, Key> keys = converterBroadcast.value().getKeysFromElement(e);
-        final Value value = converterBroadcast.value().getValueFromElement(e);
+        final Pair<Key, Key> keys = keyConverter.getKeysFromElement(element);
+        final Value value = keyConverter.getValueFromElement(element);
         tuples.add(new Tuple2<>(keys.getFirst(), value));
         final Key second = keys.getSecond();
         if (null != second) {
