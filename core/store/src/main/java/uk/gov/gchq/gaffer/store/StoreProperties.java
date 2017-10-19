2505eb7f97d823c1c4949d03c637d5cc48f6fd97
p013570 <p013570@users.noreply.github.com>
2017-10-19 15:51:42 +0100

    gh-1431 - added store properties merge test

--- core/store/src/main/java/uk/gov/gchq/gaffer/store/StoreProperties.java
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/StoreProperties.java
@@ -240,14 +240,17 @@ public class StoreProperties implements Cloneable {
     }
 
     public void merge(final StoreProperties properties) {
-        if (null != properties.getId()
-                && null != getId()
-                && !properties.getId().equals(getId())) {
-            final String newId = getId() + "_" + properties.getId();
-            properties.setId(newId);
-            setId(newId);
+        if (null != properties) {
+            if (null != properties.getId()
+                    && null != getId()
+                    && !properties.getId().equals(getId())) {
+                final String newId = getId() + "_" + properties.getId();
+                properties.setId(newId);
+                setId(newId);
+            }
+
+            props.putAll(properties.getProperties());
         }
-        getProperties().putAll(properties.getProperties());
     }
 
     public String getId() {
