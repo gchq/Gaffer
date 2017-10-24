17553bdf40249a949c0bf6cf1074e2f52681089b
m607123 <m607123@users.noreply.github.com>
2017-10-24 16:47:36 +0100

    gh-1345 - ExtractItems and Handler added

---
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/operation/handler/ExtractItemsHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.gaffer.store.operation.handler;
+
+import com.google.common.collect.Lists;
+
+import uk.gov.gchq.gaffer.commonutil.stream.Streams;
+import uk.gov.gchq.gaffer.data.element.Element;
+import uk.gov.gchq.gaffer.operation.OperationException;
+import uk.gov.gchq.gaffer.operation.impl.ExtractItems;
+import uk.gov.gchq.gaffer.store.Context;
+import uk.gov.gchq.gaffer.store.Store;
+
+import java.util.stream.Collectors;
+
+public class ExtractItemsHandler implements OutputOperationHandler<ExtractItems,Iterable<? extends Element>> {
+    @Override
+    public Iterable<? extends Element> doOperation(final ExtractItems operation, final Context context, final Store store) throws OperationException {
+        final int selection = operation.getSelection();
+        return Streams.toStream(operation.getInput())
+                .map(i -> extract(i, selection))
+                .collect(Collectors.toSet());
+    }
+
+    private Element extract(final Iterable<? extends Element> input, final int selection) {
+        return Lists.newLinkedList(input).get(selection);
+    }
+}
