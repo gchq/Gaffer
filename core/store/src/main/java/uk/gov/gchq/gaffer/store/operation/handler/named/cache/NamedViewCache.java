5d9b7d5919a79598629741aa6111944f5f5058c9
m55624 <m55624@users.noreply.github.com>
2017-11-23 15:02:34 +0000

    gh-1544 - WIP NamedViewCache

--- core/store/src/main/java/uk/gov/gchq/gaffer/store/operation/handler/named/cache/NamedViewCache.java
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/operation/handler/named/cache/NamedViewCache.java
@@ -19,46 +19,107 @@ package uk.gov.gchq.gaffer.store.operation.handler.named.cache;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.gov.gchq.gaffer.cache.CacheServiceLoader;
+import uk.gov.gchq.gaffer.cache.exception.CacheOperationException;
+import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable;
+import uk.gov.gchq.gaffer.commonutil.iterable.WrappedCloseableIterable;
 import uk.gov.gchq.gaffer.named.operation.cache.exception.CacheOperationFailedException;
-import uk.gov.gchq.gaffer.user.User;
+import uk.gov.gchq.gaffer.named.view.NamedView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * /**
  * Wrapper around the {@link uk.gov.gchq.gaffer.cache.CacheServiceLoader} to provide an interface for handling
- * the {@link uk.gov.gchq.gaffer.data.elementdefinition.NamedView}s for a Gaffer graph.
+ * the {@link uk.gov.gchq.gaffer.named.view.NamedView}s for a Gaffer graph.
  */
 public class NamedViewCache {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(NamedViewCache.class);
     private static final String CACHE_NAME = "NamedView";
 
-    public void addNamedView(final NamedView namedView, final boolean overwrite, final User user) throws CacheOperationFailedException {
-        String name;
-        try {
-            name = namedView.getViewName();
-        } catch (final NullPointerException e) {
-            throw new CacheOperationFailedException("NamedOperation cannot be null", e);
-        }
-        if (null == name) {
-            throw new CacheOperationFailedException("NamedOperation name cannot be null");
-        }
-        if (!overwrite) {
-            addToCache(name, namedView, false);
-            return;
-        }
-
-        NamedView existing = null;
-
-        try {
-            existing = getFromCache(name);
-        } catch (final CacheOperationFailedException e) { // if there is no existing named View add one
-            addToCache(name, namedView, false);
-            return;
-        }
-        if (existing.hasWriteAccess(user)) {
-            addToCache(name, namedView, true);
+    public void addNamedView(final NamedView namedView, final boolean overwrite) throws CacheOperationFailedException {
+        if (null != namedView.getViewName()) {
+            namedView.getViewName();
         } else {
-            throw new CacheOperationFailedException("User " + namedView.getCreatorId() + " does not have permission to overwrite");
+            throw new IllegalArgumentException("NamedView name cannot be null");
+        }
+
+        if (!overwrite) {
+            addToCache(namedView, false);
+        } else {
+            addToCache(namedView, true);
+        }
+    }
+
+    public void deleteNamedView(final String name) throws CacheOperationFailedException {
+        if (null != name) {
+            deleteFromCache(name);
+        } else {
+            throw new IllegalArgumentException("NamedView name cannot be null");
+        }
+    }
+
+    public NamedView getNamedView(final String name) throws CacheOperationFailedException {
+        if (null != name) {
+            return getFromCache(name);
+        } else {
+            throw new IllegalArgumentException("NamedView name cannot be null");
+        }
+    }
+
+    public CloseableIterable<NamedView> getAllNamedViews() throws CacheOperationFailedException {
+        final Set<String> keys = CacheServiceLoader.getService().getAllKeysFromCache(CACHE_NAME);
+        final Set<NamedView> views = new HashSet<>();
+        for (final String key : keys) {
+            try {
+                views.add(getFromCache(key));
+            } catch (final CacheOperationFailedException e) {
+                throw e;
+            }
+        }
+        return new WrappedCloseableIterable<>(views);
+    }
+
+    public void clearCache() throws CacheOperationFailedException {
+        try {
+            CacheServiceLoader.getService().clearCache(CACHE_NAME);
+        } catch (final CacheOperationException e) {
+            throw new CacheOperationFailedException("Failed to clear cache", e);
+        }
+    }
+
+    public void deleteFromCache(final String name) throws CacheOperationFailedException {
+        CacheServiceLoader.getService().removeFromCache(CACHE_NAME, name);
+
+        if (null != CacheServiceLoader.getService().getFromCache(CACHE_NAME, name)) {
+            throw new CacheOperationFailedException("Failed to remove " + name + " from cache");
+        }
+    }
+
+    public void addToCache(final NamedView operation, final boolean overwrite) throws CacheOperationFailedException {
+        try {
+            if (overwrite) {
+                CacheServiceLoader.getService().putInCache(CACHE_NAME, operation.getViewName(), operation);
+            } else {
+                CacheServiceLoader.getService().putSafeInCache(CACHE_NAME, operation.getViewName(), operation);
+            }
+        } catch (final CacheOperationException e) {
+            throw new CacheOperationFailedException(e);
+        }
+    }
+
+    public NamedView getFromCache(final String name) throws CacheOperationFailedException {
+        if (null != name) {
+            final NamedView op = CacheServiceLoader.getService().getFromCache(CACHE_NAME, name);
+            if (null != op) {
+                return op;
+            } else {
+                throw new CacheOperationFailedException("No named view with the name " + name + " exists in the cache");
+            }
+        } else {
+            throw new CacheOperationFailedException("Operation name cannot be null");
         }
     }
 }
