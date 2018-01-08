ce582c4eeba10551a24e839cb1610cdfe88351b2
t616178 <t616178@users.noreply.github.com>
2018-01-08 12:10:49 +0000

    gh-1438 - Re-enable test

--- core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/LongUtilTest.java
+++ core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/LongUtilTest.java
@@ -16,7 +16,6 @@
 
 package uk.gov.gchq.gaffer.commonutil;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -25,7 +24,6 @@ import java.util.Set;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@Ignore
 public class LongUtilTest {
     @Test
     public void shouldGetDifferentPositiveTimeBasedRandoms() {
