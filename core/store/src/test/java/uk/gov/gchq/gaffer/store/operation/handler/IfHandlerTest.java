f63bf7bd227cc742aa03e0f162c6caf59fb86d42
m607123 <m607123@users.noreply.github.com>
2018-02-18 23:52:07 +0000

    gh-1649 - Added further tests

--- core/store/src/test/java/uk/gov/gchq/gaffer/store/operation/handler/IfHandlerTest.java
+++ core/store/src/test/java/uk/gov/gchq/gaffer/store/operation/handler/IfHandlerTest.java
@@ -17,10 +17,13 @@ package uk.gov.gchq.gaffer.store.operation.handler;
 
 import org.junit.Test;
 
+import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.OperationChain;
 import uk.gov.gchq.gaffer.operation.OperationException;
 import uk.gov.gchq.gaffer.operation.data.EntitySeed;
 import uk.gov.gchq.gaffer.operation.impl.GetWalks;
 import uk.gov.gchq.gaffer.operation.impl.If;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
 import uk.gov.gchq.gaffer.store.Context;
 import uk.gov.gchq.gaffer.store.Store;
@@ -150,6 +153,55 @@ public class IfHandlerTest {
         assertEquals(result, input);
         verify(predicate).test(input);
         verify(store, never()).execute(then, context);
+        verify(store, never()).execute(otherwise, context);
+    }
+
+    @Test
+    public void shouldExecuteThenWithBooleanCondition() throws OperationException {
+        // Given
+        final Object input = Arrays.asList(new EntitySeed("1"), new EntitySeed("2"));
+        final GetElements then = mock(GetElements.class);
+        final GetAllElements otherwise = mock(GetAllElements.class);
+        final If filter = new If.Builder()
+                .input(input)
+                .condition(true)
+                .then(then)
+                .otherwise(otherwise)
+                .build();
+
+        final IfHandler handler = new IfHandler();
+
+        // When
+        final Object result = handler.doOperation(filter, context, store);
+
+        // Then
+        verify(store).execute(then, context);
+        verify(store, never()).execute(otherwise, context);
+    }
+
+    @Test
+    public void shouldExecuteCorrectlyWithOperationChainAsThen() throws OperationException {
+        // Given
+        final Object input = Arrays.asList(new EntitySeed("1"), new EntitySeed("2"));
+        final Predicate<Object> predicate = mock(Predicate.class);
+        final OperationChain<Object> then = mock(OperationChain.class);
+        final GetAllElements otherwise = mock(GetAllElements.class);
+        final If filter = new If.Builder()
+                .input(input)
+                .predicate(predicate)
+                .then(then)
+                .otherwise(otherwise)
+                .build();
+
+        final IfHandler handler = new IfHandler();
+
+        given(predicate.test(input)).willReturn(true);
+
+        // When
+        final Object result = handler.doOperation(filter, context, store);
+
+        // Then
+        verify(store).execute(then, context);
         verify(store, never()).execute(otherwise, context);
     }
 }
