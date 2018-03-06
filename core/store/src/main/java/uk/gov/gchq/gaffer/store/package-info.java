ab6ad047f7c8126a599e9d448415b12499ba1af0
p013570 <p013570@users.noreply.github.com>
2018-03-06 19:40:24 +0000

    gh-1701 - updated header dates

--- core/store/src/main/java/uk/gov/gchq/gaffer/store/package-info.java
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Crown Copyright
+ * Copyright 2017-2018 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 /*
- * Copyright 2017 Crown Copyright
+ * Copyright 2017-2018 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
