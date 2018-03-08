ed7bdc8cd8c216adf7cc24f610ffda55d46464a2
t616178 <t616178@users.noreply.github.com>
2018-03-08 11:43:49 +0000

    gh-1657 - Updated script (tested on macOS High Sierra 10.13.3)

--- cd/updateCopyrightHeaders.sh
+++ cd/updateCopyrightHeaders.sh
@@ -10,14 +10,14 @@ do
     for startYear in `seq 2016 1 $((currentYear - 1))`;
     do
         sedCmd="s/Copyright ${startYear} Crown Copyright/Copyright ${startYear}-${currentYear} Crown Copyright/g"
-        #echo "sed -i '' ${sedCmd} ${f}"
-        sed -i '' "$sedCmd" ${f}
+        #echo "sed -i'' -e "$sedCmd" ${f}"
+        sed -i'' -e "$sedCmd" ${f}
         if [ $((startYear+1)) -lt ${currentYear} ]; then
             for endYear in `seq $((startYear + 1)) 1 $((currentYear - 1))`;
             do
                 sedCmd="s/Copyright ${startYear}-${endYear} Crown Copyright/Copyright ${startYear}-${currentYear} Crown Copyright/g"
-                #echo "sed -i '' ${sedCmd} ${f}"
-                sed -i '' "$sedCmd" ${f}
+                #echo "sed -i'' -e "$sedCmd" ${f}"
+                sed -i'' -e "$sedCmd" ${f}
             done
         fi
     done
