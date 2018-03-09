a12fad7144e1d5c2b051f1eb179fb102e380d7ab
t616178 <t616178@users.noreply.github.com>
2018-03-09 12:33:26 +0000

    gh-1703 - Updated script to remove any/all temporary files.

--- cd/updateCopyrightHeaders.sh
+++ cd/updateCopyrightHeaders.sh
@@ -12,12 +12,14 @@ do
         sedCmd="s/Copyright ${startYear} Crown Copyright/Copyright ${startYear}-${currentYear} Crown Copyright/g"
         #echo "sed -i'' -e "$sedCmd" ${f}"
         sed -i'' -e "$sedCmd" ${f}
+        rm -f ${f}-e
         if [ $((startYear+1)) -lt ${currentYear} ]; then
             for endYear in `seq $((startYear + 1)) 1 $((currentYear - 1))`;
             do
                 sedCmd="s/Copyright ${startYear}-${endYear} Crown Copyright/Copyright ${startYear}-${currentYear} Crown Copyright/g"
                 #echo "sed -i'' -e "$sedCmd" ${f}"
                 sed -i'' -e "$sedCmd" ${f}
+                rm -f ${f}-e
             done
         fi
     done
