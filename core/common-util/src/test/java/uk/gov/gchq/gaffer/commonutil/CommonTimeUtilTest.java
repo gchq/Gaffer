a586cc7af664d5fddcdb583f7dd2f9c340b5a923
javadev001001 <javadev014841@gmail.com>
2018-04-10 14:36:25 +0100

    gh-1740 Added fields to switch statement and updated test (#1759)
    * gh-1740 Added fields to switch statement and updated test
    
    * gh-1740 Updated in line with PR comments
    
    * gh-1740 Updated in line with PR comments

--- core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/CommonTimeUtilTest.java
+++ core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/CommonTimeUtilTest.java
@@ -22,12 +22,14 @@ import java.time.OffsetDateTime;
 
 import static java.time.OffsetDateTime.parse;
 import static org.junit.Assert.assertEquals;
-import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.DAY;
-import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.HOUR;
-import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.MINUTE;
-import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.MONTH;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.MILLISECOND;
 import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.SECOND;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.MINUTE;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.HOUR;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.DAY;
 import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.WEEK;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.MONTH;
+import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.TimeBucket.YEAR;
 import static uk.gov.gchq.gaffer.commonutil.CommonTimeUtil.timeToBucket;
 
 public class CommonTimeUtilTest {
@@ -37,12 +39,17 @@ public class CommonTimeUtilTest {
         final OffsetDateTime offsetDateTime = OffsetDateTime.parse("2000-01-01T12:34:56.789Z");
         final long time = offsetDateTime.toInstant().toEpochMilli();
 
+        final OffsetDateTime yearOffsetDateTime = OffsetDateTime.parse("2000-02-03T12:34:56.789Z");
+        final long yearTime = yearOffsetDateTime.toInstant().toEpochMilli();
+
+        assertEquals(parse("2000-01-01T12:34:56.789Z").toInstant().toEpochMilli(), timeToBucket(time, MILLISECOND));
         assertEquals(parse("2000-01-01T12:34:56.000Z").toInstant().toEpochMilli(), timeToBucket(time, SECOND));
         assertEquals(parse("2000-01-01T12:34:00.000Z").toInstant().toEpochMilli(), timeToBucket(time, MINUTE));
         assertEquals(parse("2000-01-01T12:00:00.000Z").toInstant().toEpochMilli(), timeToBucket(time, HOUR));
         assertEquals(parse("2000-01-01T00:00:00.000Z").toInstant().toEpochMilli(), timeToBucket(time, DAY));
         assertEquals(parse("1999-12-27T00:00:00.000Z").toInstant().toEpochMilli(), timeToBucket(time, WEEK));
         assertEquals(parse("2000-01-01T00:00:00.000Z").toInstant().toEpochMilli(), timeToBucket(time, MONTH));
+        assertEquals(parse("2000-01-01T00:00:00.000Z").toInstant().toEpochMilli(), timeToBucket(yearTime, YEAR));
     }
 
 }
