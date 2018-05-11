edbe28ebb718a96d33ef904f73f952e0be2b5883
m55624 <m55624@users.noreply.github.com>
2018-05-11 10:12:09 +0100

    gh-1768 - test fixes

--- rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/service/v2/OperationServiceV2IT.java
+++ rest-api/core-rest/src/test/java/uk/gov/gchq/gaffer/rest/service/v2/OperationServiceV2IT.java
@@ -18,17 +18,56 @@ package uk.gov.gchq.gaffer.rest.service.v2;
 
 import org.junit.Test;
 
+import uk.gov.gchq.gaffer.mapstore.operation.CountAllElementsDefaultView;
+import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.OperationChain;
+import uk.gov.gchq.gaffer.operation.OperationChainDAO;
+import uk.gov.gchq.gaffer.operation.impl.Count;
+import uk.gov.gchq.gaffer.operation.impl.CountGroups;
+import uk.gov.gchq.gaffer.operation.impl.DiscardOutput;
+import uk.gov.gchq.gaffer.operation.impl.GetWalks;
+import uk.gov.gchq.gaffer.operation.impl.If;
+import uk.gov.gchq.gaffer.operation.impl.Limit;
+import uk.gov.gchq.gaffer.operation.impl.Map;
+import uk.gov.gchq.gaffer.operation.impl.Validate;
+import uk.gov.gchq.gaffer.operation.impl.While;
+import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
+import uk.gov.gchq.gaffer.operation.impl.compare.Max;
+import uk.gov.gchq.gaffer.operation.impl.compare.Min;
+import uk.gov.gchq.gaffer.operation.impl.compare.Sort;
+import uk.gov.gchq.gaffer.operation.impl.export.GetExports;
+import uk.gov.gchq.gaffer.operation.impl.export.set.ExportToSet;
+import uk.gov.gchq.gaffer.operation.impl.export.set.GetSetExport;
+import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
+import uk.gov.gchq.gaffer.operation.impl.function.Filter;
+import uk.gov.gchq.gaffer.operation.impl.function.Transform;
+import uk.gov.gchq.gaffer.operation.impl.generate.GenerateElements;
+import uk.gov.gchq.gaffer.operation.impl.generate.GenerateObjects;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds;
 import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
+import uk.gov.gchq.gaffer.operation.impl.output.ToArray;
+import uk.gov.gchq.gaffer.operation.impl.output.ToCsv;
+import uk.gov.gchq.gaffer.operation.impl.output.ToEntitySeeds;
+import uk.gov.gchq.gaffer.operation.impl.output.ToList;
+import uk.gov.gchq.gaffer.operation.impl.output.ToMap;
+import uk.gov.gchq.gaffer.operation.impl.output.ToSet;
+import uk.gov.gchq.gaffer.operation.impl.output.ToStream;
+import uk.gov.gchq.gaffer.operation.impl.output.ToVertices;
 import uk.gov.gchq.gaffer.rest.ServiceConstants;
 import uk.gov.gchq.gaffer.rest.service.impl.OperationServiceIT;
+import uk.gov.gchq.gaffer.store.operation.GetSchema;
+import uk.gov.gchq.gaffer.store.operation.GetTraits;
 
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -64,15 +103,64 @@ public class OperationServiceV2IT extends OperationServiceIT {
     @Test
     public void shouldReturnAllOperationsAsOperationDetails() throws IOException {
         // Given
+        final List<Class<? extends Operation>> expectedOperations = new ArrayList<>();
+        expectedOperations.add(AddElements.class);
+        expectedOperations.add(GetElements.class);
+        expectedOperations.add(GetAdjacentIds.class);
+        expectedOperations.add(GetAllElements.class);
+        expectedOperations.add(ExportToSet.class);
+        expectedOperations.add(GetSetExport.class);
+        expectedOperations.add(GetExports.class);
+        expectedOperations.add(ToArray.class);
+        expectedOperations.add(ToEntitySeeds.class);
+        expectedOperations.add(ToList.class);
+        expectedOperations.add(ToMap.class);
+        expectedOperations.add(ToCsv.class);
+        expectedOperations.add(ToSet.class);
+        expectedOperations.add(ToStream.class);
+        expectedOperations.add(ToVertices.class);
+        expectedOperations.add(Max.class);
+        expectedOperations.add(Min.class);
+        expectedOperations.add(Sort.class);
+        expectedOperations.add(OperationChain.class);
+        expectedOperations.add(OperationChainDAO.class);
+        expectedOperations.add(GetWalks.class);
+        expectedOperations.add(GenerateElements.class);
+        expectedOperations.add(GenerateObjects.class);
+        expectedOperations.add(Validate.class);
+        expectedOperations.add(Count.class);
+        expectedOperations.add(CountGroups.class);
+        expectedOperations.add(Limit.class);
+        expectedOperations.add(DiscardOutput.class);
+        expectedOperations.add(GetSchema.class);
+        expectedOperations.add(Map.class);
+        expectedOperations.add(If.class);
+        expectedOperations.add(While.class);
+        expectedOperations.add(Filter.class);
+        expectedOperations.add(Transform.class);
+        expectedOperations.add(Aggregate.class);
+        expectedOperations.add(GetTraits.class);
+        expectedOperations.add(CountAllElementsDefaultView.class);
 
         // When
         Response response = getClient().getAllOperationsAsOperationDetails();
-
         List<OperationDetail> operationDetailList = response.readEntity(new GenericType<List<OperationDetail>>() {
         });
 
-        for (OperationDetail opDetail : operationDetailList) {
-        }
+        // Then
+        assertEquals(expectedOperations.size(), operationDetailList.size());
+
+        // When
+        List<String> operationDetailClassNameList = operationDetailList.stream()
+                .map(operationDetail -> operationDetail.getName())
+                .collect(Collectors.toList());
+
+        List<String> expectedOperationClassNameList = expectedOperations.stream()
+                .map(operationDetail -> operationDetail.getName())
+                .collect(Collectors.toList());
+
+        // Then
+        assertTrue(operationDetailClassNameList.containsAll(expectedOperationClassNameList));
     }
 
     @Override
