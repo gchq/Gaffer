5012a4dcb8a4711aba227a95cd8f3746664a9d1f
p013570 <p013570@users.noreply.github.com>
2018-09-04 17:56:31 +0100

    Fixed bug with MODULES variable

--- cd/test.sh
+++ cd/test.sh
@@ -3,7 +3,6 @@ test.sh#!/usr/bin/env bash
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-    MODULES=$1
     if [ "$MODULES" == '' ]; then
         echo "Running test script: mvn -q verify -P travis,test -B"
         mvn -q verify -P travis,test -B
