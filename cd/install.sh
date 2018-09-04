1a713d1eb7968ea17c0ff4fa304d968cf9176201
p013570 <p013570@users.noreply.github.com>
2018-09-04 14:42:26 +0100

    Changing travis to use build stages

--- cd/install.sh
+++ cd/install.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-if [ "$RELEASE" != 'true' ] && [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
-    if [ "$MODULES" == '' ] || [[ $MODULES == *'!'* ]]; then
-        echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
-        mvn -q install -P quick,travis,build-extras -B -V
-    else
-        echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V -pl $MODULES -am"
-        mvn -q install -P quick,travis,build-extras -B -V -pl $MODULES -am
-    fi
-fi
+echo "Running install script: mvn -q install -P quick,travis,build-extras -B -V"
+mvn -q install -P quick,travis,build-extras -B -V
