70ecac605b11215309db42094985c40d1bc9f782
p013570 <p013570@users.noreply.github.com>
2018-09-04 17:20:10 +0100

    Fixed build matrix issues

--- cd/test.sh
+++ cd/test.sh
@@ -2,11 +2,13 @@ test.sh#!/usr/bin/env bash
 
 set -e
 
-MODULES=$1
-if [ "$MODULES" == '' ]; then
-    echo "Running test script: mvn -q verify -P travis,test -B"
-    mvn -q verify -P travis,test -B
-else
-    echo "Running test script: mvn -q verify -P travis,test -B -pl $MODULES"
-    mvn -q verify -P travis,test -B -pl $MODULES
+if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+    MODULES=$1
+    if [ "$MODULES" == '' ]; then
+        echo "Running test script: mvn -q verify -P travis,test -B"
+        mvn -q verify -P travis,test -B
+    else
+        echo "Running test script: mvn -q verify -P travis,test -B -pl $MODULES"
+        mvn -q verify -P travis,test -B -pl $MODULES
+    fi
 fi
