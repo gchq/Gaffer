0f7d5268112db9277feac185abc8c0d446bd234b
p013570 <p013570@users.noreply.github.com>
2018-09-05 16:47:00 +0100

    Removed MODULES env variables

--- cd/test.sh
+++ cd/test.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
+    MODULES=$1
     if [ "$MODULES" == '' ]; then
         echo "Running test script: mvn -q verify -P travis,test -B"
         mvn -q verify -P travis,test -B
