1acbe50fa69c2d70b8b85c4a428cf5f85d3dc2e9
m55624 <m55624@users.noreply.github.com>
2018-09-05 10:50:36 +0100

    gh-1845 - removal of commented test class

--- core/operation/src/test/java/uk/gov/gchq/gaffer/operation/impl/ForEachTest.java
+++ core/operation/src/test/java/uk/gov/gchq/gaffer/operation/impl/ForEachTest.java
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *//*
-
+ */
 
 package uk.gov.gchq.gaffer.operation.impl;
 
@@ -67,4 +66,3 @@ public class ForEachTest extends OperationTest<ForEach> {
                 .build();
     }
 }
-*/
