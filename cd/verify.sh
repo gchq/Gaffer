b6ffaef1601ca57c115d976fc0613683369fb72a
d47853 <d47853@users.noreply.github.com>
2018-10-11 11:45:22 +0100

    disabled quiet mode when running verify.sh

--- cd/verify.sh
+++ cd/verify.sh
@@ -5,13 +5,13 @@ set -e
 if [ "$RELEASE" != 'true' ] && [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
     if [ "$MODULES" == '' ]; then
         echo "Running verify script: mvn -q verify -P travis,analyze -B"
-        mvn -q verify -P travis,analyze -B
+        mvn verify -P travis,analyze -B
         echo "Running verify script: mvn -q verify -P travis,test -B"
-        mvn -q verify -P travis,test -B
+        mvn verify -P travis,test -B
     else
         echo "Running verify script: mvn -q verify -P travis,analyze -B -pl $MODULES"
-        mvn -q verify -P travis,analyze -B -pl $MODULES
+        mvn verify -P travis,analyze -B -pl $MODULES
         echo "Running verify script: mvn -q verify -P travis,test -B -pl $MODULES"
-        mvn -q verify -P travis,test -B -pl $MODULES
+        mvn verify -P travis,test -B -pl $MODULES
     fi
 fi
