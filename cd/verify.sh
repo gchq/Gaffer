50636360f5937f624464c8970e3a361184e39e55
d47853 <d47853@users.noreply.github.com>
2018-10-12 15:16:48 +0100

    gh-1911 testing fix using --no-snapshot-updates option on maven

--- cd/verify.sh
+++ cd/verify.sh
@@ -4,14 +4,14 @@ set -e
 
 if [ "$RELEASE" != 'true' ] && [ "$TRAVIS_PULL_REQUEST" != 'false' ]; then
     if [ "$MODULES" == '' ]; then
-        echo "Running verify script: mvn -q verify -P travis,analyze -B"
-        mvn verify -P travis,analyze -B
-        echo "Running verify script: mvn -q verify -P travis,test -B"
-        mvn verify -P travis,test -B
+        echo "Running verify script: mvn verify -nsu -P travis,analyze -B"
+        mvn verify -nsu -P travis,analyze -B
+        echo "Running verify script: mvn verify -nsu -P travis,test -B"
+        mvn verify -nsu -P travis,test -B
     else
-        echo "Running verify script: mvn -q verify -P travis,analyze -B -pl $MODULES"
-        mvn verify -P travis,analyze -B -pl $MODULES
-        echo "Running verify script: mvn -q verify -P travis,test -B -pl $MODULES"
-        mvn verify -P travis,test -B -pl $MODULES
+        echo "Running verify script: mvn verify -nsu -P travis,analyze -B -pl $MODULES"
+        mvn verify -nsu -P travis,analyze -B -pl $MODULES
+        echo "Running verify script: mvn verify -nsu -P travis,test -B -pl $MODULES"
+        mvn verify -nsu -P travis,test -B -pl $MODULES
     fi
 fi
