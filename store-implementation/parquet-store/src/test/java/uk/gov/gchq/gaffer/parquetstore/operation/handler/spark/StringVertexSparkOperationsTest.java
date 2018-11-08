9a25f2b4da493d8f39e8e2840373b03b955c37d2
m55624 <m55624@users.noreply.github.com>
2018-11-08 15:51:56 +0000

    gh-1657 - WIP

--- store-implementation/parquet-store/src/test/java/uk/gov/gchq/gaffer/parquetstore/operation/handler/spark/StringVertexSparkOperationsTest.java
+++ store-implementation/parquet-store/src/test/java/uk/gov/gchq/gaffer/parquetstore/operation/handler/spark/StringVertexSparkOperationsTest.java
@@ -26,6 +26,9 @@ import uk.gov.gchq.gaffer.store.schema.Schema;
 
 import java.util.List;
 
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
 public class StringVertexSparkOperationsTest extends AbstractSparkOperationsTest {
     private final StringVertexOperationsTest svot = new StringVertexOperationsTest();
 
@@ -61,61 +64,64 @@ public class StringVertexSparkOperationsTest extends AbstractSparkOperationsTest
     }
 
     /**
-     @Override protected Graph genData(final boolean withVisibilities) throws IOException, OperationException, StoreException {
-     final ParquetStoreProperties properties = TestUtils.getParquetStoreProperties(testFolder);
-     final Graph graph = getGraph(getSchema(), properties, "StringVertexSparkOperationsTest");
-     graph.execute(new ImportJavaRDDOfElements.Builder()
-     .input(getElements(TestUtils.getJavaSparkContext(), withVisibilities))
-     .build(), USER);
-     return graph;
-     }
+    @Override
+    protected Graph genData(final boolean withVisibilities) throws IOException, OperationException, StoreException {
+        final ParquetStoreProperties properties = TestUtils.getParquetStoreProperties(testFolder);
+        final Graph graph = getGraph(getSchema(), properties, "StringVertexSparkOperationsTest");
+        graph.execute(new ImportJavaRDDOfElements.Builder()
+                .input(getElements(TestUtils.getJavaSparkContext(), withVisibilities))
+                .build(), USER);
+        return graph;
+    }
 
 
 
-     @Override protected JavaRDD<Element> getElements(final JavaSparkContext spark, final boolean withVisibilities) {
-     return DataGen.generate300StringElementsWithNullPropertiesRDD(spark, withVisibilities);
-     }
+    @Override
+    protected JavaRDD<Element> getElements(final JavaSparkContext spark, final boolean withVisibilities) {
+        return DataGen.generate300StringElementsWithNullPropertiesRDD(spark, withVisibilities);
+    }
 
-     @Override protected void checkGetDataFrameOfElements(final Dataset<Row> data, final boolean withVisibilities) {
-     // check all columns are present
-     final String[] actualColumns = data.columns();
-     final List<String> expectedColumns = new ArrayList<>(14);
-     expectedColumns.add(ParquetStoreConstants.GROUP);
-     expectedColumns.add(ParquetStoreConstants.VERTEX);
-     expectedColumns.add(ParquetStoreConstants.SOURCE);
-     expectedColumns.add(ParquetStoreConstants.DESTINATION);
-     expectedColumns.add(ParquetStoreConstants.DIRECTED);
-     expectedColumns.add("byte");
-     expectedColumns.add("double");
-     expectedColumns.add("float");
-     expectedColumns.add("treeSet");
-     expectedColumns.add("long");
-     expectedColumns.add("short");
-     expectedColumns.add("date");
-     expectedColumns.add("freqMap");
-     expectedColumns.add("count");
-     expectedColumns.add(TestTypes.VISIBILITY);
+    @Override
+    protected void checkGetDataFrameOfElements(final Dataset<Row> data, final boolean withVisibilities) {
+        // check all columns are present
+        final String[] actualColumns = data.columns();
+        final List<String> expectedColumns = new ArrayList<>(14);
+        expectedColumns.add(ParquetStoreConstants.GROUP);
+        expectedColumns.add(ParquetStoreConstants.VERTEX);
+        expectedColumns.add(ParquetStoreConstants.SOURCE);
+        expectedColumns.add(ParquetStoreConstants.DESTINATION);
+        expectedColumns.add(ParquetStoreConstants.DIRECTED);
+        expectedColumns.add("byte");
+        expectedColumns.add("double");
+        expectedColumns.add("float");
+        expectedColumns.add("treeSet");
+        expectedColumns.add("long");
+        expectedColumns.add("short");
+        expectedColumns.add("date");
+        expectedColumns.add("freqMap");
+        expectedColumns.add("count");
+        expectedColumns.add(TestTypes.VISIBILITY);
 
-     assertThat(expectedColumns, containsInAnyOrder(actualColumns));
+        assertThat(expectedColumns, containsInAnyOrder(actualColumns));
 
-     String visibility = null;
-     if (withVisibilities) {
-     visibility = "A";
-     }
+        String visibility = null;
+        if (withVisibilities) {
+            visibility = "A";
+        }
 
-     //check returned elements are correct
-     final List<Element> expected = new ArrayList<>(175);
-     final List<Element> actual = TestUtils.convertStringRowsToElements(data);
-     for (long i = 0; i < 25; i++) {
-     expected.add(DataGen.getEdge(TestGroups.EDGE, "src" + i, "dst" + i, true, null, null, null, null, null, null, null, null, 2, visibility));
-     expected.add(DataGen.getEdge(TestGroups.EDGE, "src" + i, "dst" + i, false, null, null, null, null, null, null, null, null, 2, visibility));
+        //check returned elements are correct
+        final List<Element> expected = new ArrayList<>(175);
+        final List<Element> actual = TestUtils.convertStringRowsToElements(data);
+        for (long i = 0; i < 25; i++) {
+            expected.add(DataGen.getEdge(TestGroups.EDGE, "src" + i, "dst" + i, true, null, null, null, null, null, null, null, null, 2, visibility));
+            expected.add(DataGen.getEdge(TestGroups.EDGE, "src" + i, "dst" + i, false, null, null, null, null, null, null, null, null, 2, visibility));
 
-     expected.add(DataGen.getEdge(TestGroups.EDGE_2, "src" + i, "dst" + i, true, null, null, null, null, null, null, null, null, 2, visibility));
-     expected.add(DataGen.getEdge(TestGroups.EDGE_2, "src" + i, "dst" + i, false, null, null, null, null, null, null, null, null, 2, visibility));
+            expected.add(DataGen.getEdge(TestGroups.EDGE_2, "src" + i, "dst" + i, true, null, null, null, null, null, null, null, null, 2, visibility));
+            expected.add(DataGen.getEdge(TestGroups.EDGE_2, "src" + i, "dst" + i, false, null, null, null, null, null, null, null, null, 2, visibility));
 
-     expected.add(DataGen.getEntity(TestGroups.ENTITY, "vert" + i, null, null, null, null, null, null, null, null, 2, visibility));
-     expected.add(DataGen.getEntity(TestGroups.ENTITY_2, "vert" + i, null, null, null, null, null, null, null, null, 2, visibility));
-     }
-     assertThat(expected, containsInAnyOrder(actual.toArray()));
-     }*/
+            expected.add(DataGen.getEntity(TestGroups.ENTITY, "vert" + i, null, null, null, null, null, null, null, null, 2, visibility));
+            expected.add(DataGen.getEntity(TestGroups.ENTITY_2, "vert" + i, null, null, null, null, null, null, null, null, 2, visibility));
+        }
+        assertThat(expected, containsInAnyOrder(actual.toArray()));
+    }*/
 }
