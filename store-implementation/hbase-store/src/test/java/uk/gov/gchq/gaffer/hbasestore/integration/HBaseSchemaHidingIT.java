e67c0991cdc759880a11d2ab2d1a6bf79a84b80c
m55624 <m55624@users.noreply.github.com>
2018-11-27 16:06:44 +0000

    gh-1657 - test fixes

--- store-implementation/hbase-store/src/test/java/uk/gov/gchq/gaffer/hbasestore/integration/HBaseSchemaHidingIT.java
+++ store-implementation/hbase-store/src/test/java/uk/gov/gchq/gaffer/hbasestore/integration/HBaseSchemaHidingIT.java
@@ -15,11 +15,10 @@
  */
 package uk.gov.gchq.gaffer.hbasestore.integration;
 
-import uk.gov.gchq.gaffer.commonutil.StreamUtil;
-import uk.gov.gchq.gaffer.hbasestore.HBaseProperties;
-import uk.gov.gchq.gaffer.hbasestore.HBaseStore;
+import uk.gov.gchq.gaffer.hbasestore.MiniHBaseStore;
 import uk.gov.gchq.gaffer.hbasestore.utils.TableUtils;
 import uk.gov.gchq.gaffer.integration.graph.SchemaHidingIT;
+import uk.gov.gchq.gaffer.store.StoreException;
 
 public class HBaseSchemaHidingIT extends SchemaHidingIT {
     public HBaseSchemaHidingIT() {
@@ -28,24 +27,10 @@ public class HBaseSchemaHidingIT extends SchemaHidingIT {
 
     @Override
     protected void cleanUp() {
-        final HBaseProperties storeProps = HBaseProperties.loadStoreProperties(StreamUtil.openStream(getClass(), storePropertiesPath));
-
-        final HBaseStore store;
         try {
-            store = Class.forName(storeProps.getStoreClass()).asSubclass(HBaseStore.class).newInstance();
-        } catch (final InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            throw new IllegalArgumentException("Could not create store of type: " + storeProps.getStoreClass(), e);
-        }
-
-        try {
-            store.preInitialise(
-                    "graphId",
-                    createFullSchema(),
-                    storeProps
-            );
-            TableUtils.dropTable(store);
-        } catch (final Exception e) {
-            // ignore exceptions
+            TableUtils.dropAllTables(new MiniHBaseStore().getConnection());
+        } catch (StoreException e) {
+            // ignore any errors that occur when dropping test tables
         }
     }
 }
