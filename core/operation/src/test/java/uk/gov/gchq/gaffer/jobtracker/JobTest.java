569b74d6efbeed4c5163fdb60e05b03a9f4a3a39
d47853 <d47853@users.noreply.github.com>
2019-05-10 13:16:12 +0100

    gh-2154 simplified Job object (#2155)
    * gh-2154 simplified Job object
    
    * gh-2154 removed spaces from tests
    
    * gh-2154 updated equals method to throw a GafferRuntimeException

---
+++ core/operation/src/test/java/uk/gov/gchq/gaffer/jobtracker/JobTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.gaffer.jobtracker;
+
+import org.junit.Test;
+
+import uk.gov.gchq.gaffer.exception.SerialisationException;
+import uk.gov.gchq.gaffer.jsonserialisation.JSONSerialiser;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+public class JobTest {
+
+    @Test
+    public void shouldJsonSerialiseAndDeserialise() throws SerialisationException {
+        // given
+        final String json = "{" +
+                "\"repeat\":{" +
+                "\"initialDelay\":20," +
+                "\"repeatPeriod\":30," +
+                "\"timeUnit\":\"SECONDS\"" +
+                "}," +
+                "\"operation\":{" +
+                "\"class\":\"uk.gov.gchq.gaffer.operation.impl.get.GetAllElements\"" +
+                "}" +
+                "}";
+
+        // when
+        final Job deserialised = JSONSerialiser.deserialise(json, Job.class);
+
+        // then
+        Job expected = new Job(new Repeat(20L, 30L, TimeUnit.SECONDS), new GetAllElements());
+        assertEquals(expected, deserialised);
+
+        // when
+        final String serialised = new String(JSONSerialiser.serialise(deserialised));
+
+        // then
+        assertEquals(json, serialised);
+    }
+}
