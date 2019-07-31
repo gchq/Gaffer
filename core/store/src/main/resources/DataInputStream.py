c1dc2d34294b90a35d662d2bf7ed22b4e21cfdff
c015dariu <c015dariu@github.com>
2019-07-31 14:58:40 +0100

    Finished converting DataInputStream to work with sockets.

--- core/store/src/main/resources/DataInputStream.py
+++ core/store/src/main/resources/DataInputStream.py
@@ -21,35 +21,35 @@ class DataInputStream:
         self.stream = stream
 
     def read_boolean(self):
-        return struct.unpack('?', self.stream.read(1))[0]
+        return struct.unpack('?', self.stream.recv(1))[0]
 
     def read_byte(self):
-        return struct.unpack('b', self.stream.read(1))[0]
+        return struct.unpack('b', self.stream.recv(1))[0]
 
     def read_unsigned_byte(self):
-        return struct.unpack('B', self.stream.read(1))[0]
+        return struct.unpack('B', self.stream.recv(1))[0]
 
     def read_char(self):
-        return chr(struct.unpack('>H', self.stream.read(2))[0])
+        return chr(struct.unpack('>H', self.stream.recv(2))[0])
 
     def read_double(self):
-        return struct.unpack('>d', self.stream.read(8))[0]
+        return struct.unpack('>d', self.stream.recv(8))[0]
 
     def read_float(self):
-        return struct.unpack('>f', self.stream.read(4))[0]
+        return struct.unpack('>f', self.stream.recv(4))[0]
 
     def read_short(self):
-        return struct.unpack('>h', self.stream.read(2))[0]
+        return struct.unpack('>h', self.stream.recv(2))[0]
 
     def read_unsigned_short(self):
-        return struct.unpack('>H', self.stream.read(2))[0]
+        return struct.unpack('>H', self.stream.recv(2))[0]
 
     def read_long(self):
-        return struct.unpack('>q', self.stream.read(8))[0]
+        return struct.unpack('>q', self.stream.recv(8))[0]
 
     def read_utf(self):
         utf_length = struct.unpack('>H', self.stream.recv(2))[0]
         return recvall(self.stream, utf_length)
 
     def read_int(self):
-        return struct.unpack('>i', self.stream.read(4))[0]
+        return struct.unpack('>i', self.stream.recv(4))[0]
