a62820e3f19312b2912f3e1e25d929b044049a5f
n288TJYRX <n288TJYRX@tutanota.com>
2019-11-22 14:04:44 +0000

    Created Container interface and moved docker container sending and receiving code to LocalDockerContainer class

--- library/python-library/src/main/java/uk/gov/gchq/gaffer/python/operation/Container.java
+++ library/python-library/src/main/java/uk/gov/gchq/gaffer/python/operation/Container.java
@@ -1,12 +1,30 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.gaffer.python.operation;
+
+import java.util.ArrayList;
 
 public interface Container {
 
     void start(? extends Platform platform);
 
-    void sendData(String data, String port);
+    void sendData(ArrayList data, String port);
 
-    String receiveData();
+    StringBuilder receiveData();
 
     void close();
 
--- library/python-library/src/main/java/uk/gov/gchq/gaffer/python/operation/LocalDockerContainer.java
+++ library/python-library/src/main/java/uk/gov/gchq/gaffer/python/operation/LocalDockerContainer.java
@@ -1,7 +1,33 @@
+/*
+ * Copyright 2019 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.gaffer.python.operation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.gchq.gaffer.jsonserialisation.JSONSerialiser;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class LocalDockerContainer implements Container {
-    private String containerId;
+    private static final Logger LOGGER = LoggerFactory.getLogger(LocalDockerContainer.class);
+    private final String containerId;
+    private Socket clientSocket = null;
 
     LocalDockerContainer(String containerId) {
         this.containerId = containerId;
@@ -12,18 +38,117 @@ public class LocalDockerContainer implements Container {
         docker.startContainer(this.containerId);
     }
 
+    // data will be operation.getInput()
     @Override
-    public void sendData(String data, String port) {
+    public void sendData(ArrayList data, String port) {
+        LOGGER.info("Attempting to connect with the container...");
+        sleep(PythonOperationConstants.ONESECOND);
+        // The container will need some time to start up, so keep trying to connect and check
+        // that its ready to receive data.
+        for (int i = 0; i < PythonOperationConstants.MAXTRIES; i++) {
+            try {
+                // Connect to the container
+                clientSocket = new Socket(PythonOperationConstants.LOCALHOST, Integer.parseInt(port));
+                LOGGER.info("Connected to container port at {}", clientSocket.getRemoteSocketAddress());
 
+                // Check the container is ready
+                DataInputStream inputStream = getInputStream(clientSocket);
+                LOGGER.info("Container ready status: {}", inputStream.readBoolean());
+                inputStream.close();
+
+                // Send the data
+                OutputStream outToContainer = clientSocket.getOutputStream();
+                DataOutputStream outputStream = new DataOutputStream(outToContainer);
+                boolean firstObject = true;
+                for (final Object current : data) {
+                    if (firstObject) {
+                        outputStream.writeUTF("[" + new String(JSONSerialiser.serialise(current)));
+                        firstObject = false;
+                    } else {
+                        outputStream.writeUTF(", " + new String(JSONSerialiser.serialise(current)));
+                    }
+                }
+                outputStream.writeUTF("]");
+                LOGGER.info("Sending data to docker container from {}", clientSocket.getLocalSocketAddress() + "...");
+                outputStream.flush();
+                break;
+            } catch (IOException e) {
+                LOGGER.info(e.getMessage());
+                sleep(PythonOperationConstants.TIMEOUT_100);
+            }
+        }
     }
 
     @Override
-    public String receiveData() {
-        return null;
+    public StringBuilder receiveData() {
+        // First get the length of the data coming from the container. Keep trying until the container is ready.
+        DataInputStream inputStream = null;
+        try {
+            inputStream = getInputStream(clientSocket);
+        } catch (IOException e) {
+            LOGGER.info(e.getMessage());
+        }
+        LOGGER.info("Inputstream is: {}", inputStream);
+        int incomingDataLength = 0;
+        Boolean failedToConnect = true;
+        Exception error = null;
+        if (clientSocket != null && inputStream != null) {
+            int tries = 0;
+            while (tries < PythonOperationConstants.TIMEOUT_100) {
+                try {
+                    incomingDataLength = inputStream.readInt();
+                    LOGGER.info("Length of container...{}", incomingDataLength);
+                    failedToConnect = false;
+                    break;
+                } catch (final IOException e) {
+                    tries += 1;
+                    error = e;
+                    sleep(PythonOperationConstants.TIMEOUT_200);
+                }
+            }
+        }
+
+        // If it failed to get the length of the incoming data then show the error, otherwise return the data.
+        StringBuilder dataReceived = new StringBuilder();
+        if (failedToConnect) {
+            LOGGER.info("Connection failed, stopping the container...");
+            if (null != error) {
+                error.printStackTrace();
+            }
+        } else {
+            try {
+                for (int i = 0; i < incomingDataLength / PythonOperationConstants.MAXBYTES; i++) {
+                    dataReceived.append(inputStream.readUTF());
+                }
+                dataReceived.append(inputStream.readUTF());
+            } catch (IOException e) {
+                LOGGER.info(e.getMessage());
+            }
+        }
+        return dataReceived;
     }
 
     @Override
     public void close() {
+        // Close the socket
+        if (clientSocket != null) {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                LOGGER.info(e.getMessage());
+            }
+        }
+    }
 
+    private DataInputStream getInputStream(final Socket clientSocket) throws IOException {
+        return new DataInputStream(clientSocket.getInputStream());
+    }
+
+    private void sleep(final Integer time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            LOGGER.info(e.getMessage());
+        }
     }
 }
