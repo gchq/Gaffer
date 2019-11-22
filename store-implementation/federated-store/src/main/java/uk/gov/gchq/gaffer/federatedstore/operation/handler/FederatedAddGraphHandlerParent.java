30335c72489cc605a0e7c03832ca8614918631e5
GCHQDeveloper404 <GCHQDev404@gmail.com>
2019-11-22 14:11:52 +0000

    gh-2210 FedStore improved add Handlers for delegate store operations

--- store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAddGraphHandlerParent.java
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAddGraphHandlerParent.java
@@ -16,6 +16,9 @@
 
 package uk.gov.gchq.gaffer.federatedstore.operation.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable;
 import uk.gov.gchq.gaffer.federatedstore.FederatedStore;
 import uk.gov.gchq.gaffer.federatedstore.exception.StorageException;
@@ -40,7 +43,7 @@ import uk.gov.gchq.gaffer.user.User;
  * @see GraphDelegate
  */
 public abstract class FederatedAddGraphHandlerParent<OP extends AddGraph> implements OperationHandler<OP> {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(FederatedAddGraphHandlerParent.class);
     public static final String ERROR_BUILDING_GRAPH_GRAPH_ID_S = "Error building graph %s";
     public static final String ERROR_ADDING_GRAPH_GRAPH_ID_S = "Error adding graph %s";
     public static final String USER_IS_LIMITED_TO_ONLY_USING_PARENT_PROPERTIES_ID_FROM_GRAPHLIBRARY_BUT_FOUND_STORE_PROPERTIES_S = "User is limited to only using parentPropertiesId from the graphLibrary, but found storeProperties: %s";
@@ -77,17 +80,23 @@ public abstract class FederatedAddGraphHandlerParent<OP extends AddGraph> implem
     protected void addGenericHandler(final FederatedStore store, final Graph graph) {
         for (final Class<? extends Operation> supportedOperation : graph.getSupportedOperations()) {
             //some operations are not suitable for FederatedOperationGenericOutputHandler
-            if (Output.class.isAssignableFrom(supportedOperation) && !store.isSupported(supportedOperation)) {
-                Class<? extends Output> supportedOutputOperation = (Class<? extends Output>) supportedOperation;
+            if (!store.isSupported(supportedOperation)) {
+                if (Output.class.isAssignableFrom(supportedOperation)) {
+                    Class<? extends Output> supportedOutputOperation = (Class<? extends Output>) supportedOperation;
 
-                Class outputClass;
-                try {
-                    outputClass = supportedOutputOperation.newInstance().getOutputClass();
-                } catch (final InstantiationException | IllegalAccessException e) {
-                    continue;
-                }
-                if (CloseableIterable.class.equals(outputClass)) {
-                    store.addOperationHandler((Class) supportedOutputOperation, new FederatedOperationIterableHandler());
+                    Class outputClass;
+                    try {
+                        outputClass = supportedOutputOperation.newInstance().getOutputClass();
+                    } catch (final InstantiationException | IllegalAccessException e) {
+                        continue;
+                    }
+                    if (CloseableIterable.class.equals(outputClass)) {
+                        store.addOperationHandler((Class) supportedOutputOperation, new FederatedOperationIterableHandler());
+                    } else {
+                        LOGGER.warn("No generic default handler can be used for an Output operation that does not return CloseableIterable. operation: " + supportedOutputOperation);
+                    }
+                } else {
+                    store.addOperationHandler(supportedOperation, new FederatedOperationHandler());
                 }
             }
         }
