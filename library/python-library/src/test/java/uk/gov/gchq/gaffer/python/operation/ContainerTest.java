174c7e76a1462d17366259a6218d7a41116ca13a
b492ddo <b492ddo@protonmail.com>
2019-11-27 16:15:10 +0000

    Fixed merge conflicts.

---
+++ library/python-library/src/test/java/uk/gov/gchq/gaffer/python/operation/ContainerTest.java
@@ -0,0 +1,80 @@
+///*
+// * Copyright 2019 Crown Copyright
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+package uk.gov.gchq.gaffer.python.operation;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ContainerTest {
+
+    @Test
+    public void shouldCreateContainer() {
+        // Given
+        setupTestServer();
+
+        LocalDockerContainer localDockerContainer = new LocalDockerContainer("");
+        ArrayList<String> inputData = new ArrayList<>();
+        inputData.add("Test Data");
+
+        // When
+        StringBuilder result = null;
+        try {
+            localDockerContainer.sendData(inputData, ScriptTestConstants.TEST_SERVER_PORT_3);
+            result = localDockerContainer.receiveData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Then
+        assert result != null;
+        Assert.assertEquals("Test Complete", result.toString());
+    }
+
+    private void setupTestServer() {
+        Runnable serverTask = () -> {
+            try (ServerSocket serverSocket = new ServerSocket(ScriptTestConstants.TEST_SERVER_PORT_3)) {
+                System.out.println("Waiting for clients to connect...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected.");
+                DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+                dos.writeBoolean(true);
+                dos.flush();
+                dis.readUTF();
+                dis.readUTF();
+                dos.writeInt(1);
+                dos.writeUTF("Test Complete");
+                serverSocket.close();
+                System.out.println("Closing Socket.");
+                dos.flush();
+            } catch (IOException e) {
+                System.err.println("Unable to process client request");
+                System.out.println("Unable to process client request");
+                e.printStackTrace();
+            }
+        };
+        final Thread serverThread = new Thread(serverTask);
+        serverThread.start();
+    }
+}
