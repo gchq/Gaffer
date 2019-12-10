044722b6f263ccd9d8b928743d2cc8aa7a72fff4
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2019-12-10 13:15:32 +0000

    gh-2214 FederatedAggregateHandler should Aggregate Duplicate Elements From Diff Stores (#2215)
    * gh-2214 FederatedAggregateHandler improvements
    
    * Update store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAggregateHandler.java

--- store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAggregateHandler.java
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAggregateHandler.java
@@ -16,7 +16,9 @@
 package uk.gov.gchq.gaffer.federatedstore.operation.handler;
 
 import uk.gov.gchq.gaffer.data.element.Element;
+import uk.gov.gchq.gaffer.data.elementdefinition.exception.SchemaException;
 import uk.gov.gchq.gaffer.federatedstore.FederatedStore;
+import uk.gov.gchq.gaffer.federatedstore.FederatedStoreConstants;
 import uk.gov.gchq.gaffer.operation.OperationException;
 import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
 import uk.gov.gchq.gaffer.store.Context;
@@ -40,6 +42,10 @@ public class FederatedAggregateHandler implements OutputOperationHandler<Aggrega
                                                    final Context context,
                                                    final Store store)
             throws OperationException {
-        return handler.doOperation(operation, ((FederatedStore) store).getSchema(operation, context));
+        try {
+            return handler.doOperation(operation, ((FederatedStore) store).getSchema(operation, context));
+        } catch (final SchemaException e) {
+            throw new OperationException("Unable to get the merged schema for the federated store, add graphId to Aggregate operation using option: " + FederatedStoreConstants.KEY_OPERATION_OPTIONS_GRAPH_IDS, e);
+        }
     }
 }
--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/impl/FederatedAggregateHandlerTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/impl/FederatedAggregateHandlerTest.java
@@ -18,14 +18,33 @@ package uk.gov.gchq.gaffer.federatedstore.operation.handler.impl;
 
 import org.junit.Test;
 
+import uk.gov.gchq.gaffer.accumulostore.MockAccumuloStore;
+import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable;
+import uk.gov.gchq.gaffer.data.element.Edge;
+import uk.gov.gchq.gaffer.data.element.Element;
 import uk.gov.gchq.gaffer.federatedstore.FederatedStore;
+import uk.gov.gchq.gaffer.federatedstore.FederatedStoreConstants;
+import uk.gov.gchq.gaffer.federatedstore.FederatedStoreProperties;
+import uk.gov.gchq.gaffer.federatedstore.operation.AddGraph;
 import uk.gov.gchq.gaffer.federatedstore.operation.handler.FederatedAggregateHandler;
+import uk.gov.gchq.gaffer.graph.Graph;
+import uk.gov.gchq.gaffer.graph.GraphConfig;
+import uk.gov.gchq.gaffer.operation.OperationChain;
 import uk.gov.gchq.gaffer.operation.OperationException;
+import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
 import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
+import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
 import uk.gov.gchq.gaffer.store.Context;
+import uk.gov.gchq.gaffer.store.StoreProperties;
 import uk.gov.gchq.gaffer.store.operation.handler.function.AggregateHandler;
 import uk.gov.gchq.gaffer.store.schema.Schema;
+import uk.gov.gchq.gaffer.store.schema.SchemaEdgeDefinition;
+import uk.gov.gchq.gaffer.user.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -53,5 +72,78 @@ public class FederatedAggregateHandlerTest {
         // Then
         assertSame(expectedResult, result);
         verify(handler).doOperation(op, schema);
+    }
+
+    @Test
+    public void shouldAggregateDuplicatesFromDiffStores() throws Exception {
+        final Graph fed = new Graph.Builder()
+                .config(new GraphConfig("fed"))
+                .addSchema(new Schema())
+                .storeProperties(new FederatedStoreProperties())
+                .build();
+
+        final StoreProperties storeProperties = new StoreProperties();
+        storeProperties.setStoreClass(MockAccumuloStore.class);
+
+        final Context context = new Context(new User());
+        fed.execute(new OperationChain.Builder()
+                .first(new AddGraph.Builder()
+                        .graphId("a")
+                        .schema(new Schema.Builder()
+                                .edge("edge", new SchemaEdgeDefinition.Builder()
+                                        .source("string")
+                                        .destination("string")
+                                        .build())
+                                .type("string", String.class)
+                                .build())
+                        .storeProperties(storeProperties)
+                        .build())
+                .then(new AddGraph.Builder()
+                        .graphId("b")
+                        .schema(new Schema.Builder()
+                                .edge("edge", new SchemaEdgeDefinition.Builder()
+                                        .source("string")
+                                        .destination("string")
+                                        .build())
+                                .type("string", String.class)
+                                .build())
+                        .storeProperties(storeProperties)
+                        .build())
+                .build(), context);
+
+        fed.execute(new AddElements.Builder()
+                .input(new Edge.Builder()
+                        .group("edge")
+                        .source("s1")
+                        .dest("d1")
+                        .build())
+                .option(FederatedStoreConstants.KEY_OPERATION_OPTIONS_GRAPH_IDS, "a")
+                .build(), context);
+
+        fed.execute(new AddElements.Builder()
+                .input(new Edge.Builder()
+                        .group("edge")
+                        .source("s1")
+                        .dest("d1")
+                        .build())
+                .option(FederatedStoreConstants.KEY_OPERATION_OPTIONS_GRAPH_IDS, "b")
+                .build(), context);
+
+        final CloseableIterable<? extends Element> getAll = fed.execute(new GetAllElements(), context);
+
+        List<Element> list = new ArrayList<>();
+        getAll.forEach(list::add);
+
+        assertEquals(2, list.size());
+
+        final Iterable<? extends Element> getAggregate = fed.execute(new OperationChain.Builder()
+                .first(new GetAllElements())
+                .then(new Aggregate())
+                .build(), context);
+
+        list.clear();
+        getAggregate.forEach(list::add);
+
+        assertEquals(1, list.size());
     }
 }
