d5e12317f7845921c75a197896532f2fad2b9b43
c015dariu <c015dariu@github.com>
2020-01-17 13:40:41 +0000

    Added more container logging

--- library/script-library/src/main/resources/.ScriptBin/setupHost.sh
+++ library/script-library/src/main/resources/.ScriptBin/setupHost.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo -e "`/sbin/ip route|awk '/default/ { print $3 }'`\tdocker.host.internal" | sudo tee -a /etc/hosts > /dev/null
+
+HOST_DOMAIN="host.docker.internal"
+ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
+  echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
+fi
+cat /etc/hosts
