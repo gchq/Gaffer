8210fd125f9d1b2f4ca3c921c77b9c4c36a9f490
c015dariu <c015dariu@github.com>
2020-01-17 13:07:43 +0000

    add file permissions

--- library/script-library/src/main/resources/.ScriptBin/entrypoint.py
+++ library/script-library/src/main/resources/.ScriptBin/entrypoint.py
@@ -4,7 +4,7 @@ import struct
 import re
 import sys
 import http.client
-from subprocess import call
+import subprocess
 from ast import literal_eval
 
 from DataInputStream import DataInputStream
@@ -16,12 +16,10 @@ print('scriptName is ', scriptName)
 print('scriptPort is', sys.argv[2])
 
 # Setup Host redirect if machine is running on Linux
-# process = subprocess.Popen(['./setupHost.sh'], shell=True)
-# process.wait()  # Wait for process to complete.
-
-with open('setupHost.sh', 'rb') as file:
-    script = file.read()
-rc = call(script, shell=True)
+correct = subprocess.run(['chmod', '+x', 'setupHost.sh'],
+                         check=True, text=True)
+process = subprocess.Popen(['./setupHost.sh'], shell=True)
+process.wait()  # Wait for process to complete.
 
 # Get the script parameters
 scriptParameters = sys.argv[3]
