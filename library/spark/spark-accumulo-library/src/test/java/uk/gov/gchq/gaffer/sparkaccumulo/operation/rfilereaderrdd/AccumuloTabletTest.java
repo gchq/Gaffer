6574b9103ebefcacef37beeaba9eedf9a70487a3
macenturalxl1 <macenturalxl1@tutanota.com>
2020-03-13 10:38:58 +0000

    Fixed warnings

--- library/spark/spark-accumulo-library/src/test/java/uk/gov/gchq/gaffer/sparkaccumulo/operation/rfilereaderrdd/AccumuloTabletTest.java
+++ library/spark/spark-accumulo-library/src/test/java/uk/gov/gchq/gaffer/sparkaccumulo/operation/rfilereaderrdd/AccumuloTabletTest.java
@@ -1,11 +1,13 @@
 package uk.gov.gchq.gaffer.sparkaccumulo.operation.rfilereaderrdd;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AccumuloTabletTest {
 
+    @DisplayName("Equals and hashcode test coverage")
     @Test
     public void testAccumuloTabletEquals() {
         final AccumuloTablet accumuloTablet = new AccumuloTablet(0, 0, "a", "b");
