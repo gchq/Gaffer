51968b37a683cb71e2a31a117b84e1dc891ff6ee
macenturalxl1 <macenturalxl1@tutanota.com>
2020-03-27 11:16:34 +0000

    Reverted UpdateTableIteratorTable to fix surefire

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
@@ -19,9 +19,7 @@ package uk.gov.gchq.gaffer.accumulostore.utils;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import uk.gov.gchq.gaffer.accumulostore.AccumuloProperties;
 import uk.gov.gchq.gaffer.commonutil.JsonAssert;
@@ -30,12 +28,9 @@ import uk.gov.gchq.gaffer.store.StoreProperties;
 import uk.gov.gchq.gaffer.store.library.FileGraphLibrary;
 import uk.gov.gchq.gaffer.store.schema.Schema;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Paths;
-import java.security.Permission;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -48,15 +43,11 @@ public class AddUpdateTableIteratorTest {
     private static final String SCHEMA_2_DIR = "src/test/resources/schema2";
     private static final String STORE_PROPS_PATH = "src/test/resources/store.properties";
     private static final String STORE_PROPS_2_PATH = "src/test/resources/store2.properties";
-    private static final String EMPTY_STORE_PROPS_PATH = "src/test/resources/empty-store.properties";
     private static final String FILE_GRAPH_LIBRARY_TEST_PATH = "target/graphLibrary";
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     @After
-    public void setUpAndTearDown() throws IOException {
+    public void cleanUp() throws IOException {
         if (new File(FILE_GRAPH_LIBRARY_TEST_PATH).exists()) {
             FileUtils.forceDelete(new File(FILE_GRAPH_LIBRARY_TEST_PATH));
         }
@@ -69,6 +60,7 @@ public class AddUpdateTableIteratorTest {
 
         // When
         AddUpdateTableIterator.main(args);
+
 
         // Then
         final Pair<Schema, StoreProperties> pair = new FileGraphLibrary(FILE_GRAPH_LIBRARY_TEST_PATH).get(GRAPH_ID);
@@ -108,76 +100,5 @@ public class AddUpdateTableIteratorTest {
         // Then - no exceptions
         final Pair<Schema, StoreProperties> pair = new FileGraphLibrary(FILE_GRAPH_LIBRARY_TEST_PATH).get(GRAPH_ID);
         assertNull("Graph should not have been stored", pair);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldThrowKeyErrorWhenInvalidModifyKeyGiven() throws Exception {
-        // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "invalid key", FILE_GRAPH_LIBRARY_TEST_PATH};
-
-        // When
-        AddUpdateTableIterator.main(args);
-
-        // Then
-        final String expected = "Supplied add or update key (invalid key) was not valid, it must either be add,remove or update.";
-        thrown.expectMessage(expected);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldReturnStoreClassNameNotFoundWhenStorePropsIsEmpty() throws Exception {
-        // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, EMPTY_STORE_PROPS_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
-
-        // When
-        AddUpdateTableIterator.main(args);
-
-        // Then
-        final String expected = "The Store class name was not found in the store properties for key: gaffer.store.class";
-        thrown.expectMessage(expected);
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void shouldReturnInvalidFilePathErrorWhenPathDoesNotExist() throws Exception {
-        // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, "invalid/file/path", "update", FILE_GRAPH_LIBRARY_TEST_PATH};
-
-        // When
-        AddUpdateTableIterator.main(args);
-
-        // Then
-        final String expected = "Failed to load store properties file : invalid/file/path";
-        thrown.expectMessage(expected);
-    }
-
-    @Test(expected = SecurityException.class)
-    public void whenExecutedWithLessThan3ArgsPrintlnWrongNoOfArgsError() throws Exception {
-        // Given
-        // Override System.exit when not enough args given
-        System.setSecurityManager(new OverrideSysExitSecurityManager());
-        ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-        System.setErr(new PrintStream(errContent));
-
-        final String[] threeArgs = {"1", "2", "3"};
-
-        // When
-        AddUpdateTableIterator.main(threeArgs);
-
-        // Then
-        final String expected = "Wrong number of arguments. \n" +
-                "Usage: <graphId> <comma separated schema paths> " +
-                "<store properties path> <add,remove or update> <file graph library path>\n";
-        assertEquals(expected, errContent.toString());
-    }
-
-    private class OverrideSysExitSecurityManager extends SecurityManager {
-        @Override
-        public void checkExit(int status) {
-            throw new SecurityException();
-        }
-
-        @Override
-        public void checkPermission(Permission perm) {
-            // Allow
-        }
     }
 }
