32fcea41bd6970093251468be6694b3cbf4c5f1d
macenturalxl1 <macenturalxl1@tutanota.com>
2020-05-15 14:37:02 +0100

    basic-model JUnit5

--- example/basic/basic-model/src/test/java/uk/gov/gchq/gaffer/basic/SchemaIT.java
+++ example/basic/basic-model/src/test/java/uk/gov/gchq/gaffer/basic/SchemaIT.java
@@ -15,31 +15,31 @@
  */
 package uk.gov.gchq.gaffer.basic;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
 import uk.gov.gchq.gaffer.graph.Graph;
 import uk.gov.gchq.gaffer.graph.GraphConfig;
 
-import java.io.IOException;
 import java.io.InputStream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class SchemaIT {
+
     @Test
-    public void shouldCreateGraphWithSchemaAndProperties() throws IOException {
+    public void shouldCreateGraphWithSchemaAndProperties() {
         // Given
         final InputStream storeProps = StreamUtil.openStream(getClass(), "/mockaccumulo.properties");
         final InputStream[] schema = StreamUtil.schemas(ElementGroup.class);
 
-        // When
-        new Graph.Builder()
+        // When / Then
+        assertDoesNotThrow(() -> new Graph.Builder()
                 .config(new GraphConfig.Builder()
                         .graphId("basicGraph")
                         .build())
                 .storeProperties(storeProps)
                 .addSchemas(schema)
-                .build();
-
-        // Then - no exceptions thrown
+                .build());
     }
 }
