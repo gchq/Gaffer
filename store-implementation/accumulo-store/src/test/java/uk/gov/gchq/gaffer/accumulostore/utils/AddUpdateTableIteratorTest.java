474d2f8ffe5c586bb0c854c6dc8db1dada446b2d
phosphorbronze50 <phosphorbronze50@protonmail.com>
2020-06-24 10:12:28 +0100

    Amendments for Mock to Mini Accumulo testing change.

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
@@ -18,12 +18,17 @@ package uk.gov.gchq.gaffer.accumulostore.utils;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import uk.gov.gchq.gaffer.accumulostore.AccumuloProperties;
+import uk.gov.gchq.gaffer.accumulostore.AccumuloTestClusterManager;
 import uk.gov.gchq.gaffer.commonutil.JsonAssert;
 import uk.gov.gchq.gaffer.commonutil.pair.Pair;
 import uk.gov.gchq.gaffer.store.StoreProperties;
@@ -31,8 +36,11 @@ import uk.gov.gchq.gaffer.store.library.FileGraphLibrary;
 import uk.gov.gchq.gaffer.store.schema.Schema;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -40,16 +48,51 @@ import static org.junit.Assert.assertNull;
 
 public class AddUpdateTableIteratorTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddUpdateTableIteratorTest.class);
+
     private static final String GRAPH_ID = "graphId";
     private static final String SCHEMA_DIR = "src/test/resources/schema";
     private static final String SCHEMA_2_DIR = "src/test/resources/schema2";
     private static final String STORE_PROPS_PATH = "src/test/resources/store.properties";
     private static final String STORE_PROPS_2_PATH = "src/test/resources/store2.properties";
+    private static final String STORE_PROPS_PATH_UPDATED = "src/test/resources/current_store.properties";
+    private static final String STORE_PROPS_2_PATH_UPDATED = "src/test/resources/current_store2.properties";
     private static final String EMPTY_STORE_PROPS_PATH = "src/test/resources/empty-store.properties";
     private static final String FILE_GRAPH_LIBRARY_TEST_PATH = "target/graphLibrary";
 
+    private static AccumuloTestClusterManager accumuloTestClusterManager1;
+    private static AccumuloTestClusterManager accumuloTestClusterManager2;
+    private static final AccumuloProperties PROPERTIES_1 = AccumuloProperties.loadStoreProperties(STORE_PROPS_PATH);
+    private static final AccumuloProperties PROPERTIES_2 = AccumuloProperties.loadStoreProperties(STORE_PROPS_2_PATH);
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
+
+    @BeforeClass
+    public static void setUpStore() {
+        accumuloTestClusterManager1 = new AccumuloTestClusterManager(PROPERTIES_1);
+        accumuloTestClusterManager2 = new AccumuloTestClusterManager(PROPERTIES_2);
+        createUpdatedPropertiesFile(PROPERTIES_1, STORE_PROPS_PATH_UPDATED);
+        createUpdatedPropertiesFile(PROPERTIES_2, STORE_PROPS_2_PATH_UPDATED);
+    }
+
+    @AfterClass
+    public static void tearDownStore() {
+        accumuloTestClusterManager1.close();
+        accumuloTestClusterManager2.close();
+    }
+
+    private static void createUpdatedPropertiesFile(AccumuloProperties accumuloProperties, String filename) {
+        Properties properties = accumuloProperties.getProperties();
+        try {
+            OutputStream fos = new FileOutputStream(filename);
+            properties.store(fos, "AddUpdateTableIteratorTest - " + filename+ " with current zookeeper");
+            fos.close();
+            FileUtils.forceDeleteOnExit(new File(filename));
+        } catch (IOException e) {
+            LOGGER.error("Failed to write Properties file: " + filename + ": " + e.getMessage());
+        }
+    }
 
     @Before
     @After
@@ -62,7 +105,7 @@ public class AddUpdateTableIteratorTest {
     @Test
     public void shouldRunMainWithFileGraphLibrary() throws Exception {
         // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
+        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH_UPDATED, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
 
         // When
         AddUpdateTableIterator.main(args);
@@ -73,14 +116,14 @@ public class AddUpdateTableIteratorTest {
         assertNotNull("Schema not found", pair.getFirst());
         assertNotNull("Store properties not found", pair.getSecond());
         JsonAssert.assertEquals(Schema.fromJson(Paths.get(SCHEMA_DIR)).toJson(false), pair.getFirst().toJson(false));
-        assertEquals(AccumuloProperties.loadStoreProperties(STORE_PROPS_PATH).getProperties(), pair.getSecond().getProperties());
+        assertEquals(AccumuloProperties.loadStoreProperties(STORE_PROPS_PATH_UPDATED).getProperties(), pair.getSecond().getProperties());
     }
 
     @Test
     public void shouldOverrideExistingGraphInGraphLibrary() throws Exception {
         // Given
         shouldRunMainWithFileGraphLibrary(); // load version graph version 1 into the library.
-        final String[] args = {GRAPH_ID, SCHEMA_2_DIR, STORE_PROPS_2_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
+        final String[] args = {GRAPH_ID, SCHEMA_2_DIR, STORE_PROPS_2_PATH_UPDATED, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
 
         // When
         AddUpdateTableIterator.main(args);
@@ -91,13 +134,13 @@ public class AddUpdateTableIteratorTest {
         assertNotNull("Schema not found", pair.getFirst());
         assertNotNull("Store properties not found", pair.getSecond());
         JsonAssert.assertEquals(Schema.fromJson(Paths.get(SCHEMA_2_DIR)).toJson(false), pair.getFirst().toJson(false));
-        assertEquals(AccumuloProperties.loadStoreProperties(STORE_PROPS_2_PATH).getProperties(), pair.getSecond().getProperties());
+        assertEquals(AccumuloProperties.loadStoreProperties(STORE_PROPS_2_PATH_UPDATED).getProperties(), pair.getSecond().getProperties());
     }
 
     @Test
     public void shouldRunMainWithNoGraphLibrary() throws Exception {
         // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "update"};
+        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH_UPDATED, "update"};
 
         // When
         AddUpdateTableIterator.main(args);
@@ -110,7 +153,7 @@ public class AddUpdateTableIteratorTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowKeyErrorWhenInvalidModifyKeyGiven() throws Exception {
         // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "invalid key", FILE_GRAPH_LIBRARY_TEST_PATH};
+        final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH_UPDATED, "invalid key", FILE_GRAPH_LIBRARY_TEST_PATH};
 
         // When
         AddUpdateTableIterator.main(args);
