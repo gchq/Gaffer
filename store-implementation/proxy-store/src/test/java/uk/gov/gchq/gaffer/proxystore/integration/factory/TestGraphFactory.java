0cac60e0369a54b2fbf7e2e3fd52764644a199e4
d47853 <d47853@users.noreply.github.com>
2021-01-08 13:14:52 +0000

    removed old dependency from map and proxy stores

--- store-implementation/proxy-store/src/test/java/uk/gov/gchq/gaffer/proxystore/integration/factory/TestGraphFactory.java
+++ store-implementation/proxy-store/src/test/java/uk/gov/gchq/gaffer/proxystore/integration/factory/TestGraphFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.gaffer.proxystore.integration.factory;
 
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
@@ -11,6 +27,9 @@ import uk.gov.gchq.gaffer.store.schema.Schema;
 public class TestGraphFactory implements GraphFactory {
     private static final StoreProperties MAP_STORE_PROPERTIES = MapStoreProperties.loadStoreProperties(StreamUtil.openStream(TestGraphFactory.class, "/map-store.properties"));
     private static final Schema SCHEMA = Schema.fromJson(StreamUtil.schema(TestGraphFactory.class));
+
+    private Graph graph;
+
     @Override
     public Graph.Builder createGraphBuilder() {
         return new Graph.Builder()
@@ -21,7 +40,14 @@ public class TestGraphFactory implements GraphFactory {
 
     @Override
     public Graph getGraph() {
-        return createGraphBuilder().build();
+        if (graph == null) {
+            graph = createGraphBuilder().build();
+        }
+        return graph;
+    }
+
+    public void reset() {
+        graph = createGraphBuilder().build();
     }
 
 }
