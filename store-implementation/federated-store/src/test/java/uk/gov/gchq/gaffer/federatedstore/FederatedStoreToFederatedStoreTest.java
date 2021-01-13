bd43e4aefae191de2698941e64307a735121843c
d47853 <d47853@users.noreply.github.com>
2021-01-13 18:48:38 +0000

    gh-2008 Added more tests

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreToFederatedStoreTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreToFederatedStoreTest.java
@@ -20,6 +20,7 @@ import com.google.common.collect.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import uk.gov.gchq.gaffer.data.element.Edge;
 import uk.gov.gchq.gaffer.data.element.Element;
 import uk.gov.gchq.gaffer.data.element.Entity;
 import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
@@ -37,6 +38,9 @@ import uk.gov.gchq.gaffer.user.User;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static uk.gov.gchq.gaffer.federatedstore.integration.FederatedViewsIT.BASIC_EDGE;
 import static uk.gov.gchq.gaffer.federatedstore.integration.FederatedViewsIT.BASIC_ENTITY;
 
 /**
@@ -87,6 +91,71 @@ public class FederatedStoreToFederatedStoreTest {
             .graphId("RestProxy")
             .schema(new Schema())
             .build(), new User());
+    }
+
+    @Test
+    public void shouldErrorIfViewIsInvalid() throws OperationException {
+        // Given
+        Entity entity = new Entity.Builder()
+            .group(BASIC_ENTITY)
+            .vertex("myVertex")
+            .property("property1", 1)
+            .build();
+
+        restApiFederatedGraph.execute(new AddElements.Builder()
+            .input(entity)
+            .build(), new User());
+
+        // When
+        OperationException e = assertThrows(OperationException.class, () -> federatedStoreGraph.execute(new GetAllElements.Builder()
+            .view(new View.Builder()
+                .edge(BASIC_EDGE)
+                .build())
+            .build(), new User()));
+
+        assertTrue(e.getMessage().contains("View is not valid for graphIds:[mapStore]"));
+    }
+
+    @Test
+    public void shouldMaintainView() throws OperationException {
+        // Given
+        final String mapStoreGraphId = "mapStoreWithFullSchema";
+        restApiFederatedGraph.execute(new AddGraph.Builder()
+            .storeProperties(new MapStoreProperties())
+            .graphId(mapStoreGraphId)
+            .schema(Schema.fromJson(getClass().getResourceAsStream("/schema/basicEntitySchema.json"),
+                getClass().getResourceAsStream("/schema/basicEdgeSchema.json")))
+            .build(), new User());
+
+        Entity entity = new Entity.Builder()
+            .group(BASIC_ENTITY)
+            .vertex("myVertex")
+            .property("property1", 1)
+            .build();
+
+        Edge edge = new Edge.Builder()
+            .source("mySource")
+            .dest("myDest")
+            .group(BASIC_EDGE)
+            .property("columnQualifier", 2)
+            .property("prooperty1", 1)
+            .build();
+
+        restApiFederatedGraph.execute(new AddElements.Builder()
+            .input(entity, edge)
+            .option(FederatedStoreConstants.KEY_OPERATION_OPTIONS_GRAPH_IDS, mapStoreGraphId)
+            .build(), new User());
+
+        // When
+        List<? extends Element> results = Lists.newArrayList(federatedStoreGraph.execute(new GetAllElements.Builder()
+            .view(new View.Builder()
+                .entity(BASIC_ENTITY)
+                .build())
+            .build(), new User()));
+
+        // Then
+        assertEquals(1, results.size());
+
     }
 
     @Test
