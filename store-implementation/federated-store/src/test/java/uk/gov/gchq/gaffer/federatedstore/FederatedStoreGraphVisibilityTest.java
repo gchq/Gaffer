ba266dd895d2f84291ef2952096c5502ec9a3fb4
d47853 <d47853@users.noreply.github.com>
2021-01-28 15:11:44 +0000

    Fixed checkstyle

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreGraphVisibilityTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreGraphVisibilityTest.java
@@ -17,9 +17,7 @@
 package uk.gov.gchq.gaffer.federatedstore;
 
 import com.google.common.collect.Sets;
-import org.apache.commons.jcs.engine.CacheElementSerialized;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
