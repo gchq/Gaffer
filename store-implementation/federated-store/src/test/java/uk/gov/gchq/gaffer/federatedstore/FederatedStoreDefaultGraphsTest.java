22a556ce0aa5b3f7a448e346079fa9dba810ace1
GCHQDev404 <gchqdev404@users.noreply.github.com>
2021-05-17 04:46:53 -0700

    gh-2357 FederatedStore FederatedOperation.v02 removing TODOs.

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreDefaultGraphsTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreDefaultGraphsTest.java
@@ -17,7 +17,7 @@
 package uk.gov.gchq.gaffer.federatedstore;
 
 import org.apache.commons.io.IOUtils;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
 import uk.gov.gchq.gaffer.federatedstore.operation.GetAllGraphInfo;
