43571b46a86f2b7168e34d4437eec72ec21ce89e
t92549 <80890692+t92549@users.noreply.github.com>
2021-06-22 06:00:17 -0700

    Allowed merging of schemas when one is a subset of another

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreSchemaTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreSchemaTest.java
@@ -16,7 +16,6 @@
 
 package uk.gov.gchq.gaffer.federatedstore;
 
-import com.google.common.collect.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -24,20 +23,36 @@ import uk.gov.gchq.gaffer.accumulostore.AccumuloProperties;
 import uk.gov.gchq.gaffer.cache.CacheServiceLoader;
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
 import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterable;
+import uk.gov.gchq.gaffer.commonutil.iterable.CloseableIterator;
+import uk.gov.gchq.gaffer.data.element.Edge;
 import uk.gov.gchq.gaffer.data.element.Element;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.ViewElementDefinition;
 import uk.gov.gchq.gaffer.federatedstore.operation.AddGraph;
 import uk.gov.gchq.gaffer.operation.OperationChain;
+import uk.gov.gchq.gaffer.operation.OperationException;
+import uk.gov.gchq.gaffer.operation.data.EntitySeed;
+import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
+import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
 import uk.gov.gchq.gaffer.store.Context;
-import uk.gov.gchq.gaffer.store.library.HashMapGraphLibrary;
+import uk.gov.gchq.gaffer.store.operation.GetSchema;
 import uk.gov.gchq.gaffer.store.schema.Schema;
 import uk.gov.gchq.gaffer.store.schema.SchemaEdgeDefinition;
 import uk.gov.gchq.gaffer.store.schema.TypeDefinition;
 import uk.gov.gchq.gaffer.user.User;
+import uk.gov.gchq.koryphe.ValidationResult;
 import uk.gov.gchq.koryphe.impl.binaryoperator.StringConcat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static uk.gov.gchq.gaffer.store.TestTypes.DIRECTED_EITHER;
 import static uk.gov.gchq.gaffer.user.StoreUser.testUser;
 
@@ -53,12 +68,9 @@ public class FederatedStoreSchemaTest {
     public User testUser;
     public Context testContext;
     public static final String TEST_FED_STORE = "testFedStore";
-    public static final HashMapGraphLibrary LIBRARY = new HashMapGraphLibrary();
-    public static final String ACC_PROP = "accProp";
 
     private FederatedStore fStore;
     private static final FederatedStoreProperties FEDERATED_PROPERTIES = new FederatedStoreProperties();
-    private static final String CACHE_SERVICE_CLASS_STRING = "uk.gov.gchq.gaffer.cache.impl.HashMapCacheService";
 
     private static Class currentClass = new Object() { }.getClass().getEnclosingClass();
     private static final AccumuloProperties PROPERTIES = AccumuloProperties.loadStoreProperties(StreamUtil.openStream(currentClass, "properties/accumuloStore.properties"));
@@ -70,84 +82,43 @@ public class FederatedStoreSchemaTest {
         fStore = new FederatedStore();
         fStore.initialise(TEST_FED_STORE, null, FEDERATED_PROPERTIES);
 
-        fStore.setGraphLibrary(LIBRARY);
-
         testUser = testUser();
         testContext = new Context(testUser);
     }
 
     @Test
     public void shouldBeAbleToAddGraphsWithSchemaCollisions() throws Exception {
-        LIBRARY.addProperties(ACC_PROP, PROPERTIES);
-        fStore.setGraphLibrary(LIBRARY);
+        // Given
+        addGroupCollisionGraphs();
 
-        String aSchema1ID = "aSchema";
         final Schema aSchema = new Schema.Builder()
                 .edge("e1", getProp("prop1"))
                 .type(DIRECTED_EITHER, Boolean.class)
                 .merge(STRING_SCHEMA)
                 .build();
 
-        LIBRARY.addSchema(aSchema1ID, aSchema);
-
-        fStore.execute(OperationChain.wrap(
-                new AddGraph.Builder()
-                        .graphId("a")
-                        .parentPropertiesId(ACC_PROP)
-                        .parentSchemaIds(Lists.newArrayList(aSchema1ID))
-                        .build()), testContext);
-
-        String bSchema1ID = "bSchema";
-        final Schema bSchema = new Schema.Builder()
-                .edge("e1", getProp("prop2"))
-                .type(DIRECTED_EITHER, Boolean.class)
-                .merge(STRING_SCHEMA)
-                .build();
-
-        LIBRARY.addSchema(bSchema1ID, bSchema);
-
-        assertFalse(LIBRARY.exists("b"));
-
-        fStore.execute(OperationChain.wrap(new AddGraph.Builder()
-                .graphId("b")
-                .parentPropertiesId(ACC_PROP)
-                .parentSchemaIds(Lists.newArrayList(bSchema1ID))
-                .build()), testContext);
-
-        fStore.execute(OperationChain.wrap(new AddGraph.Builder()
+        fStore.execute(new AddGraph.Builder()
                 .graphId("c")
-                .parentPropertiesId(ACC_PROP)
-                .parentSchemaIds(Lists.newArrayList(aSchema1ID))
-                .build()), testContext);
+                .schema(aSchema)
+                .storeProperties(PROPERTIES)
+                .build(), testContext);
+        // When
+        Collection<String> graphIds = fStore.getAllGraphIds(testUser);
 
-        // No exceptions thrown - as all 3 graphs should be able to be added together.
+        // Then
+        HashSet<String> expected = new HashSet<>();
+        expected.addAll(Arrays.asList("a", "b", "c"));
+
+        assertTrue(expected.equals(graphIds));
     }
 
     @Test
     public void shouldGetCorrectDefaultViewForAChosenGraphOperation() throws
             Exception {
-        final HashMapGraphLibrary library = new HashMapGraphLibrary();
-        library.add("a", new Schema.Builder()
-                .edge("e1", getProp("prop1"))
-                .type(DIRECTED_EITHER, Boolean.class)
-                .merge(STRING_SCHEMA)
-                .build(), PROPERTIES);
+        // Given
+        addGroupCollisionGraphs();
 
-        library.add("b", new Schema.Builder()
-                .edge("e1", getProp("prop2"))
-                .type(DIRECTED_EITHER, Boolean.class)
-                .merge(STRING_SCHEMA)
-                .build(), PROPERTIES);
-
-
-        fStore.execute(new AddGraph.Builder()
-                .graphId("a")
-                .build(), testContext);
-
-        fStore.execute(new AddGraph.Builder()
-                .graphId("b")
-                .build(), testContext);
-
+        // When
         final CloseableIterable<? extends Element> a = fStore.execute(new OperationChain.Builder()
                 .first(new GetAllElements.Builder()
                         //No view so makes default view, should get only view compatible with graph "a"
@@ -155,9 +126,72 @@ public class FederatedStoreSchemaTest {
                         .build())
                 .build(), testContext);
 
+        // Then
         assertNotNull(a);
         assertFalse(a.iterator().hasNext());
     }
+
+    @Test
+    public void shouldBeAbleToGetElementsWithOverlappingSchemas() throws
+            OperationException {
+        // Given
+        addOverlappingPropertiesGraphs();
+
+        fStore.execute(new AddElements.Builder()
+                .input(new Edge.Builder()
+                        .group("e1")
+                        .source("source1")
+                        .dest("dest1")
+                        .property("prop1", "value1")
+                        .build())
+                .build(), testContext);
+
+        fStore.execute(new AddElements.Builder()
+                .input(new Edge.Builder()
+                        .group("e1")
+                        .source("source1")
+                        .dest("dest1")
+                        .property("prop1", "value1")
+                        .property("prop2", "value2")
+                        .build())
+                .build(), testContext);
+
+        // When
+        final CloseableIterable<? extends Element> a = fStore.execute(new GetElements.Builder()
+                .input(new EntitySeed("source1"))
+                .view(new View.Builder()
+                        .edge("e1", new ViewElementDefinition.Builder()
+                                .properties("prop2")
+                                .build())
+                        .build())
+                .build(), testContext);
+
+        assertNotNull(a);
+        final ArrayList<Element> results = new ArrayList<>();
+        final CloseableIterator<? extends Element> aIterator = a.iterator();
+        while (aIterator.hasNext()) {
+            Element result = aIterator.next();
+            results.add(result);
+        }
+
+        // Then
+        assertEquals(results.size(), 2);
+    }
+
+    @Test
+    public void shouldBeAbleToGetSchemaWithOverlappingSchemas() throws
+            OperationException {
+        // Given
+        addOverlappingPropertiesGraphs();
+
+        // When
+        final Schema s = fStore.execute(new GetSchema.Builder()
+                .build(), testContext);
+
+        // Then
+        assertEquals(s.validate(), new ValidationResult());
+    }
+
 
     private SchemaEdgeDefinition getProp(final String propName) {
         return new SchemaEdgeDefinition.Builder()
@@ -166,5 +200,69 @@ public class FederatedStoreSchemaTest {
                 .directed(DIRECTED_EITHER)
                 .property(propName, STRING)
                 .build();
+    }
+
+    private void addGroupCollisionGraphs() throws OperationException {
+        final Schema aSchema = new Schema.Builder()
+                .edge("e1", getProp("prop1"))
+                .type(DIRECTED_EITHER, Boolean.class)
+                .merge(STRING_SCHEMA)
+                .build();
+
+        fStore.execute(OperationChain.wrap(
+                new AddGraph.Builder()
+                        .graphId("a")
+                        .schema(aSchema)
+                        .storeProperties(PROPERTIES)
+                        .build()), testContext);
+
+        final Schema bSchema = new Schema.Builder()
+                .edge("e1", getProp("prop2"))
+                .type(DIRECTED_EITHER, Boolean.class)
+                .merge(STRING_SCHEMA)
+                .build();
+
+        fStore.execute(new AddGraph.Builder()
+                .graphId("b")
+                .schema(bSchema)
+                .storeProperties(PROPERTIES)
+                .build(), testContext);
+    }
+
+    private void addOverlappingPropertiesGraphs() throws OperationException {
+        final Schema aSchema = new Schema.Builder()
+                .edge("e1", new SchemaEdgeDefinition.Builder()
+                        .source(STRING)
+                        .destination(STRING)
+                        .directed(DIRECTED_EITHER)
+                        .property("prop1", STRING)
+                        .build())
+                .type(DIRECTED_EITHER, Boolean.class)
+                .merge(STRING_SCHEMA)
+                .build();
+
+        final Schema bSchema = new Schema.Builder()
+                .edge("e1", new SchemaEdgeDefinition.Builder()
+                        .source(STRING)
+                        .destination(STRING)
+                        .directed(DIRECTED_EITHER)
+                        .property("prop1", STRING)
+                        .property("prop2", STRING)
+                        .build())
+                .type(DIRECTED_EITHER, Boolean.class)
+                .merge(STRING_SCHEMA)
+                .build();
+
+        fStore.execute(new AddGraph.Builder()
+                .graphId("a")
+                .schema(aSchema)
+                .storeProperties(PROPERTIES)
+                .build(), testContext);
+
+        fStore.execute(new AddGraph.Builder()
+                .graphId("b")
+                .schema(bSchema)
+                .storeProperties(PROPERTIES)
+                .build(), testContext);
     }
 }
