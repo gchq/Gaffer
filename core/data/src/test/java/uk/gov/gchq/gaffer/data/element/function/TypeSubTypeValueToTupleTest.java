da65010290281acb1cfaa8b2ccbd419ffe2b62c8
t92549 <t92549@users.noreply.github.com>
2021-07-29 17:39:01 +0100

    Fix checkstyle

--- core/data/src/test/java/uk/gov/gchq/gaffer/data/element/function/TypeSubTypeValueToTupleTest.java
+++ core/data/src/test/java/uk/gov/gchq/gaffer/data/element/function/TypeSubTypeValueToTupleTest.java
@@ -35,7 +35,6 @@ import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class TypeSubTypeValueToTupleTest extends FunctionTest<TypeSubTypeValueToTuple> {
 
