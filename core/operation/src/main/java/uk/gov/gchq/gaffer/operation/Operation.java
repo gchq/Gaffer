4210bd35626900297f22361a10d471c7c46f5f28
GCHQDev404 <gchqdev404@users.noreply.github.com>
2021-08-19 08:06:00 -0700

    gh-2487 Generic Operation new Operation.

--- core/operation/src/main/java/uk/gov/gchq/gaffer/operation/Operation.java
+++ core/operation/src/main/java/uk/gov/gchq/gaffer/operation/Operation.java
@@ -16,15 +16,19 @@
 
 package uk.gov.gchq.gaffer.operation;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.exception.CloneFailedException;
 
 import uk.gov.gchq.gaffer.commonutil.Required;
+import uk.gov.gchq.koryphe.Since;
+import uk.gov.gchq.koryphe.Summary;
 import uk.gov.gchq.koryphe.ValidationResult;
 import uk.gov.gchq.koryphe.serialisation.json.JsonSimpleClassName;
 
@@ -34,62 +38,92 @@ import java.lang.reflect.Field;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeMap;
 
-import static java.util.Objects.isNull;
-
-/**
- * An {@code Operation} defines an operation to be processed on a graph.
- * All operations must to implement this interface.
- * Operations should be written to be as generic as possible to allow them to be applied to different graph/stores.
- * NOTE - operations should not contain the operation logic. The logic should be separated out into a operation handler.
- * This will allow you to execute the same operation on different stores with different handlers.
- * <p>
- * Operations must be JSON serialisable in order to make REST API calls.
- * </p>
- * <p>
- * Any fields that are required should be annotated with the {@link Required} annotation.
- * </p>
- * <p>
- * Operation implementations need to implement this Operation interface and any of the following interfaces they wish to make use of:
- * {@link uk.gov.gchq.gaffer.operation.io.Input}
- * {@link uk.gov.gchq.gaffer.operation.io.Output}
- * {@link uk.gov.gchq.gaffer.operation.io.InputOutput} (Use this instead of Input and Output if your operation takes both input and output.)
- * {@link uk.gov.gchq.gaffer.operation.io.MultiInput} (Use this in addition if you operation takes multiple inputs. This will help with json  serialisation)
- * {@link uk.gov.gchq.gaffer.operation.SeedMatching}
- * {@link uk.gov.gchq.gaffer.operation.Validatable}
- * {@link uk.gov.gchq.gaffer.operation.graph.OperationView}
- * {@link uk.gov.gchq.gaffer.operation.graph.GraphFilters}
- * {@link uk.gov.gchq.gaffer.operation.graph.SeededGraphFilters}
- * </p>
- * <p>
- * Each Operation implementation should have a corresponding unit test class
- * that extends the OperationTest class.
- * </p>
- * <p>
- * Implementations should override the close method and ensure all closeable fields are closed.
- * </p>
- * <p>
- * All implementations should also have a static inner Builder class that implements
- * the required builders. For example:
- * </p>
- * <pre>
- * public static class Builder extends Operation.BaseBuilder&lt;GetElements, Builder&gt;
- *         implements InputOutput.Builder&lt;GetElements, Iterable&lt;? extends ElementId&gt;, CloseableIterable&lt;? extends Element&gt;, Builder&gt;,
- *         MultiInput.Builder&lt;GetElements, ElementId, Builder&gt;,
- *         SeededGraphFilters.Builder&lt;GetElements, Builder&gt;,
- *         SeedMatching.Builder&lt;GetElements, Builder&gt; {
- *     public Builder() {
- *             super(new GetElements());
- *     }
- * }
- * </pre>
- */
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = As.PROPERTY, property = "class", defaultImpl = OperationChain.class)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
+@JsonPropertyOrder(value = {"class", "id", "operationArgs"}, alphabetic = true)
 @JsonSimpleClassName(includeSubtypes = true)
-public interface Operation extends Closeable {
+@Since("0.0.1")
+@Summary("An Operation which contains an Id and a mapping of args to be used by handlers associated by the Id.")
+public class Operation implements Closeable {
+    private final String id;
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
+    @JsonPropertyOrder(value = {"class"}, alphabetic = true)
+    private Map<String, Object> operationArgs = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    public static final Locale LOCALE = Locale.ENGLISH;
+
+    public Operation(final String id) {
+        this.id = id;
+    }
+
+    @JsonCreator
+    public Operation(@JsonProperty("id") final String id, @JsonProperty("operationArgs") final Map<String, Object> operationArgs) {
+        this.id = id;
+        if (Objects.nonNull(operationArgs)) {
+            this.operationArgs = operationArgs;
+        }
+    }
+
+    public boolean containsKey(final String key) {
+        return operationArgs.containsKey(key);
+    }
+
+    public Operation operationArgs(final Map<String, Object> operationsArgs) {
+        this.operationArgs = operationsArgs;
+        return this;
+    }
+
+    public Map<String, Object> getOperationArgs() {
+        return operationArgs;
+    }
+
+    public Operation addOperationArgs(final Map<String, Object> operationsArgs) {
+        this.operationArgs.putAll(operationsArgs);
+        return this;
+    }
+
+    public Operation operationArg(final String operationArg, final Object value) {
+        this.operationArgs.put(operationArg, value);
+        return this;
+    }
+
+    public Operation inputOperationArg(final Object value) {
+        return this.operationArg("input", value);
+    }
+
+    public Object get(final String key) {
+        return operationArgs.get(key);
+    }
+
+    public Operation input(final Object input) {
+        return inputOperationArg(input);
+    }
+
+    public Object input() {
+        return get("input");
+    }
+
+    public Object getOrDefault(final String key, final Object defaultValue) {
+        return operationArgs.getOrDefault(key, defaultValue);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Boolean getIdComparison(final String s) {
+        return getId().toLowerCase(LOCALE).equals(s.toLowerCase(Locale.ENGLISH));
+    }
+
+    public Set<String> keySet() {
+        return ImmutableSet.copyOf(operationArgs.keySet());
+    }
+
     /**
      * Operation implementations should ensure a ShallowClone method is implemented.
      * Performs a shallow clone. Creates a new instance and copies the fields across.
@@ -99,71 +133,9 @@ public interface Operation extends Closeable {
      * @return shallow clone
      * @throws CloneFailedException if a Clone error occurs
      */
-    Operation shallowClone() throws CloneFailedException;
-
-    /**
-     * @return the operation options. This may contain store specific options such as authorisation strings or and
-     * other properties required for the operation to be executed. Note these options will probably not be interpreted
-     * in the same way by every store implementation.
-     */
-    @JsonIgnore
-    Map<String, String> getOptions();
-
-    /**
-     * @param options the operation options. This may contain store specific options such as authorisation strings or and
-     *                other properties required for the operation to be executed. Note these options will probably not be interpreted
-     *                in the same way by every store implementation.
-     */
-    @JsonSetter
-    void setOptions(final Map<String, String> options);
-
-    /**
-     * Adds an operation option. This may contain store specific options such as authorisation strings or and
-     * other properties required for the operation to be executed. Note these options will probably not be interpreted
-     * in the same way by every store implementation.
-     *
-     * @param name  the name of the option
-     * @param value the value of the option
-     */
-    default void addOption(final String name, final String value) {
-        if (isNull(getOptions())) {
-            setOptions(new HashMap<>());
-        }
-        getOptions().put(name, value);
-    }
-
-    /**
-     * Gets an operation option by its given name.
-     *
-     * @param name the name of the option
-     * @return the value of the option
-     */
-    default String getOption(final String name) {
-        return isNull(getOptions())
-                ? null
-                : getOptions().get(name);
-    }
-
-    /**
-     * Gets an operation option by its given name.
-     *
-     * @param name         the name of the option
-     * @param defaultValue the default value to return if value is null.
-     * @return the value of the option
-     */
-    default String getOption(final String name, final String defaultValue) {
-        return (isNull(getOptions()))
-                ? defaultValue
-                : getOptions().getOrDefault(name, defaultValue);
-    }
-
-    @JsonGetter("options")
-    default Map<String, String> _getNullOrOptions() {
-        if (null == getOptions()) {
-            return null;
-        }
-
-        return getOptions().isEmpty() ? null : getOptions();
+    public Operation shallowClone() throws CloneFailedException {
+        return new Operation(id)
+                .operationArgs(operationArgs);
     }
 
     /**
@@ -171,21 +143,14 @@ public interface Operation extends Closeable {
      *
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    default void close() throws IOException {
+    public void close() throws IOException {
         // do nothing by default
     }
 
-    /**
-     * Validates an operation. This should be used to validate that fields have been be configured correctly.
-     * By default no validation is applied. Override this method to implement validation.
-     *
-     * @return validation result.
-     */
-    default ValidationResult validate() {
+    public ValidationResult validate() {
         final ValidationResult result = new ValidationResult();
 
-        HashSet<Field> fields = Sets.<Field>newHashSet();
+        HashSet<Field> fields = Sets.newHashSet();
         Class<?> currentClass = this.getClass();
         while (null != currentClass) {
             fields.addAll(Arrays.asList(currentClass.getDeclaredFields()));
@@ -210,7 +175,7 @@ public interface Operation extends Closeable {
         return result;
     }
 
-    default void validateRequiredFieldPresent(final ValidationResult result, final Field field) {
+    public void validateRequiredFieldPresent(final ValidationResult result, final Field field) {
         final Object value;
         try {
             value = field.get(this);
@@ -223,74 +188,60 @@ public interface Operation extends Closeable {
         }
     }
 
-    /**
-     * This has been replaced with {@link OperationChain#wrap(Operation)}
-     *
-     * @param operation the operation to wrap into a chain
-     * @param <O>       the output type of the operation chain
-     * @return the operation chain
-     * @deprecated see {@link OperationChain#wrap(Operation)}
-     */
-    @Deprecated
-    static <O> OperationChain<O> asOperationChain(final Operation operation) {
-        return (OperationChain<O>) OperationChain.wrap(operation);
-    }
-
-    interface Builder<OP, B extends Builder<OP, ?>> {
-        OP _getOp();
-
-        B _self();
-    }
-
-    abstract class BaseBuilder<OP extends Operation, B extends BaseBuilder<OP, ?>>
-            implements Builder<OP, B> {
-        private OP op;
-
-        protected BaseBuilder(final OP op) {
-            this.op = op;
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
         }
 
-        /**
-         * @param name  the name of the option to add
-         * @param value the value of the option to add
-         * @return this Builder
-         * @see Operation#addOption(String, String)
-         */
-        public B option(final String name, final String value) {
-            _getOp().addOption(name, value);
-            return _self();
+        if (o == null || getClass() != o.getClass()) {
+            return false;
         }
 
-        public B options(final Map<String, String> options) {
-            if (null != options) {
-                if (null == _getOp().getOptions()) {
-                    _getOp().setOptions(new HashMap<>(options));
+        final Operation that = (Operation) o;
+
+        final EqualsBuilder equalsBuilder = new EqualsBuilder()
+                .append(id, that.id)
+                .append(operationArgs.size(), that.operationArgs.size());
+
+
+        if (equalsBuilder.isEquals()) {
+            boolean mapsAreEqual = true;
+            // final boolean mapsAreEqual =
+            //         operationArgs.entrySet().stream()
+            //                 .allMatch(e -> that.containsKey(e.getKey())
+            //                         && ( that.get(e.getKey()).equals(e.getValue()))
+            //                 || e.getValue() instanceof Arrays  );
+
+
+            for (final Map.Entry<String, Object> entry : this.operationArgs.entrySet()) {
+                final String thisKey = entry.getKey();
+                final boolean b = that.operationArgs.containsKey(thisKey);
+                if (!b) {
+                    mapsAreEqual = false;
+                    break;
                 } else {
-                    _getOp().getOptions().putAll(options);
+                    final Object thisValue = entry.getValue();
+                    final Object thatValue = operationArgs.get(thisKey);
+                    if (!thisValue.equals(thatValue)) {
+                        mapsAreEqual = false;
+                        break;
+                    }
                 }
             }
-            return _self();
+
+
+            equalsBuilder.appendSuper(mapsAreEqual);
         }
 
-        /**
-         * Builds the operation and returns it.
-         *
-         * @return the built operation.
-         */
-        public OP build() {
-            return _getOp();
-        }
+        return equalsBuilder.isEquals();
+    }
 
-        @Override
-        public OP _getOp() {
-            return op;
-        }
-
-        @Override
-        public B _self() {
-            return (B) this;
-        }
-
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(operationArgs)
+                .toHashCode();
     }
 }
-
