2a466b8a28f49fddd1a41e6cc38becf3af1cb02d
r32575 <63105662+r32575@users.noreply.github.com>
2022-04-12 18:08:18 +0100

    Re-added incorrectly deleted empty line

--- core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/iterable/ChainedIterableTest.java
+++ core/common-util/src/test/java/uk/gov/gchq/gaffer/commonutil/iterable/ChainedIterableTest.java
@@ -20,6 +20,7 @@ import com.google.common.collect.Lists;
 import org.junit.jupiter.api.Test;
 
 import uk.gov.gchq.gaffer.commonutil.CloseableUtil;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
