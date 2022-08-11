d04d1a94072f2af4a2c97e4e40665ad4ece0a50a
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2022-08-11 11:36:29 +0100

    FederatedStore Alpha4 staging: pt2 Chain and Output (#2727)
    * FederatedStore Alpha4 staging: Chain and Output
    
    * FederatedStore Alpha4 staging: FederatedOperation and Federated Interfaces PR Update. (#2728)
    
    Co-authored-by: GCHQDev404 <GCHQDev404@users.noreply.github.com>
    
    * Update store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/impl/FederatedOperationHandler.java
    
    * FederatedStore Alpha4 staging: FederatedOperation and Federated Interfaces PR Update. (#2730)
    
    Co-authored-by: GCHQDev404 <GCHQDev404@users.noreply.github.com>
    
    Co-authored-by: GCHQDev404 <GCHQDev404@users.noreply.github.com>

---
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/util/DefaultBestEffortsMergeFunction.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2022 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.gaffer.federatedstore.util;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import uk.gov.gchq.koryphe.impl.function.IterableConcat;
+import uk.gov.gchq.koryphe.impl.function.ToIterable;
+
+import java.util.function.BiFunction;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+public class DefaultBestEffortsMergeFunction implements BiFunction<Object, Iterable<Object>, Iterable<Object>> {
+
+    @Override
+    public Iterable<Object> apply(final Object o, final Iterable<Object> objects) {
+        return isNull(objects)
+                ? new ToIterable().apply(o)
+                : new IterableConcat<>().apply(Lists.newArrayList(new ToIterable().apply(o), objects));
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(13, 47)
+                .append(super.hashCode())
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return nonNull(obj) && this.getClass().equals(obj.getClass());
+    }
+}
