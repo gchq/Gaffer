6323ea7207ddfcba77ce0b2e35eb977351418c9c
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2022-08-18 12:58:52 +0100

    FederatedStore Alpha4 staging: Get and change Graph Ids and Info (#2740)
    * FederatedStore Alpha4 staging: Get and change Graph Ids and Info
    
    * FederatedStore Alpha4 staging: Get and change Graph Ids and Info - PR request
    
    Co-authored-by: GCHQDev404 <GCHQDev404@users.noreply.github.com>

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/impl/FederatedGetAllGraphsIDHandlerTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/impl/FederatedGetAllGraphsIDHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 Crown Copyright
+ * Copyright 2017-2022 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.gchq.gaffer.user.StoreUser.testUser;
 
-public class FederatedGetAllGraphsIDHandlerTest {
+public class FederatedGetAllGraphIDsHandlerTest {
 
 
     private User testUser;
