a5376db6011d1b62d6b40b901ed4bcd9185ab4e1
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2022-08-19 13:04:32 +0100

    Fix import order

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/AccumuloPropertiesTest.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/AccumuloPropertiesTest.java
@@ -25,8 +25,8 @@ import uk.gov.gchq.gaffer.sketches.serialisation.json.SketchesJsonModules;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccumuloPropertiesTest {
