324730270b0dff52789c612969ec08b0b7e06caa
lb324567 <108922572+lb324567@users.noreply.github.com>
2022-09-15 16:18:39 +0000

    Another fix

--- store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
+++ store-implementation/accumulo-store/src/test/java/uk/gov/gchq/gaffer/accumulostore/utils/AddUpdateTableIteratorTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2021 Crown Copyright
+ * Copyright 2017-2022 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package uk.gov.gchq.gaffer.accumulostore.utils;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,21 +41,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class AddUpdateTableIteratorTest {
+class AddUpdateTableIteratorTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(AddUpdateTableIteratorTest.class);
     private static final String GRAPH_ID = "graphId";
     private static final String SCHEMA_DIR = "src/test/resources/schema";
     private static final String SCHEMA_2_DIR = "src/test/resources/schema2";
     private static final String STORE_PROPS_PATH = "src/test/resources/store.properties";
     private static final String STORE_PROPS_2_PATH = "src/test/resources/store2.properties";
-    private static final String STORE_PROPS_PATH_UPDATED = "src/test/resources/current_store.properties";
-    private static final String STORE_PROPS_2_PATH_UPDATED = "src/test/resources/current_store2.properties";
     private static final String EMPTY_STORE_PROPS_PATH = "src/test/resources/empty-store.properties";
     private static final String FILE_GRAPH_LIBRARY_TEST_PATH = "target/graphLibrary";
-
-    private static final AccumuloProperties PROPERTIES_1 = AccumuloProperties.loadStoreProperties(STORE_PROPS_PATH);
-    private static final AccumuloProperties PROPERTIES_2 = AccumuloProperties.loadStoreProperties(STORE_PROPS_2_PATH);
 
     @BeforeEach
     @AfterEach
@@ -65,7 +60,7 @@ public class AddUpdateTableIteratorTest {
     }
 
     @Test
-    public void shouldRunMainWithFileGraphLibrary() throws Exception {
+    void shouldRunMainWithFileGraphLibrary() throws Exception {
         // Given
         final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
 
@@ -82,7 +77,7 @@ public class AddUpdateTableIteratorTest {
     }
 
     @Test
-    public void shouldOverrideExistingGraphInGraphLibrary() throws Exception {
+    void shouldOverrideExistingGraphInGraphLibrary() throws Exception {
         // Given
         shouldRunMainWithFileGraphLibrary(); // load version graph version 1 into the library.
         final String[] args = {GRAPH_ID, SCHEMA_2_DIR, STORE_PROPS_2_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
@@ -100,7 +95,7 @@ public class AddUpdateTableIteratorTest {
     }
 
     @Test
-    public void shouldRunMainWithNoGraphLibrary() throws Exception {
+    void shouldRunMainWithNoGraphLibrary() throws Exception {
         // Given
         final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "update"};
 
@@ -113,35 +108,42 @@ public class AddUpdateTableIteratorTest {
     }
 
     @Test
-    public void shouldThrowKeyErrorWhenInvalidModifyKeyGiven() throws Exception {
+    void shouldThrowKeyErrorWhenInvalidModifyKeyGiven() throws Exception {
         // Given
         final String[] args = {GRAPH_ID, SCHEMA_DIR, STORE_PROPS_PATH, "invalid key", FILE_GRAPH_LIBRARY_TEST_PATH};
 
         // When / Then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> AddUpdateTableIterator.main(args))
-                .withMessage("Supplied add or update key (invalid key) was not valid, it must either be add,remove or update.");
+            .isThrownBy(() -> AddUpdateTableIterator.main(args))
+            .withMessage("Supplied add or update key (invalid key) was not valid, it must either be add,remove or update.");
     }
 
     @Test
-    public void shouldReturnStoreClassNameNotFoundWhenStorePropsIsEmpty() throws Exception {
+    void shouldReturnStoreClassNameNotFoundWhenStorePropsIsEmpty() throws Exception {
         // Given
         final String[] args = {GRAPH_ID, SCHEMA_DIR, EMPTY_STORE_PROPS_PATH, "update", FILE_GRAPH_LIBRARY_TEST_PATH};
 
         // When / Then
         assertThatIllegalArgumentException()
-                .isThrownBy(() -> AddUpdateTableIterator.main(args))
-                        .withMessage("The Store class name was not found in the store properties for key: gaffer.store.class");
+            .isThrownBy(() -> AddUpdateTableIterator.main(args))
+            .withMessage("The Store class name was not found in the store properties for key: gaffer.store.class");
     }
 
     @Test
-    public void shouldReturnInvalidFilePathErrorWhenPathDoesNotExist() throws Exception {
-        // Given
-        final String[] args = {GRAPH_ID, SCHEMA_DIR, "invalid/file/path", "update", FILE_GRAPH_LIBRARY_TEST_PATH};
+    void shouldReturnInvalidFilePathErrorWhenPathDoesNotExist() throws Exception {
+        String expectedMessage;
 
-        // When / Then
+        final String[] args = {GRAPH_ID, SCHEMA_DIR, "invalid/file/path", "update", FILE_GRAPH_LIBRARY_TEST_PATH};
+        
+        // Without the following the return message will have / instead of \ when run on Windows";
+        if(SystemUtils.IS_OS_WINDOWS){
+            expectedMessage = "Failed to load store properties file : invalid\\file\\path";
+        } else{
+            expectedMessage = "Failed to load store properties file : invalid/file/path";
+        }
+
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> AddUpdateTableIterator.main(args))
-                .withMessage("Failed to load store properties file : invalid/file/path");
+            .isThrownBy(() -> AddUpdateTableIterator.main(args))
+            .withMessage(expectedMessage);
     }
 }
