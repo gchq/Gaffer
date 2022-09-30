3cb54851176e2aeeeed50b5d42d3193c124877ad
lb324567 <108922572+lb324567@users.noreply.github.com>
2022-09-30 13:20:05 +0000

    fix

--- core/graph/src/test/java/uk/gov/gchq/gaffer/graph/GraphTest.java
+++ core/graph/src/test/java/uk/gov/gchq/gaffer/graph/GraphTest.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Why won't this work?
  */
 
 package uk.gov.gchq.gaffer.graph;
