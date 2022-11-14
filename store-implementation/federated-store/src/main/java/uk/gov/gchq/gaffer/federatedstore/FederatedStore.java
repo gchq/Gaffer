7f03ae2fbea69b7dbd0d4c9989f05b1b7ad7d33d
GCHQDev404 <GCHQDev404@users.noreply.github.com>
2022-11-14 12:12:42 +0000

    gh-2819 FederatedStore DiscardOutput should not be wrapped.

--- store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/FederatedStore.java
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/FederatedStore.java
@@ -62,6 +62,7 @@ import uk.gov.gchq.gaffer.graph.GraphSerialisable;
 import uk.gov.gchq.gaffer.named.operation.AddNamedOperation;
 import uk.gov.gchq.gaffer.named.view.AddNamedView;
 import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.impl.DiscardOutput;
 import uk.gov.gchq.gaffer.operation.impl.Validate;
 import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
 import uk.gov.gchq.gaffer.operation.impl.function.Aggregate;
@@ -70,6 +71,7 @@ import uk.gov.gchq.gaffer.operation.impl.function.Transform;
 import uk.gov.gchq.gaffer.operation.impl.get.GetAdjacentIds;
 import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetElements;
+import uk.gov.gchq.gaffer.operation.io.Input;
 import uk.gov.gchq.gaffer.operation.io.Output;
 import uk.gov.gchq.gaffer.serialisation.Serialiser;
 import uk.gov.gchq.gaffer.store.Context;
@@ -499,7 +501,8 @@ public class FederatedStore extends Store {
                 .filter(op -> !Output.class.isAssignableFrom(op)
                         && !AddElements.class.equals(op)
                         && !AddNamedOperation.class.equals(op)
-                        && !AddNamedView.class.equals(op))
+                        && !AddNamedView.class.equals(op)
+                        && !DiscardOutput.class.equals(op))
                 //TODO FS [ op1 op2[graphA], op3 ] if op3 is one these it will break compared to old way?
                 .forEach(op -> addOperationHandler(op, new FederatedNoOutputHandler()));
 
