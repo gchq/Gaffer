44e36e8caee59e7942e17a1adf3c8ccad70518fd
t92549 <80890692+t92549@users.noreply.github.com>
2022-11-24 16:54:13 +0000

    Fix typo in package name

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreSchemaOverlapTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreSchemaOverlapTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- package uk.gov.gchq.gaffer.federatedstore;
+package uk.gov.gchq.gaffer.federatedstore;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
