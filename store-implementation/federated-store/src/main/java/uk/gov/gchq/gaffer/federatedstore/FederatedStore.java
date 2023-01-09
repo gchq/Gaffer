3ea443f5e05797bcc6bf8043a3376254007ab66e
GCHQDev404 <GCHQDev404@users.noreply.github.com>
2023-01-09 12:15:40 +0000

    gh-2859 revert typo of typo

--- store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/FederatedStore.java
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/FederatedStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2022 Crown Copyright
+ * Copyright 2017-2023 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -442,7 +442,7 @@ public class FederatedStore extends Store {
             operation.addOption(keyForFedStoreId, getValueForProcessedFedStoreId());
 
             // 4) return if the ID was found.
-            isFedStoreIdPreexisting = doesFedStoreIDOptionHaveContent || doesFedStoreIDOptionHaveContent;
+            isFedStoreIdPreexisting = doesFedStoreIDOptionHaveContent || doesPayloadHavePreexistingFedStoreId;
         }
         return isFedStoreIdPreexisting;
     }
