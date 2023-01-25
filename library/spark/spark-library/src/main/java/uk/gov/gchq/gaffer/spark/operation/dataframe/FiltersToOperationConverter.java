cee7f20ab9a0ddb58fe7fb1478b834c9e2e6634f
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2023-01-25 17:13:05 +0000

    Copyright

--- library/spark/spark-library/src/main/java/uk/gov/gchq/gaffer/spark/operation/dataframe/FiltersToOperationConverter.java
+++ library/spark/spark-library/src/main/java/uk/gov/gchq/gaffer/spark/operation/dataframe/FiltersToOperationConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 Crown Copyright
+ * Copyright 2016-2023 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.spark.operation.dataframe;
 
 import org.apache.commons.lang3.StringUtils;
