d7cd2730e9a144a9bce66e2c56c7d0499b48b9b6
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2023-03-01 11:28:02 +0000

    Revert superseded change identified in PR comment

--- store-implementation/map-store/src/test/java/uk/gov/gchq/gaffer/mapstore/MapStoreTest.java
+++ store-implementation/map-store/src/test/java/uk/gov/gchq/gaffer/mapstore/MapStoreTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Crown Copyright
+ * Copyright 2017-2020 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package uk.gov.gchq.gaffer.mapstore;
 
 import org.junit.jupiter.api.Test;
@@ -45,7 +44,7 @@ public class MapStoreTest {
                 StoreTrait.TRANSFORMATION,
                 StoreTrait.POST_TRANSFORMATION_FILTERING,
                 StoreTrait.MATCHED_VERTEX));
-        assertEquals(expectedTraits, mapStore.TRAITS);
+        assertEquals(expectedTraits, mapStore.getTraits());
     }
 
     @Test
