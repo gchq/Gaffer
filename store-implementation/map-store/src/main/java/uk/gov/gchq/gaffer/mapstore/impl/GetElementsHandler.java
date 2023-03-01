c3cc31f43588c1f4db49ca1155315af16ba9d63f
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2023-03-01 08:34:39 +0000

    Copyright corrections

--- store-implementation/map-store/src/main/java/uk/gov/gchq/gaffer/mapstore/impl/GetElementsHandler.java
+++ store-implementation/map-store/src/main/java/uk/gov/gchq/gaffer/mapstore/impl/GetElementsHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Crown Copyright
+ * Copyright 2017-2020 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package uk.gov.gchq.gaffer.mapstore.impl;
 
 import uk.gov.gchq.gaffer.commonutil.iterable.EmptyIterable;
