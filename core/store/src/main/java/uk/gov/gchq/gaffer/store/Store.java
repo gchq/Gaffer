a89d77810dfef3c8728ef232f97fbff6e610b7f7
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2023-03-02 20:22:48 +0000

    gh-2909 merge

--- core/store/src/main/java/uk/gov/gchq/gaffer/store/Store.java
+++ core/store/src/main/java/uk/gov/gchq/gaffer/store/Store.java
@@ -254,8 +254,7 @@ public abstract class Store {
 
         final String storeClass = storeProperties.getStoreClass();
         if (isNull(storeClass)) {
-            throw new IllegalArgumentException(String.format("The Store class name was not found in the store properties for key: %s, GraphId: %s",
-                    StoreProperties.STORE_CLASS, graphId));
+            throw new IllegalArgumentException(String.format("The Store class name was not found in the store properties for key: %s, GraphId: %s", StoreProperties.STORE_CLASS, graphId));
         }
 
         final Store newStore;
