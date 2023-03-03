1d38b80879c8e0a55a63981d17e46d81d6ef2a7e
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2023-03-03 00:06:27 +0000

    gh-2903 removed IT test.

--- store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAddGraphHandlerParent.java
+++ store-implementation/federated-store/src/main/java/uk/gov/gchq/gaffer/federatedstore/operation/handler/FederatedAddGraphHandlerParent.java
@@ -141,7 +141,7 @@ public abstract class FederatedAddGraphHandlerParent<OP extends AddGraph> implem
                 .store(store)
                 .graphId(operation.getGraphId())
                 .schema(operation.getSchema())
-                .storeProperties(operation.getStoreProperties()) //TODO add OperationDeclarations
+                .storeProperties(operation.getStoreProperties())
                 .parentSchemaIds(operation.getParentSchemaIds())
                 .parentStorePropertiesId(operation.getParentPropertiesId())
                 .buildGraphSerialisable();
