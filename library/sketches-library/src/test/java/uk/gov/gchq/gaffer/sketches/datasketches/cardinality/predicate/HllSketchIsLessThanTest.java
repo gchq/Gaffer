dec9129f883f3ae597ea63e5bdcf559176e26f94
t92549 <80890692+t92549@users.noreply.github.com>
2023-03-17 11:44:02 +0000

    Spotless apply

--- library/sketches-library/src/test/java/uk/gov/gchq/gaffer/sketches/datasketches/cardinality/predicate/HllSketchIsLessThanTest.java
+++ library/sketches-library/src/test/java/uk/gov/gchq/gaffer/sketches/datasketches/cardinality/predicate/HllSketchIsLessThanTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.sketches.datasketches.cardinality.predicate;
 
 import com.yahoo.sketches.hll.HllSketch;
