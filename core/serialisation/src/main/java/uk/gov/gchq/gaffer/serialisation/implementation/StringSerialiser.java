8eec02ed203a6c1bade305626f38af5d279bf1e3
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2023-03-23 10:45:04 +0000

    Copyright

--- core/serialisation/src/main/java/uk/gov/gchq/gaffer/serialisation/implementation/StringSerialiser.java
+++ core/serialisation/src/main/java/uk/gov/gchq/gaffer/serialisation/implementation/StringSerialiser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 Crown Copyright
+ * Copyright 2016-2023 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.serialisation.implementation;
 
 import uk.gov.gchq.gaffer.exception.SerialisationException;
