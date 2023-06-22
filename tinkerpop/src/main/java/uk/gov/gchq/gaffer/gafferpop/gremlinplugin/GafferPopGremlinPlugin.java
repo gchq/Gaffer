adbbee2197830a5a804c833542af423224d2d7bb
t92549 <80890692+t92549@users.noreply.github.com>
2023-06-22 14:12:15 +0100

    Fix copyright headers

--- tinkerpop/src/main/java/uk/gov/gchq/gaffer/gafferpop/gremlinplugin/GafferPopGremlinPlugin.java
+++ tinkerpop/src/main/java/uk/gov/gchq/gaffer/gafferpop/gremlinplugin/GafferPopGremlinPlugin.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.gafferpop.gremlinplugin;
 
 import org.apache.tinkerpop.gremlin.jsr223.AbstractGremlinPlugin;
--- tinkerpop/src/test/java/uk/gov/gchq/gaffer/gafferpop/generator/GafferPopEdgeGeneratorTest.java
+++ tinkerpop/src/test/java/uk/gov/gchq/gaffer/gafferpop/generator/GafferPopEdgeGeneratorTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.gafferpop.generator;
 
 import com.google.common.collect.Lists;
