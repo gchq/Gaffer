69c284aee95f384ff00af8e0a897a7f1bf973bf1
l46978 <141753189+l46978@users.noreply.github.com>
2023-11-13 10:47:48 +0000

    Reverted GafferPopEdge.java

--- library/tinkerpop/src/main/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopEdge.java
+++ library/tinkerpop/src/main/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopEdge.java
@@ -130,7 +130,11 @@ public final class GafferPopEdge extends GafferPopElement implements Edge {
     private static GafferPopVertex checkVertex(final Object vertexId, final GafferPopGraph graph) {
         final GafferPopVertex gafferPopVertex;
         if (vertexId instanceof Vertex) {
-            gafferPopVertex = new GafferPopVertex(GafferPopGraph.ID_LABEL, ((Vertex) vertexId).id(), graph);
+            if (vertexId instanceof GafferPopVertex) {
+                gafferPopVertex = checkVertex(((GafferPopVertex) vertexId), graph);
+            } else {
+                gafferPopVertex = new GafferPopVertex(GafferPopGraph.ID_LABEL, ((Vertex) vertexId).id(), graph);
+            }
         } else {
             gafferPopVertex = new GafferPopVertex(GafferPopGraph.ID_LABEL, vertexId, graph);
         }
