dd69dfefaafcb76a9a830b4fe20d10e16ca097c8
cn337131 <141730190+cn337131@users.noreply.github.com>
2023-11-29 12:33:07 +0000

    improve and refactor tests

--- library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopGremlinPluginTest.java
+++ library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopGremlinPluginTest.java
@@ -16,22 +16,73 @@
 
 package uk.gov.gchq.gaffer.tinkerpop;
 
+import org.apache.tinkerpop.gremlin.jsr223.Customizer;
+import org.apache.tinkerpop.gremlin.jsr223.GremlinPlugin;
+import org.apache.tinkerpop.gremlin.jsr223.ImportCustomizer;
+
 import org.junit.jupiter.api.Test;
 
+import uk.gov.gchq.gaffer.data.element.IdentifierType;
+import uk.gov.gchq.gaffer.data.element.function.ElementFilter;
+import uk.gov.gchq.gaffer.data.element.function.ElementTransformer;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.View;
+import uk.gov.gchq.gaffer.data.elementdefinition.view.ViewElementDefinition;
 import uk.gov.gchq.gaffer.tinkerpop.gremlinplugin.GafferPopGremlinPlugin;
+import uk.gov.gchq.koryphe.impl.function.Concat;
+import uk.gov.gchq.koryphe.impl.predicate.Exists;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GafferPopGremlinPluginTest {
 
-    // TODO name test better
     @Test
-    void shouldReturnStuff() {
+    void shouldCreateGafferPopGremlinPlugin() {
         // Given
         final GafferPopGremlinPlugin plugin = GafferPopGremlinPlugin.instance();
 
         // Then
         assertThat(plugin.getName()).isEqualTo("gafferpop");
+        assertThat(plugin).isInstanceOf(GremlinPlugin.class);
+    }
+
+    @Test
+    void shouldRequireRestart() {
+        // Given
+        final GafferPopGremlinPlugin plugin = GafferPopGremlinPlugin.instance();
+
+        // Then
         assertThat(plugin.requireRestart()).isTrue();
+    }
+
+    @Test
+    void shouldHaveSpecificCustomImports() {
+        // Given
+        final GafferPopGremlinPlugin plugin = GafferPopGremlinPlugin.instance();
+
+        // When
+        final Optional<Customizer[]> customizers = plugin.getCustomizers();
+        assertThat(customizers).isPresent();
+
+        Optional<ImportCustomizer> importCustomizer = Arrays.stream(customizers.get())
+                .filter(ImportCustomizer.class::isInstance)
+                .map(customizer -> (ImportCustomizer) customizer)
+                .findFirst();
+        assertThat(importCustomizer).isPresent();
+
+        // Then
+        assertThat(importCustomizer.get().getClassImports())
+                .hasSize(8);
+        assertThat(importCustomizer.get().getClassImports())
+                .contains(GafferPopGraph.class,
+                        View.class,
+                        ViewElementDefinition.class,
+                        ElementFilter.class,
+                        ElementTransformer.class,
+                        IdentifierType.class,
+                        Exists.class,
+                        Concat.class);
     }
 }
