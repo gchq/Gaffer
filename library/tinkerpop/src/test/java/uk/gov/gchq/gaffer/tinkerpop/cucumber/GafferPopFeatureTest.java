1fff8d0a8c75212163f1c6fb8f10b9aaf408e941
tb06904 <141412860+tb06904@users.noreply.github.com>
2023-12-15 13:49:12 +0000

    tweak to cucumber

--- library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/cucumber/GafferPopFeatureTest.java
+++ library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/cucumber/GafferPopFeatureTest.java
@@ -27,7 +27,6 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 import org.apache.commons.configuration2.MapConfiguration;
-import org.apache.tinkerpop.gremlin.LoadGraphWith.GraphData;
 import org.apache.tinkerpop.gremlin.TestHelper;
 import org.apache.tinkerpop.gremlin.features.World;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
@@ -54,7 +53,7 @@ import static org.apache.tinkerpop.gremlin.LoadGraphWith.GraphData;
         glue = { "org.apache.tinkerpop.gremlin.features" },
         objectFactory = GuiceFactory.class,
         features = { "classpath:/org/apache/tinkerpop/gremlin/test/features" },
-        plugin = {"progress", "junit:target/cucumber-reports/cucumber.xml", "html:target/cucumber-reports/cucumber.html"})
+        plugin = {"junit:target/cucumber-reports/cucumber.xml", "html:target/cucumber-reports/cucumber.html"})
 public final class GafferPopFeatureTest {
 
     public static final String TEST_USER_ID = "tinkerpopTestUser";
