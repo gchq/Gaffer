cc174bbb6cd8ddecee899f914d985ae9a7d585ba
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2024-02-20 15:40:34 +0000

    PR Comment - Use only AssertJ in CacheTest.java

--- core/cache/src/test/java/uk/gov/gchq/gaffer/cache/CacheTest.java
+++ core/cache/src/test/java/uk/gov/gchq/gaffer/cache/CacheTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2024 Crown Copyright
+ * Copyright 2018-2023 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,36 +16,43 @@
 
 package uk.gov.gchq.gaffer.cache;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import uk.gov.gchq.gaffer.cache.exception.CacheOperationException;
+import uk.gov.gchq.gaffer.cache.util.CacheProperties;
 import uk.gov.gchq.gaffer.commonutil.exception.OverwritingException;
 
-import java.util.Collections;
 import java.util.Properties;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CacheTest {
+
     private static final String CACHE_SERVICE_CLASS_STRING = "uk.gov.gchq.gaffer.cache.impl.HashMapCacheService";
     private static Cache<String, Integer> cache;
+    private static final Properties properties = new Properties();
+
+    @BeforeAll
+    public static void setUp() {
+        properties.setProperty(CacheProperties.CACHE_SERVICE_CLASS, CACHE_SERVICE_CLASS_STRING);
+        CacheServiceLoader.initialise(properties);
+        cache = new Cache<>("serviceName1");
+    }
 
     @BeforeEach
-    public void beforeEach() {
-        CacheServiceLoader.shutdown();
-        CacheServiceLoader.initialise(CACHE_SERVICE_CLASS_STRING);
-        cache = new Cache<>("cacheName1");
+    public void beforeEach() throws CacheOperationException {
+        cache.clearCache();
     }
 
     @Test
     public void shouldAddAndGetValueFromCache() throws CacheOperationException {
         cache.addToCache("key1", 1, true);
 
-        assertEquals((Integer) 1, cache.getFromCache("key1"));
-        assertNull(cache.getFromCache("key2"));
+        assertThat(cache.getFromCache("key1")).isEqualTo(1);
+        assertThat(cache.getFromCache("key2")).isNull();
     }
 
     @Test
@@ -53,7 +60,7 @@ public class CacheTest {
         cache.addToCache("key1", 1, true);
         cache.addToCache("key1", 2, true);
 
-        assertEquals(2, cache.getFromCache("key1").intValue());
+        assertThat(cache.getFromCache("key1").intValue()).isEqualTo(2);
     }
 
     @Test
@@ -64,12 +71,12 @@ public class CacheTest {
                 .isThrownBy(() -> cache.addToCache("key1", 2, false))
                 .withMessage("Cache entry already exists for key: key1");
 
-        assertEquals(1, cache.getFromCache("key1").intValue());
+        assertThat(cache.getFromCache("key1").intValue()).isEqualTo(1);
     }
 
     @Test
-    public void shouldGetCacheName() {
-        assertEquals("cacheName1", cache.getCacheName());
+    public void shouldGetCacheServiceName() {
+        assertThat(cache.getCacheName()).isEqualTo("serviceName1");
     }
 
     @Test
@@ -77,44 +84,6 @@ public class CacheTest {
         cache.addToCache("key1", 1, false);
         cache.deleteFromCache("key1");
 
-        assertNull(cache.getFromCache("key1"));
-    }
-
-    @Test
-    public void shouldGetAllKeys() throws CacheOperationException {
-        assertEquals(Collections.emptySet(), cache.getAllKeys());
-        cache.addToCache("key1", 1, false);
-        assertEquals(Collections.singleton("key1"), cache.getAllKeys());
-    }
-
-    @Test
-    public void shouldThrowExceptionTryingToGetAllKeysWhenNoServiceAvailable() {
-        CacheServiceLoader.shutdown();
-        assertThatExceptionOfType(uk.gov.gchq.gaffer.core.exception.GafferRuntimeException.class)
-                .isThrownBy(() -> cache.getAllKeys())
-                .withMessage("Error getting all keys")
-                .withStackTraceContaining("is not enabled, check it was initialised");
-    }
-
-    @Test
-    public void shouldConstructCacheWithCacheAndServiceName() {
-        final String serviceName = "myService";
-        final String cacheName = "myCache";
-        // Using the default service
-        Cache<String, Integer> tmpCache = new Cache<>(cacheName, serviceName);
-        assertEquals(cacheName, tmpCache.getCacheName());
-
-        // Using new service with name "myService"
-        CacheServiceLoader.shutdown();
-        CacheServiceLoader.initialise(serviceName, CACHE_SERVICE_CLASS_STRING, new Properties());
-        Cache<String, Integer> tmpCache2 = new Cache<>(cacheName, serviceName);
-        assertEquals(cacheName, tmpCache2.getCacheName());
-    }
-
-    @Test
-    public void shouldGetICache() {
-        assertEquals(CACHE_SERVICE_CLASS_STRING.replaceAll("Service", ""), cache.getCache().getClass().getName());
-        CacheServiceLoader.shutdown();
-        assertNull(cache.getCache());
+        assertThat(cache.getFromCache("key1")).isNull();
     }
 }
