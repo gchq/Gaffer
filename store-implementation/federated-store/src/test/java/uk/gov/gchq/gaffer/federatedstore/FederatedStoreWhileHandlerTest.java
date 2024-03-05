61943dca591f6f4236945ce47d930cbd6cec55fa
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2024-03-05 20:30:51 +0000

    gh-3166 added test to replicate issue

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreWhileHandlerTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreWhileHandlerTest.java
@@ -19,12 +19,23 @@ package uk.gov.gchq.gaffer.federatedstore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import uk.gov.gchq.gaffer.cache.impl.HashMapCacheService;
+import uk.gov.gchq.gaffer.cache.util.CacheProperties;
 import uk.gov.gchq.gaffer.data.element.Entity;
+import uk.gov.gchq.gaffer.federatedstore.operation.AddGraph;
+import uk.gov.gchq.gaffer.graph.Graph;
+import uk.gov.gchq.gaffer.graph.GraphConfig;
+import uk.gov.gchq.gaffer.named.operation.AddNamedOperation;
+import uk.gov.gchq.gaffer.named.operation.NamedOperation;
 import uk.gov.gchq.gaffer.operation.Operation;
+import uk.gov.gchq.gaffer.operation.OperationChain;
 import uk.gov.gchq.gaffer.operation.impl.While;
 import uk.gov.gchq.gaffer.operation.impl.add.AddElements;
 import uk.gov.gchq.gaffer.operation.impl.get.GetAllElements;
+import uk.gov.gchq.gaffer.store.Context;
+import uk.gov.gchq.gaffer.store.StoreProperties;
 import uk.gov.gchq.gaffer.store.schema.Schema;
+import uk.gov.gchq.gaffer.user.User;
 import uk.gov.gchq.koryphe.impl.predicate.Exists;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,6 +49,7 @@ import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.basicEnti
 import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.contextTestUser;
 import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.entityBasic;
 import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.resetForFederatedTests;
+import static uk.gov.gchq.gaffer.user.User.UNKNOWN_USER_ID;
 
 public class FederatedStoreWhileHandlerTest {
     @BeforeEach
@@ -82,7 +94,7 @@ public class FederatedStoreWhileHandlerTest {
     }
 
     @Test
-    void shouldNotWhileLoopOperationWithUnDistinctOperations() throws Exception {
+    void shouldNotWhileLoopOperationWithUnDistinctJavaObjectOperations() throws Exception {
         //given
         final FederatedStore federatedStore = new FederatedStore();
         final FederatedStoreProperties properties = new FederatedStoreProperties();
@@ -98,7 +110,7 @@ public class FederatedStoreWhileHandlerTest {
         federatedStore.execute(addElements, contextTestUser());
 
         //2nd Add Elements is same as 1st
-        //This is Java OO reuse issue.
+        //This is Java OO reuse.
         final Operation operation = new While.Builder()
                 .operation(addElements)
                 .conditional(o -> true)
@@ -114,9 +126,7 @@ public class FederatedStoreWhileHandlerTest {
                 .containsExactly(new Entity.Builder()
                         .group(GROUP_BASIC_ENTITY)
                         .vertex(BASIC_VERTEX)
-                        // This isn't 3 because of Java Object reuse issue,
-                        // Federation detects seeing the operation more than once
-                        .property(PROPERTY_1, 1)
+                        .property(PROPERTY_1, 3)
                         .build());
     }
 
@@ -147,6 +157,64 @@ public class FederatedStoreWhileHandlerTest {
 
         //then
         assertThat(federatedStore.execute(new GetAllElements(), contextTestUser()))
+                .containsExactly(new Entity.Builder()
+                        .group(GROUP_BASIC_ENTITY)
+                        .vertex(BASIC_VERTEX)
+                        .property(PROPERTY_1, 3)
+                        .build());
+    }
+
+    @Test
+    void shouldWhileLoopOperationFromNamedOperation() throws Exception {
+        //given
+        final FederatedStoreProperties properties = new FederatedStoreProperties();
+        properties.set(CacheProperties.CACHE_SERVICE_CLASS, HashMapCacheService.class.getName());
+
+        //Use graph due to hooks needed.
+        final Graph federated = new Graph.Builder()
+                .addSchema(new Schema())
+                .config(new GraphConfig(GRAPH_ID_TEST_FEDERATED_STORE))
+                .storeProperties(properties)
+                .build();
+
+        final Schema schema = basicEntitySchema();
+        final StoreProperties storeProperties = FederatedStoreTestUtil.loadAccumuloStoreProperties(FederatedStoreTestUtil.ACCUMULO_STORE_SINGLE_USE_PROPERTIES);
+        final Context context = new Context(new User(UNKNOWN_USER_ID));
+        federated.execute(OperationChain.wrap(
+                new AddGraph.Builder()
+                        .graphId(GRAPH_ID_ACCUMULO)
+                        .storeProperties(storeProperties)
+                        .schema(schema)
+                        .parentSchemaIds(null)
+                        .parentPropertiesId(null)
+                        .isPublic(true)
+                        .build()), context);
+
+        federated.execute(new AddElements.Builder()
+                .input(entityBasic())
+                .build(), contextTestUser());
+
+        //when
+        final String savedWhileOp = "savedWhileOp";
+        federated.execute(
+                new AddNamedOperation.Builder()
+                        .name(savedWhileOp)
+                        .operationChain(
+                                OperationChain.wrap(
+                                        new While.Builder()
+                                                .operation(new AddElements.Builder()
+                                                        .input(entityBasic())
+                                                        .build())
+                                                //This GetAllElements is re-occurring amd should not trigger a looping error.
+                                                .conditional(new Exists(), new GetAllElements())
+                                                .maxRepeats(2)
+                                                .build()))
+                        .build(), contextTestUser());
+
+        federated.execute(new NamedOperation.Builder().name(savedWhileOp).build(), contextTestUser());
+
+        //then
+        assertThat(federated.execute(new GetAllElements(), contextTestUser()))
                 .containsExactly(new Entity.Builder()
                         .group(GROUP_BASIC_ENTITY)
                         .vertex(BASIC_VERTEX)
