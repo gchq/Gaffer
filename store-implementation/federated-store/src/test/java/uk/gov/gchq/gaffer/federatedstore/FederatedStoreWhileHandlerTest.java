c2d98215c403eadeedb867c2fbafb8f17f1c880c
GCHQDev404 <45399082+GCHQDev404@users.noreply.github.com>
2024-03-13 15:19:56 +0000

    gh-3166 json added to try replicate bug

--- store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreWhileHandlerTest.java
+++ store-implementation/federated-store/src/test/java/uk/gov/gchq/gaffer/federatedstore/FederatedStoreWhileHandlerTest.java
@@ -39,6 +39,8 @@ import uk.gov.gchq.gaffer.store.schema.Schema;
 import uk.gov.gchq.gaffer.user.User;
 import uk.gov.gchq.koryphe.impl.predicate.Exists;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.BASIC_VERTEX;
 import static uk.gov.gchq.gaffer.federatedstore.FederatedStoreTestUtil.GRAPH_ID_ACCUMULO;
@@ -97,50 +99,137 @@ public class FederatedStoreWhileHandlerTest {
     @Test
     void shouldWhileLoopOperationWithRepeatingConditionalOperation() throws Exception {
         //given
-        final FederatedStore federatedStore = new FederatedStore();
         final FederatedStoreProperties properties = new FederatedStoreProperties();
-        federatedStore.initialise(GRAPH_ID_TEST_FEDERATED_STORE, new Schema(), properties);
+        properties.set(CacheProperties.CACHE_SERVICE_CLASS, HashMapCacheService.class.getName());
 
-        addGraphToAccumuloStore(federatedStore, GRAPH_ID_ACCUMULO, true, basicEntitySchema());
+        //Use graph due to hooks needed.
+        final Graph federated = new Graph.Builder()
+                .addSchema(new Schema())
+                .config(new GraphConfig(GRAPH_ID_TEST_FEDERATED_STORE))
+                .storeProperties(properties)
+                .build();
 
-        federatedStore.execute(new AddElements.Builder()
-                .input(entityBasic())
+
+        final Schema schema = basicEntitySchema();
+        final StoreProperties storeProperties = FederatedStoreTestUtil.loadAccumuloStoreProperties(FederatedStoreTestUtil.ACCUMULO_STORE_SINGLE_USE_PROPERTIES);
+        final Context context = new Context(new User(UNKNOWN_USER_ID));
+        federated.execute(OperationChain.wrap(
+                new AddGraph.Builder()
+                        .graphId(GRAPH_ID_ACCUMULO)
+                        .storeProperties(storeProperties)
+                        .schema(schema)
+                        .parentSchemaIds(null)
+                        .parentPropertiesId(null)
+                        .isPublic(true)
+                        .build()), context);
+
+        federated.execute(new AddElements.Builder()
+                .input(entityBasic(),entityBasic(),entityBasic())
                 .build(), contextTestUser());
 
         final Operation operation = JSONSerialiser.deserialise("{\n" +
-                "  \"class\" : \"uk.gov.gchq.gaffer.operation.OperationChain\",\n" +
-                "  \"operations\" : [ {\n" +
-                "    \"class\" : \"uk.gov.gchq.gaffer.operation.impl.While\",\n" +
-                "    \"conditional\" : {\n" +
-                "      \"transform\" : {\n" +
-                "        \"class\" : \"uk.gov.gchq.gaffer.operation.impl.get.GetAllElements\"\n" +
-                "      },\n" +
-                "      \"predicate\" : {\n" +
-                "        \"class\" : \"uk.gov.gchq.koryphe.impl.predicate.Exists\"\n" +
-                "      }\n" +
-                "    },\n" +
-                "    \"operation\" : {\n" +
-                "      \"class\" : \"uk.gov.gchq.gaffer.operation.impl.add.AddElements\",\n" +
-                "      \"input\" : [ {\n" +
-                "        \"class\" : \"uk.gov.gchq.gaffer.data.element.Entity\",\n" +
-                "        \"group\" : \"BasicEntity\",\n" +
-                "        \"vertex\" : \"basicVertex\",\n" +
-                "        \"properties\" : {\n" +
-                "          \"property1\" : 1\n" +
+                "  \"class\": \"uk.gov.gchq.gaffer.operation.OperationChain\",\n" +
+                "  \"operations\": [\n" +
+                "    {\n" +
+                "      \"class\": \"uk.gov.gchq.gaffer.operation.impl.get.GetElements\",\n" +
+//                "      \"options\": { \"gaffer.federatedstore.operation.graphIds\": \"dummy_graph\" },\n" +
+                "      \"input\": [\n" +
+                "        {\n" +
+                "          \"class\": \"uk.gov.gchq.gaffer.operation.data.EntitySeed\",\n" +
+                "          \"vertex\": \"basicVertex\"" +
+//                "          \"vertex\": {\n" +
+//                "            \"uk.gov.gchq.gaffer.types.TypeSubTypeValue\": {\n" +
+//                "              \"type\": \"test\",\n" +
+//                "              \"subType\": \"test\",\n" +
+//                "              \"value\": \"1\"\n" +
+//                "            }\n" +
+//                "          }\n" +
                 "        }\n" +
-                "      } ],\n" +
-                "      \"skipInvalidElements\" : false,\n" +
-                "      \"validate\" : true\n" +
+                "      ]\n" +
                 "    },\n" +
-                "    \"maxRepeats\" : 2\n" +
-                "  } ]\n" +
-                "}", OperationChain.class);
+                "    {\n" +
+                "      \"class\": \"uk.gov.gchq.gaffer.operation.impl.output.ToVertices\",\n" +
+                "      \"useMatchedVertex\": \"OPPOSITE\"\n" +
+                "    },\n" +
+                "    { \"class\": \"uk.gov.gchq.gaffer.operation.impl.output.ToEntitySeeds\" },\n" +
+                "    {\n" +
+                "      \"class\": \"uk.gov.gchq.gaffer.operation.impl.export.set.ExportToSet\",\n" +
+                "      \"key\": \"seeds\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"class\": \"uk.gov.gchq.gaffer.operation.impl.While\",\n" +
+//                "      \"options\": { \"gaffer.federatedstore.operation.graphIds\": \"dummy_graph\" },\n" +
+                "      \"maxRepeats\": 2,\n" +
+                "      \"operation\": {\n" +
+                "        \"class\": \"uk.gov.gchq.gaffer.operation.OperationChain\",\n" +
+                "        \"operations\": [\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.get.GetElements\",\n" +
+                "            \"options\": {\n" +
+//                "              \"gaffer.federatedstore.operation.graphIds\": \"dummy_graph\"\n" +
+                "            }\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.SetVariable\",\n" +
+                "            \"variableName\": \"current_hop_edges\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.GetVariable\",\n" +
+                "            \"variableName\": \"current_hop_edges\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.output.ToSingletonList\"\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.Reduce\",\n" +
+                "            \"aggregateFunction\": {\n" +
+                "              \"class\": \"uk.gov.gchq.koryphe.impl.binaryoperator.First\"\n" +
+                "            }\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.output.ToVertices\",\n" +
+                "            \"useMatchedVertex\": \"OPPOSITE\"\n" +
+                "          },\n" +
+                "          { \"class\": \"uk.gov.gchq.gaffer.operation.impl.output.ToEntitySeeds\" },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.join.Join\",\n" +
+                "            \"joinType\": \"OUTER\",\n" +
+                "            \"matchKey\": \"LEFT\",\n" +
+                "            \"matchMethod\": {\n" +
+                "              \"class\": \"uk.gov.gchq.gaffer.store.operation.handler.join.match.KeyFunctionMatch\"\n" +
+                "            },\n" +
+                "            \"operation\": {\n" +
+                "              \"class\": \"uk.gov.gchq.gaffer.operation.OperationChain\",\n" +
+                "              \"operations\": [\n" +
+                "                { \"class\": \"uk.gov.gchq.gaffer.operation.impl.DiscardOutput\" },\n" +
+                "                {\n" +
+                "                  \"class\": \"uk.gov.gchq.gaffer.operation.impl.export.set.GetSetExport\",\n" +
+                "                  \"key\": \"seeds\"\n" +
+                "                }\n" +
+                "              ]\n" +
+                "            }\n" +
+                "          },\n" +
+                "          {\n" +
+                "            \"class\": \"uk.gov.gchq.gaffer.operation.impl.ForEach\",\n" +
+                "            \"operation\": {\n" +
+                "              \"class\": \"uk.gov.gchq.gaffer.operation.impl.Reduce\",\n" +
+                "              \"aggregateFunction\": {\n" +
+                "                \"class\": \"uk.gov.gchq.koryphe.impl.binaryoperator.Last\"\n" +
+                "              }\n" +
+                "            }\n" +
+                "          }\n" +
+                "        ]\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}\n", OperationChain.class);
 
         //when
-        federatedStore.execute(operation, contextTestUser());
+        federated.execute(operation, contextTestUser());
+//        federated.execute(new NamedOperation.Builder<>().name("testNamedOp").input("basicVertex").build(), contextTestUser());
 
         //then
-        assertThat(federatedStore.execute(new GetAllElements(), contextTestUser()))
+        assertThat(federated.execute(new GetAllElements(), contextTestUser()))
                 .containsExactly(new Entity.Builder()
                         .group(GROUP_BASIC_ENTITY)
                         .vertex(BASIC_VERTEX)
@@ -196,7 +285,6 @@ public class FederatedStoreWhileHandlerTest {
                         .build(), contextTestUser());
 
         federated.execute(new NamedOperation.Builder().name(savedWhileOp).build(), contextTestUser());
-
         //then
         assertThat(federated.execute(new GetAllElements(), contextTestUser()))
                 .containsExactly(new Entity.Builder()
