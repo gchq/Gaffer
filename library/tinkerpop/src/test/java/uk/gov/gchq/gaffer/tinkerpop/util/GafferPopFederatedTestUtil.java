8b846346442733470987194da670f3393a436c57
cn337131 <141730190+cn337131@users.noreply.github.com>
2024-04-19 11:10:52 +0000

    copyright

--- library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/util/GafferPopFederatedTestUtil.java
+++ library/tinkerpop/src/test/java/uk/gov/gchq/gaffer/tinkerpop/util/GafferPopFederatedTestUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 Crown Copyright
+ * Copyright 2024 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package uk.gov.gchq.gaffer.tinkerpop.util;
 
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
