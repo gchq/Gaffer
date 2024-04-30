331f1eddd829caef2393468c627eeea613dbf144
GCHQDeveloper314 <94527357+GCHQDeveloper314@users.noreply.github.com>
2024-04-30 18:00:39 +0100

    Add tests for new options

--- core/store/src/test/java/uk/gov/gchq/gaffer/store/integration/StoreIT.java
+++ core/store/src/test/java/uk/gov/gchq/gaffer/store/integration/StoreIT.java
@@ -16,8 +16,10 @@
 
 package uk.gov.gchq.gaffer.store.integration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import uk.gov.gchq.gaffer.cache.CacheServiceLoader;
 import uk.gov.gchq.gaffer.commonutil.StreamUtil;
 import uk.gov.gchq.gaffer.commonutil.TestGroups;
 import uk.gov.gchq.gaffer.data.element.Element;
@@ -49,6 +51,10 @@ import static uk.gov.gchq.gaffer.store.StoreTrait.PRE_AGGREGATION_FILTERING;
 import static uk.gov.gchq.gaffer.store.StoreTrait.TRANSFORMATION;
 
 public class StoreIT {
+    @BeforeEach
+    void before() {
+        CacheServiceLoader.shutdown();
+    }
 
     @Test
     public void shouldCreateStoreAndValidateSchemas() throws SchemaException, StoreException {
@@ -73,6 +79,40 @@ public class StoreIT {
         assertThat(testStore.getSchema().getEntities()).doesNotContainKey(TestGroups.ENTITY_2);
 
         assertThat(testStore.getSchema().validate().isValid()).isTrue();
+    }
+
+    @Test
+    public void shouldCreateStoreWithSpecifiedCaches() throws SchemaException, StoreException {
+        // Given
+        final Store testStore = new TestStore();
+
+        // When
+        testStore.initialise("testGraph", new Schema(), StoreProperties.loadStoreProperties("allCaches.properties"));
+
+        // Then
+        assertThat(CacheServiceLoader.isDefaultEnabled()).isFalse();
+        assertThat(CacheServiceLoader.isEnabled("JobTracker")).isTrue();
+        assertThat(CacheServiceLoader.isEnabled("NamedView")).isTrue();
+        assertThat(CacheServiceLoader.isEnabled("NamedOperation")).isTrue();
+    }
+
+    @Test
+    public void shouldCreateStoreWithoutAnyCaches() throws SchemaException, StoreException {
+        // Given
+        final Store testStore = new TestStore();
+        final StoreProperties properties = new StoreProperties();
+        properties.setJobTrackerEnabled(false);
+        properties.setNamedViewEnabled(false);
+        properties.setNamedOperationEnabled(false);
+
+        // When
+        testStore.initialise("testGraph", new Schema(), properties);
+
+        // Then
+        assertThat(CacheServiceLoader.isDefaultEnabled()).isFalse();
+        assertThat(CacheServiceLoader.isEnabled("JobTracker")).isFalse();
+        assertThat(CacheServiceLoader.isEnabled("NamedView")).isFalse();
+        assertThat(CacheServiceLoader.isEnabled("NamedOperation")).isFalse();
     }
 
     private class TestStore extends Store {
