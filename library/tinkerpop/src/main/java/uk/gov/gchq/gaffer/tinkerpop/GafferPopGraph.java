9d904ca0655955753da1b0e81135d1716e2629c6
cn337131 <141730190+cn337131@users.noreply.github.com>
2024-05-09 13:24:48 +0000

    Gh-3200: Additional Federated Stores Tinkerpop tests (#3204)
    * additional testing and remove hard coded direction
    
    * change graph

--- library/tinkerpop/src/main/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopGraph.java
+++ library/tinkerpop/src/main/java/uk/gov/gchq/gaffer/tinkerpop/GafferPopGraph.java
@@ -894,7 +894,7 @@ public class GafferPopGraph implements org.apache.tinkerpop.gremlin.structure.Gr
                 seeds.add(new EntitySeed(((Vertex) id).id()));
             // Extract Edge ID
             } else if (id instanceof Edge) {
-                seeds.add(new EdgeSeed(((Edge) id).outVertex().id(), ((Edge) id).inVertex().id(), true));
+                seeds.add(new EdgeSeed(((Edge) id).outVertex().id(), ((Edge) id).inVertex().id()));
             // Extract source and destination from ID list
             } else if (id instanceof Iterable) {
                 ((Iterable<?>) id).forEach(edgeIdList::add);
@@ -912,7 +912,7 @@ public class GafferPopGraph implements org.apache.tinkerpop.gremlin.structure.Gr
 
             // If found a list verify source and destination
             if (edgeIdList.size() == 2) {
-                seeds.add(new EdgeSeed(edgeIdList.get(0), edgeIdList.get(1), true));
+                seeds.add(new EdgeSeed(edgeIdList.get(0), edgeIdList.get(1)));
             }
         });
 
