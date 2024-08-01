a81dee03157f7c6b5326ccd5598fb2de72cdec2c
j69772 <172510359+j69772@users.noreply.github.com>
2024-08-01 15:28:35 +0000

    add attempt at improving readability of size method and removed comments

--- core/common-util/src/main/java/uk/gov/gchq/gaffer/commonutil/OneOrMore.java
+++ core/common-util/src/main/java/uk/gov/gchq/gaffer/commonutil/OneOrMore.java
@@ -116,24 +116,12 @@ public class OneOrMore<T> implements Iterable<T> {
         }
     }
 
-    // public int size() {
-    //     if (null != collection) {
-    //         return collection.size();
-    //     }
-    //     return (singleItem != null) ? 1 : 0;
-    // }
-
     public int size() {
-        if (null == collection) {
-            if (null != singleItem) {
-                return 1;
-            }
-            return 0;
+        if (null != collection) {
+            return collection.size();
         }
-
-        return collection.size();
+        return (singleItem != null) ? 1 : 0;
     }
-
 
     public boolean isEmpty() {
         if (null == collection) {
