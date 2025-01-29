3ec4c9bdeb2872c2d98e29f1b9d9e295b1608d25
p29876 <165825455+p29876@users.noreply.github.com>
2025-01-29 14:45:56 +0000

    Gh-3350: Simple fed store fixes (#3358)
    * Gh-3350 Fed store fixes
    
    * checkstyle
    
    * debug
    
    * undo
    
    ---------
    
    Co-authored-by: tb06904 <141412860+tb06904@users.noreply.github.com>

--- store-implementation/simple-federated-store/src/main/java/uk/gov/gchq/gaffer/federated/simple/operation/handler/SeparateOutputHandler.java
+++ store-implementation/simple-federated-store/src/main/java/uk/gov/gchq/gaffer/federated/simple/operation/handler/SeparateOutputHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Crown Copyright
+ * Copyright 2024-2025 Crown Copyright
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import uk.gov.gchq.gaffer.federated.simple.FederatedStore;
+import uk.gov.gchq.gaffer.federated.simple.FederatedUtils;
 import uk.gov.gchq.gaffer.graph.GraphSerialisable;
+import uk.gov.gchq.gaffer.operation.OperationChain;
 import uk.gov.gchq.gaffer.operation.OperationException;
 import uk.gov.gchq.gaffer.operation.io.Output;
 import uk.gov.gchq.gaffer.store.Context;
@@ -39,6 +41,7 @@ public class SeparateOutputHandler<P extends Output<O>, O> extends FederatedOper
 
     @Override
     public Map<String, O> doOperation(final P operation, final Context context, final Store store) throws OperationException {
+        final int fixLimit = Integer.parseInt(operation.getOption(OPT_FIX_OP_LIMIT, String.valueOf(DFLT_FIX_OP_LIMIT)));
         List<GraphSerialisable> graphsToExecute = this.getGraphsToExecuteOn(operation, context, (FederatedStore) store);
 
         if (graphsToExecute.isEmpty()) {
@@ -50,7 +53,8 @@ public class SeparateOutputHandler<P extends Output<O>, O> extends FederatedOper
         Map<String, O> results = new HashMap<>();
         for (final GraphSerialisable gs : graphsToExecute) {
             try {
-                results.put(gs.getGraphId(), gs.getGraph().execute(operation, context.getUser()));
+                OperationChain<O> fixedChain = FederatedUtils.getValidOperationForGraph(operation, gs, 0, fixLimit);
+                results.put(gs.getGraphId(), gs.getGraph().execute(fixedChain, context.getUser()));
             } catch (final OperationException | UnsupportedOperationException e) {
                 // Optionally skip this error if user has specified to do so
                 LOGGER.error("Operation failed on graph: {}", gs.getGraphId());
